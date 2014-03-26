e430ae4602945afc502f7cf98b8627a03483dc94
Gaelan <gbs@canishe.com>
2014-03-25 19:50:31 -0700

    Break Ruby's built-in `leap?` function.

--- leap/leap_test.rb
+++ leap/leap_test.rb
@@ -1,5 +1,15 @@
+require 'date'
 require 'minitest/autorun'
 require_relative 'year'
+
+class Date
+  def leap?
+    throw "Try to implement this yourself instead of using Ruby's implementation."
+  end
+  
+  alias :gregorian_leap?, :leap?
+  alias :julian_leap?, :leap?
+end
 
 class YearTest < MiniTest::Unit::TestCase
   def test_leap_year
