a07a3b7b24879043998050f62aa03da209104f27
Bob Ippolito <bob@redivi.com>
2014-04-12 18:52:01 -0700

    make Ruby house consistent with Python and the README, see exercism/xhaskell#6

--- house/example.rb
+++ house/example.rb
@@ -8,25 +8,25 @@ class House
   end
 
   def line(i)
-    "This is %s.\n" % pieces.last(i).join(' ')
+    "This is %s.\n" % pieces.last(i).map {|s| s.join("\n") }.join(' ')
   end
 
   private
 
   def pieces
     [
-      'the horse and the hound and the horn that belonged to',
-      'the farmer sowing his corn that kept',
-      'the rooster that crowed in the morn that woke',
-      'the priest all shaven and shorn that married',
-      'the man all tattered and torn that kissed',
-      'the maiden all forlorn that milked',
-      'the cow with the crumpled horn that tossed',
-      'the dog that worried',
-      'the cat that killed',
-      'the rat that ate',
-      'the malt that lay in',
-      'the house that Jack built',
+      ['the horse and the hound and the horn', 'that belonged to'],
+      ['the farmer sowing his corn', 'that kept'],
+      ['the rooster that crowed in the morn', 'that woke'],
+      ['the priest all shaven and shorn', 'that married'],
+      ['the man all tattered and torn', 'that kissed'],
+      ['the maiden all forlorn', 'that milked'],
+      ['the cow with the crumpled horn', 'that tossed'],
+      ['the dog', 'that worried'],
+      ['the cat', 'that killed'],
+      ['the rat', 'that ate'],
+      ['the malt', 'that lay in'],
+      ['the house that Jack built'],
     ]
   end
 end
