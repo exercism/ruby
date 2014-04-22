2ff5523fb62ab4988b36b1f7b1141620d937b36d
Katrina Owen <katrina.owen@gmail.com>
2014-04-22 09:39:51 -0600

    Add a tutorial for bob. Fixes exercism/exercism.io#1545

--- bob/bob_test.rb
+++ bob/bob_test.rb
@@ -1,169 +1,133 @@
 require 'minitest/autorun'
-
 begin
   require_relative 'bob'
 rescue LoadError => e
-  eval("\"#{DATA.read}\n\"").split("\n.\n").each_with_index do |s,i|
-    if i > 0
-      puts "\t--- press enter to continue ---"
-      gets
-    end
-    puts "\n\n", s, "\n\n\n"
-  end
-  exit!
+  puts "\n\n#{e.backtrace.first} #{e.message}"
+  puts DATA.read
+  exit 1
 end
 
-class TeenagerTest < MiniTest::Unit::TestCase
-  attr_reader :teenager
-
-  def setup
-    @teenager = ::Bob.new
-  end
-
+class BobTest < MiniTest::Unit::TestCase
   def test_stating_something
-    assert_equal 'Whatever.', teenager.hey('Tom-ay-to, tom-aaaah-to.')
+    assert_equal 'Whatever.', ::Bob.new.hey('Tom-ay-to, tom-aaaah-to.')
   end
 
   def test_shouting
     skip
-    assert_equal 'Woah, chill out!', teenager.hey('WATCH OUT!')
+    assert_equal 'Woah, chill out!', ::Bob.new.hey('WATCH OUT!')
   end
 
   def test_shouting_gibberish
     skip
     gibberish = ('A'..'Z').to_a.shuffle[0,10].join
-    assert_equal 'Woah, chill out!', teenager.hey(gibberish)
+    assert_equal 'Woah, chill out!', ::Bob.new.hey(gibberish)
   end
 
   def test_asking_a_question
     skip
-    assert_equal 'Sure.', teenager.hey('Does this cryogenic chamber make me look fat?')
+    assert_equal 'Sure.', ::Bob.new.hey('Does this cryogenic chamber make me look fat?')
   end
 
   def test_asking_a_numeric_question
     skip
-    assert_equal 'Sure.', teenager.hey('You are, what, like 15?')
+    assert_equal 'Sure.', ::Bob.new.hey('You are, what, like 15?')
   end
 
   def test_asking_gibberish
     skip
     gibberish = ('a'..'z').to_a.shuffle[0,10].join
-    assert_equal 'Sure.', teenager.hey("#{gibberish}?")
+    assert_equal 'Sure.', ::Bob.new.hey("#{gibberish}?")
   end
 
   def test_talking_forcefully
     skip
-    assert_equal 'Whatever.', teenager.hey("Let's go make out behind the gym!")
+    assert_equal 'Whatever.', ::Bob.new.hey("Let's go make out behind the gym!")
   end
 
   def test_using_acronyms_in_regular_speech
     skip
-    assert_equal 'Whatever.', teenager.hey("It's OK if you don't want to go to the DMV.")
+    assert_equal 'Whatever.', ::Bob.new.hey("It's OK if you don't want to go to the DMV.")
   end
 
   def test_forceful_questions
     skip
-    assert_equal 'Woah, chill out!', teenager.hey('WHAT THE HELL WERE YOU THINKING?')
+    assert_equal 'Woah, chill out!', ::Bob.new.hey('WHAT THE HELL WERE YOU THINKING?')
   end
 
   def test_shouting_numbers
     skip
-    assert_equal 'Woah, chill out!', teenager.hey('1, 2, 3 GO!')
+    assert_equal 'Woah, chill out!', ::Bob.new.hey('1, 2, 3 GO!')
   end
 
   def test_only_numbers
     skip
-    assert_equal 'Whatever.', teenager.hey('1, 2, 3')
+    assert_equal 'Whatever.', ::Bob.new.hey('1, 2, 3')
   end
 
   def test_question_with_only_numbers
     skip
-    assert_equal 'Sure.', teenager.hey('4?')
+    assert_equal 'Sure.', ::Bob.new.hey('4?')
   end
 
   def test_shouting_with_special_characters
     skip
-    assert_equal 'Woah, chill out!', teenager.hey('ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!')
+    assert_equal 'Woah, chill out!', ::Bob.new.hey('ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!')
   end
 
   def test_shouting_with_no_exclamation_mark
     skip
-    assert_equal 'Woah, chill out!', teenager.hey('I HATE YOU')
+    assert_equal 'Woah, chill out!', ::Bob.new.hey('I HATE YOU')
   end
 
   def test_statement_containing_question_mark
     skip
-    assert_equal 'Whatever.', teenager.hey('Ending with ? means a question.')
+    assert_equal 'Whatever.', ::Bob.new.hey('Ending with ? means a question.')
   end
 
   def test_prattling_on
     skip
-    assert_equal 'Sure.', teenager.hey("Wait! Hang on. Are you going to be OK?")
+    assert_equal 'Sure.', ::Bob.new.hey("Wait! Hang on. Are you going to be OK?")
   end
 
   def test_silence
     skip
-    assert_equal 'Fine. Be that way!', teenager.hey('')
+    assert_equal 'Fine. Be that way!', ::Bob.new.hey('')
   end
 
   def test_prolonged_silence
     skip
     silence = " " * rand(1..10)
-    assert_equal 'Fine. Be that way!', teenager.hey(silence)
+    assert_equal 'Fine. Be that way!', ::Bob.new.hey(silence)
   end
 
   def test_on_multiple_line_questions
     skip
-    assert_equal 'Whatever.', teenager.hey(%{
+    assert_equal 'Whatever.', ::Bob.new.hey(%{
 Does this cryogenic chamber make me look fat?
 no})
   end
 end
+
 __END__
 
-######## ########  ########   #######  ########
-##       ##     ## ##     ## ##     ## ##     ##
-##       ##     ## ##     ## ##     ## ##     ##
-######   ########  ########  ##     ## ########
-##       ##   ##   ##   ##   ##     ## ##   ##
-##       ##    ##  ##    ##  ##     ## ##    ##
-######## ##     ## ##     ##  #######  ##     ##
+*****************************************************
+You got an error, which is exactly as it should be.
+This is the first step in the Test-Driven Development
+(TDD) process.
 
+The most important part of the error is
 
-#{e.backtrace.first} #{e.message}
+      cannot load such file
 
-Welcome to your first ruby Exercism exercise!  This is your first error message.
-.
-First it tells you the name of the file where the error is occurring:
+It's looking for a file named bob.rb that doesn't
+exist yet.
 
-    bob_test.rb
-.
-Then it tells you which line that error is on:
+To fix the error, create an empty file named bob.rb
+in the same directory as the bob_test.rb file.
 
-    bob_test.rb:4
-.
-After that, it tells you the name of the method where the error is occurring:
+Then run the test again.
 
-    in `require_relative'
-.
-Next, it tells you exactly what the error is:
+For more guidance as you work on this exercise, see
+GETTING_STARTED.md.
+*****************************************************
 
-    #{e.message.split('--').first}
-.
-Finally, it tells you which file is missing:
-
-    #{e.message.split('--').last}
-.
-So the error is there on line 4 of this file (bob_test.rb).  What's on line 4?
-
-    require_relative 'bob'
-
-We are trying to require the file, and it isn't there.
-.
-You can fix the problem by creating an empty file named bob.rb inside
-of the ruby/bob directory.
-
-Then run this test again (ruby bob_test.rb).  Make all the tests pass,
-and submit your solution (exercism submit bob.rb).
-
-More instructions are in README.md in this directory. Good luck!
