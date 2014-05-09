098abac926c6685d7bc19476894784c8469ab03d
Anthony Green <anthony.green@bbc.co.uk>
2014-05-09 03:04:57 +0100

    Add Circular Buffer exercism

---
+++ circular-buffer/circular_buffer_test.rb
@@ -0,0 +1,108 @@
+require 'minitest/autorun'
+require_relative 'circular_buffer'
+
+class CircularBufferTest < MiniTest::Unit::TestCase
+  
+  def test_read_empty_buffer_throws_buffer_empty_exception
+    buffer = CircularBuffer.new(1)
+    assert_raises(CircularBuffer::BufferEmptyException) { buffer.read }
+  end
+  
+  def test_write_and_read_back_one_item
+    skip
+    buffer = CircularBuffer.new(1)
+    buffer.write '1'
+    assert_equal '1', buffer.read
+    assert_raises(CircularBuffer::BufferEmptyException) { buffer.read }
+  end
+  
+  def test_write_and_read_back_multiple_items
+    skip
+    buffer = CircularBuffer.new(2)
+    buffer.write '1'
+    buffer.write '2'
+    assert_equal '1', buffer.read
+    assert_equal '2', buffer.read
+    assert_raises(CircularBuffer::BufferEmptyException) { buffer.read }
+  end
+  
+  def test_clearing_buffer
+    skip
+    buffer = CircularBuffer.new(3)
+    (1..3).each { |i| buffer.write String(i) }
+    buffer.clear
+    assert_raises(CircularBuffer::BufferEmptyException) { buffer.read }
+    buffer.write '1'
+    buffer.write '2'
+    assert_equal '1', buffer.read
+    buffer.write '3'
+    assert_equal '2', buffer.read
+  end
+  
+  def test_alternate_write_and_read
+    skip
+    buffer = CircularBuffer.new(2)
+    buffer.write '1'
+    assert_equal '1', buffer.read
+    buffer.write '2'
+    assert_equal '2', buffer.read
+  end
+
+  def test_reads_back_oldest_item
+    skip
+    buffer = CircularBuffer.new(3)
+    buffer.write '1'
+    buffer.write '2'
+    buffer.read
+    buffer.write '3'
+    assert_equal '2', buffer.read
+    assert_equal '3', buffer.read
+  end
+  
+  def test_writes_of_nil_should_not_occupy_buffer
+    skip    
+    buffer = CircularBuffer.new(5)
+    buffer.write nil
+    (1..3).each { |i| buffer.write String(i) }
+    assert_equal '1', buffer.read
+  end
+  
+  def test_writing_to_a_full_buffer_throws_an_exception
+    skip    
+    buffer = CircularBuffer.new(2)
+    buffer.write '1'
+    buffer.write '2'
+    assert_raises(CircularBuffer::BufferFullException) { buffer.write 'A' }
+  end
+
+  def test_overwriting_oldest_item_in_a_full_buffer
+    skip    
+    buffer = CircularBuffer.new(2)
+    buffer.write '1'
+    buffer.write '2'
+    buffer.write! 'A'
+    assert_equal '2', buffer.read
+    assert_equal 'A', buffer.read
+    assert_raises(CircularBuffer::BufferEmptyException) { buffer.read }
+  end
+  
+  def test_alternate_read_and_write_into_buffer_overflow
+    skip    
+    buffer = CircularBuffer.new(5)
+    (1..3).each { |i| buffer.write String(i) }
+    buffer.read
+    buffer.read
+    buffer.write '4'
+    buffer.read
+    (5..8).each { |i| buffer.write String(i) }
+    buffer.write! 'A'
+    buffer.write! 'B'
+    (6..8).each do |i| 
+      assert_equal String(i), buffer.read
+    end
+    assert_equal 'A', buffer.read
+    assert_equal 'B', buffer.read
+    assert_raises(CircularBuffer::BufferEmptyException) { buffer.read }
+  end
+    
+end
