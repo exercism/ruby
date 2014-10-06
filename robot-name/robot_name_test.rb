32daea55206fad38b3684c096247b5e54e96d3e6
Vitor Sakaguti <vitoreiji0@gmail.com>
2014-10-06 18:57:22 -0300

    Enforce limits to robot name pattern

--- robot-name/robot_name_test.rb
+++ robot-name/robot_name_test.rb
@@ -3,7 +3,7 @@ require_relative 'robot'
 
 class RobotTest < MiniTest::Unit::TestCase
   def test_has_name
-    assert_match /[a-zA-Z]{2}\d{3}/, Robot.new.name
+    assert_match /^[a-zA-Z]{2}\d{3}$/, Robot.new.name
   end
 
   def test_name_sticks
@@ -25,6 +25,6 @@ class RobotTest < MiniTest::Unit::TestCase
     robot.reset
     name2 = robot.name
     assert name != name2
-    assert_match /[a-zA-Z]{2}\d{3}/, name2
+    assert_match /^[a-zA-Z]{2}\d{3}$/, name2
   end
 end
