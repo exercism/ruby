19498095c437f5359cb9e9007464b5c4facd2e0d
Katrina Owen <katrina.owen@gmail.com>
2014-11-12 07:01:26 -0800

    Add guide rails to hamming problem

--- hamming/hamming_test.rb
+++ hamming/hamming_test.rb
@@ -1,5 +1,11 @@
 require 'minitest/autorun'
-require_relative 'hamming'
+begin
+  require_relative 'hamming'
+rescue LoadError => e
+  puts "\n\n#{e.backtrace.first} #{e.message}"
+  puts DATA.read
+  exit 1
+end
 
 class HammingTest < MiniTest::Unit::TestCase
   def test_no_difference_between_identical_strands
@@ -46,3 +52,26 @@ class HammingTest < MiniTest::Unit::TestCase
     assert_equal 9, Hamming.compute('GGACGGATTCTG', 'AGGACGGATTCT')
   end
 end
+
+__END__
+
+*****************************************************
+You got an error, which is exactly as it should be.
+This is the first step in the Test-Driven Development
+(TDD) process.
+
+The most important part of the error is
+
+      cannot load such file
+
+It's looking for a file named bob.rb that doesn't
+exist yet.
+
+To fix the error, create an empty file named bob.rb
+in the same directory as the bob_test.rb file.
+
+Then run the test again.
+
+For more guidance as you work on this exercise, see
+GETTING_STARTED.md.
+*****************************************************
