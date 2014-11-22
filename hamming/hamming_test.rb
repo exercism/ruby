4592571c9af1697bf16e86f46b05a0c81f7c2c72
Kyle Martin <KyleMartin901@gmail.com>
2014-11-22 22:39:59 +1100

    Change hamming tests to test against instance methods
    The hamming tests were testing against class methods yet the Getting Started
    document refers to hamming as an object and the solution given for the first
    test is an instance method. Based on the evidence of the getting started document
    and exercise being the first I have presumed that the tests should of been
    against an instance method not a class method. I have corrected the tests to
    initialise a hamming object before calling the methods.

--- hamming/hamming_test.rb
+++ hamming/hamming_test.rb
@@ -9,47 +9,47 @@ end
 
 class HammingTest < MiniTest::Unit::TestCase
   def test_no_difference_between_identical_strands
-    assert_equal 0, Hamming.compute('A', 'A')
+    assert_equal 0, Hamming.new.compute('A', 'A')
   end
 
   def test_complete_hamming_distance_of_for_single_nucleotide_strand
     skip
-    assert_equal 1, Hamming.compute('A','G')
+    assert_equal 1, Hamming.new.compute('A','G')
   end
 
   def test_complete_hamming_distance_of_for_small_strand
     skip
-    assert_equal 2, Hamming.compute('AG','CT')
+    assert_equal 2, Hamming.new.compute('AG','CT')
   end
 
   def test_small_hamming_distance
     skip
-    assert_equal 1, Hamming.compute('AT','CT')
+    assert_equal 1, Hamming.new.compute('AT','CT')
   end
 
   def test_small_hamming_distance_in_longer_strand
     skip
-    assert_equal 1, Hamming.compute('GGACG', 'GGTCG')
+    assert_equal 1, Hamming.new.compute('GGACG', 'GGTCG')
   end
 
   def test_ignores_extra_length_on_first_strand_when_longer
     skip
-    assert_equal 1, Hamming.compute('AGAGACTTA', 'AAA')
+    assert_equal 1, Hamming.new.compute('AGAGACTTA', 'AAA')
   end
 
   def test_ignores_extra_length_on_other_strand_when_longer
     skip
-    assert_equal 2, Hamming.compute('AGG', 'AAAACTGACCCACCCCAGG')
+    assert_equal 2, Hamming.new.compute('AGG', 'AAAACTGACCCACCCCAGG')
   end
 
   def test_large_hamming_distance
     skip
-    assert_equal 4, Hamming.compute('GATACA', 'GCATAA')
+    assert_equal 4, Hamming.new.compute('GATACA', 'GCATAA')
   end
 
   def test_hamming_distance_in_very_long_strand
     skip
-    assert_equal 9, Hamming.compute('GGACGGATTCTG', 'AGGACGGATTCT')
+    assert_equal 9, Hamming.new.compute('GGACGGATTCTG', 'AGGACGGATTCT')
   end
 end
 
