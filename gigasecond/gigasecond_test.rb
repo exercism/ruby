ba4c0315f4b1559e185708be6e9034c9d2892974
Andrew Haines <andrew@haines.org.nz>
2015-02-04 17:11:33 +0000

    Use UTC in gigasecond exercise to avoid daylight savings issues

--- gigasecond/gigasecond_test.rb
+++ gigasecond/gigasecond_test.rb
@@ -4,34 +4,34 @@ require 'time'
 
 require_relative 'gigasecond'
 class GigasecondTest < Minitest::Unit::TestCase
-  def test_1_winter_time
-    gs = Gigasecond.from(Time.new(2011, 4, 25))
-    assert_equal Time.new(2043, 1, 1, 0 , 46, 40), gs
+  def test_1
+    gs = Gigasecond.from(Time.utc(2011, 4, 25))
+    assert_equal Time.utc(2043, 1, 1, 1, 46, 40), gs
   end
 
-  def test_2_winter_time
+  def test_2
     skip
-    gs = Gigasecond.from(Time.new(1977, 6, 13))
-    assert_equal Time.new(2009, 2, 19, 0, 46, 40), gs
+    gs = Gigasecond.from(Time.utc(1977, 6, 13))
+    assert_equal Time.utc(2009, 2, 19, 1, 46, 40), gs
   end
 
-  def test_3_summer_time
+  def test_3
     skip
-    gs = Gigasecond.from(Time.new(1959, 7, 19))
-    assert_equal Time.new(1991, 3, 27, 0, 46, 40), gs
+    gs = Gigasecond.from(Time.utc(1959, 7, 19))
+    assert_equal Time.utc(1991, 3, 27, 1, 46, 40), gs
   end
 
-  def test_4_summer_time_with_seconds
+  def test_4_with_seconds
     skip
-    gs = Gigasecond.from(Time.new(1959, 7, 19, 23, 59, 59))
-    assert_equal Time.new(1991, 3, 28, 0, 46, 39), gs
+    gs = Gigasecond.from(Time.utc(1959, 7, 19, 23, 59, 59))
+    assert_equal Time.utc(1991, 3, 28, 1, 46, 39), gs
   end
 
   # modify the test to test your 1 Gs anniversary
   def test_5_with_your_birthday
     skip
-    your_birthday = Time.new(year, month, day)
+    your_birthday = Time.utc(year, month, day)
     gs = Gigasecond.from(your_birthday)
-    assert_equal Time.new(2009, 1, 31), gs
+    assert_equal Time.utc(2009, 1, 31, 1, 46, 39), gs
   end
 end
