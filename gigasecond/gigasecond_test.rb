8e59fee68c74472399bae53ef55240e35e171e96
Benoit <benoit@hopsandfork.com>
2015-02-04 16:24:12 +0100

    Change test names to be easier to understand
    I had some problems when doing the exercice because of the DST. I think
    more explicit method's name should help.

--- gigasecond/gigasecond_test.rb
+++ gigasecond/gigasecond_test.rb
@@ -4,31 +4,31 @@ require 'time'
 
 require_relative 'gigasecond'
 class GigasecondTest < Minitest::Unit::TestCase
-  def test_1
+  def test_1_winter_time
     gs = Gigasecond.from(Time.new(2011, 4, 25))
     assert_equal Time.new(2043, 1, 1, 0 , 46, 40), gs
   end
 
-  def test_2
+  def test_2_winter_time
     skip
     gs = Gigasecond.from(Time.new(1977, 6, 13))
     assert_equal Time.new(2009, 2, 19, 0, 46, 40), gs
   end
 
-  def test_3
+  def test_3_summer_time
     skip
     gs = Gigasecond.from(Time.new(1959, 7, 19))
     assert_equal Time.new(1991, 3, 27, 0, 46, 40), gs
   end
 
-  def test_time_with_seconds
+  def test_4_summer_time_with_seconds
     skip
     gs = Gigasecond.from(Time.new(1959, 7, 19, 23, 59, 59))
     assert_equal Time.new(1991, 3, 28, 0, 46, 39), gs
   end
 
   # modify the test to test your 1 Gs anniversary
-  def test_yourself
+  def test_5_with_your_birthday
     skip
     your_birthday = Time.new(year, month, day)
     gs = Gigasecond.from(your_birthday)
