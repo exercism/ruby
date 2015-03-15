da617d5b7e98f3590f149a955eeeb9c45b0b452a
Martin Tournoij <martin@lico.nl>
2015-03-15 17:15:31 +0100

    Prevent people from using Ruby's Prime class to implement the nth-prime exercise
    See: Issue #95

--- nth-prime/example.rb
+++ nth-prime/example.rb
@@ -1,5 +1,3 @@
-require 'prime'
-
 class Prime
   def self.nth(n)
     if n < 1
@@ -11,8 +9,17 @@ class Prime
     i = 1
     while primes < n
       i += 1
-      primes += 1 if Prime.prime?(i)
+      primes += 1 if self.prime?(i)
     end
     i
+  end
+
+  def self.prime?(n)
+    return false if n == 1
+    return true if n == 2
+    (2..Math.sqrt(n).ceil).each do |i|
+      return false if n % i == 0
+    end
+    return true
   end
 end
