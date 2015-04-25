5c55cfa904a47c1390e09db05e0d4982a4df5893
r11runner <r11runner@gmail.com>
2015-04-25 16:56:48 +0200

    hamming; added test generator; raise ArgumentError if the two strands have different lengths

--- hamming/hamming_test.rb
+++ hamming/hamming_test.rb
@@ -8,48 +8,74 @@ rescue LoadError => e
 end
 
 class HammingTest < Minitest::Test
-  def test_no_difference_between_identical_strands
+
+  def test_identical_strands
     assert_equal 0, Hamming.compute('A', 'A')
   end
 
-  def test_complete_hamming_distance_of_single_nucleotide_strand
+  def test_long_identical_strands
+    skip
+    assert_equal 0, Hamming.compute('GGACTGA', 'GGACTGA')
+  end
+
+  def test_complete_distance_in_single_nucleotide_strands
     skip
     assert_equal 1, Hamming.compute('A', 'G')
   end
 
-  def test_complete_hamming_distance_of_small_strand
+  def test_complete_distance_in_small_strands
     skip
     assert_equal 2, Hamming.compute('AG', 'CT')
   end
 
-  def test_small_hamming_distance
+  def test_small_distance_in_small_strands
     skip
     assert_equal 1, Hamming.compute('AT', 'CT')
   end
 
-  def test_small_hamming_distance_in_longer_strand
+  def test_small_distance
     skip
     assert_equal 1, Hamming.compute('GGACG', 'GGTCG')
   end
 
-  def test_nonunique_characters_within_first_strand
+  def test_small_distance_in_long_strands
+    skip
+    assert_equal 2, Hamming.compute('ACCAGGG', 'ACTATGG')
+  end
+
+  def test_non_unique_character_in_first_strand
     skip
     assert_equal 1, Hamming.compute('AGA', 'AGG')
   end
 
-  def test_nonunique_characters_within_second_strand
+  def test_non_unique_character_in_second_strand
     skip
     assert_equal 1, Hamming.compute('AGG', 'AGA')
   end
 
-  def test_large_hamming_distance
+  def test_large_distance
     skip
     assert_equal 4, Hamming.compute('GATACA', 'GCATAA')
   end
 
-  def test_hamming_distance_in_very_long_strand
+  def test_large_distance_in_off_by_one_strand
     skip
     assert_equal 9, Hamming.compute('GGACGGATTCTG', 'AGGACGGATTCT')
+  end
+
+  def test_empty_strands
+    skip
+    assert_equal 0, Hamming.compute('', '')
+  end
+
+  def test_disallow_first_strand_longer
+    skip
+    assert_raises(ArgumentError){ Hamming.compute('AATG', 'AAA') }
+  end
+
+  def test_disallow_second_strand_longer
+    skip
+    assert_raises(ArgumentError){ Hamming.compute('ATA', 'AGTG') }
   end
 end
 
