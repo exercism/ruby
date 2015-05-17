218052183e750f8651d2dc9a3d641a581ecc2495
Katrina Owen <katrina.owen@gmail.com>
2015-05-17 10:12:26 -0700

    Clean up 'food-chain'
    - Normalize expected strings.
    - Replace instance methods with class methods.
    - Implement example as a module rather than a class.
    - Rename module to FoodChain instead of FoodChainSong.
    - Rename `sing` to `song` (noun for consistency with other methods).
    - Remove trailing newline on last line of each verse.
    - Add a VERSION constant for book-keeping purposes.
    - Replace logic with value in test for 'song'

--- food-chain/example.rb
+++ food-chain/example.rb
@@ -1,37 +1,37 @@
-class FoodChainSong
-  def sing
+module FoodChain
+  VERSION = 1
+
+  def self.song
     verses(1, 8)
   end
 
-  def verses(first, last)
-    first.upto(last).map do |number|
-      verse(number)
-    end.join("\n") + "\n"
+  def self.verses(lower_bound, upper_bound)
+    (lower_bound..upper_bound).map { |i| verse(i) }.join("\n\n")
   end
 
-  def verse(number)
+  def self.verse(number)
     case number
     when 1
       "I know an old lady who swallowed a fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+      "I don't know why she swallowed the fly. Perhaps she'll die."
     when 2
       "I know an old lady who swallowed a spider.\n" \
       "It wriggled and jiggled and tickled inside her.\n" \
       "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+      "I don't know why she swallowed the fly. Perhaps she'll die."
     when 3
       "I know an old lady who swallowed a bird.\n" \
       "How absurd to swallow a bird!\n" \
       "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
       "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+      "I don't know why she swallowed the fly. Perhaps she'll die."
     when 4
       "I know an old lady who swallowed a cat.\n" \
       "Imagine that, to swallow a cat!\n" \
       "She swallowed the cat to catch the bird.\n" \
       "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
       "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+      "I don't know why she swallowed the fly. Perhaps she'll die."
     when 5
       "I know an old lady who swallowed a dog.\n" \
       "What a hog, to swallow a dog!\n" \
@@ -39,7 +39,7 @@ class FoodChainSong
       "She swallowed the cat to catch the bird.\n" \
       "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
       "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+      "I don't know why she swallowed the fly. Perhaps she'll die."
     when 6
       "I know an old lady who swallowed a goat.\n" \
       "Just opened her throat and swallowed a goat!\n" \
@@ -48,7 +48,7 @@ class FoodChainSong
       "She swallowed the cat to catch the bird.\n" \
       "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
       "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+      "I don't know why she swallowed the fly. Perhaps she'll die."
     when 7
       "I know an old lady who swallowed a cow.\n" \
       "I don't know how she swallowed a cow!\n" \
@@ -58,10 +58,10 @@ class FoodChainSong
       "She swallowed the cat to catch the bird.\n" \
       "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
       "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+      "I don't know why she swallowed the fly. Perhaps she'll die."
     when 8
       "I know an old lady who swallowed a horse.\n" \
-      "She's dead, of course!\n"
+      "She's dead, of course!"
     end
   end
 end
--- food-chain/food_chain_test.rb
+++ food-chain/food_chain_test.rb
@@ -3,80 +3,77 @@ require_relative 'food_chain'
 
 # rubocop:disable Metrics/MethodLength, Metrics/LineLength
 class FoodChainTest < Minitest::Test
-  attr_reader :song
-  def song
-    @song = ::FoodChainSong.new
-  end
-
-  def teardown
-    @song = nil
-  end
-
   def test_fly
-    expected = "I know an old lady who swallowed a fly.\nI don't know why she swallowed the fly. Perhaps she'll die.\n"
-    assert_equal expected, song.verse(1)
+    expected = "" \
+      "I know an old lady who swallowed a fly.\n" \
+      "I don't know why she swallowed the fly. Perhaps she'll die."
+    assert_equal expected, FoodChain.verse(1)
   end
 
   def test_spider
     skip
-    expected = "I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\n" \
+    expected = "" \
+      "I know an old lady who swallowed a spider.\n" \
+      "It wriggled and jiggled and tickled inside her.\n" \
       "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
-    assert_equal expected, song.verse(2)
+      "I don't know why she swallowed the fly. Perhaps she'll die."
+    assert_equal expected, FoodChain.verse(2)
   end
 
   def test_bird
     skip
-    expected = "I know an old lady who swallowed a bird.\n" \
+    expected = "" \
+      "I know an old lady who swallowed a bird.\n" \
       "How absurd to swallow a bird!\n" \
       "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
       "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
-    assert_equal expected, song.verse(3)
+      "I don't know why she swallowed the fly. Perhaps she'll die."
+    assert_equal expected, FoodChain.verse(3)
   end
 
   def test_cat
     skip
-    expected = "I know an old lady who swallowed a cat.\n" \
+    expected = "" \
+      "I know an old lady who swallowed a cat.\n" \
       "Imagine that, to swallow a cat!\n" \
       "She swallowed the cat to catch the bird.\n" \
       "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
       "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. " \
-      "Perhaps she'll die.\n"
-    assert_equal expected, song.verse(4)
+      "I don't know why she swallowed the fly. Perhaps she'll die."
+    assert_equal expected, FoodChain.verse(4)
   end
 
   def test_dog
     skip
-    expected = "I know an old lady who swallowed a dog.\n" \
+    expected = "" \
+      "I know an old lady who swallowed a dog.\n" \
       "What a hog, to swallow a dog!\n" \
       "She swallowed the dog to catch the cat.\n" \
       "She swallowed the cat to catch the bird.\n" \
       "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
       "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. " \
-      "Perhaps she'll die.\n"
-    assert_equal expected, song.verse(5)
+      "I don't know why she swallowed the fly. Perhaps she'll die."
+    assert_equal expected, FoodChain.verse(5)
   end
 
   def test_goat
     skip
-    expected = "I know an old lady who swallowed a goat.\n" \
+    expected = "" \
+      "I know an old lady who swallowed a goat.\n" \
       "Just opened her throat and swallowed a goat!\n" \
       "She swallowed the goat to catch the dog.\n" \
       "She swallowed the dog to catch the cat.\n" \
       "She swallowed the cat to catch the bird.\n" \
       "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
       "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. " \
-      "Perhaps she'll die.\n"
-    assert_equal expected, song.verse(6)
+      "I don't know why she swallowed the fly. Perhaps she'll die."
+    assert_equal expected, FoodChain.verse(6)
   end
 
   def test_cow
     skip
-    expected = "I know an old lady who swallowed a cow.\n" \
+    expected = "" \
+      "I know an old lady who swallowed a cow.\n" \
       "I don't know how she swallowed a cow!\n" \
       "She swallowed the cow to catch the goat.\n" \
       "She swallowed the goat to catch the dog.\n" \
@@ -84,30 +81,93 @@ class FoodChainTest < Minitest::Test
       "She swallowed the cat to catch the bird.\n" \
       "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
       "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. " \
-      "Perhaps she'll die.\n"
-    assert_equal expected, song.verse(7)
+      "I don't know why she swallowed the fly. Perhaps she'll die."
+    assert_equal expected, FoodChain.verse(7)
   end
 
   def test_horse
     skip
-    expected = "I know an old lady who swallowed a horse.\n" \
-      "She's dead, of course!\n"
-    assert_equal expected, song.verse(8)
+    expected = "" \
+      "I know an old lady who swallowed a horse.\n" \
+      "She's dead, of course!"
+    assert_equal expected, FoodChain.verse(8)
   end
 
   def test_multiple_verses
     skip
-    expected = ''
-    expected << "I know an old lady who swallowed a fly.\nI don't know why she swallowed the fly. Perhaps she'll die.\n\n"
-    expected << "I know an old lady who swallowed a spider.\nIt wriggled and jiggled and tickled inside her.\n" \
-      "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die.\n\n"
-    assert_equal expected, song.verses(1, 2)
+    expected = <<-SONG.strip
+I know an old lady who swallowed a fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a spider.
+It wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+    SONG
+    assert_equal expected, FoodChain.verses(1, 2)
   end
 
   def test_the_whole_song
     skip
-    assert_equal song.verses(1, 8), song.sing
+    expected = <<-SONG.strip
+I know an old lady who swallowed a fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a spider.
+It wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a bird.
+How absurd to swallow a bird!
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a cat.
+Imagine that, to swallow a cat!
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a dog.
+What a hog, to swallow a dog!
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a goat.
+Just opened her throat and swallowed a goat!
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a cow.
+I don't know how she swallowed a cow!
+She swallowed the cow to catch the goat.
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+
+I know an old lady who swallowed a horse.
+She's dead, of course!
+    SONG
+    assert_equal expected, FoodChain.song
+  end
+
+  # this is some simple book-keeping to let people who are
+  # giving feedback know which version of the exercise you solved.
+  def test_version
+    skip
+    assert_equal 1, FoodChain::VERSION
   end
 end
