45236e3997368f8ee27e98e9e38bcbae4536f488
Victor Goff <keeperotphones@gmail.com>
2015-05-17 14:41:30 -0400

    Clean up food chain - continued
    re #132
    
    Continuing to normalize the strings and in so doing making the Heredoc
    the One Pattern here.  It is a little bit of overkill maybe for the
    first 2 or three tests, but soon makes sense to do this, May as well get
    them all in the same pattern.

--- food-chain/food_chain_test.rb
+++ food-chain/food_chain_test.rb
@@ -4,92 +4,100 @@ require_relative 'food_chain'
 # rubocop:disable Metrics/MethodLength, Metrics/LineLength
 class FoodChainTest < Minitest::Test
   def test_fly
-    expected = "" \
-      "I know an old lady who swallowed a fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die."
+    expected = <<-SONG.strip
+I know an old lady who swallowed a fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+    SONG
     assert_equal expected, FoodChain.verse(1)
   end
 
   def test_spider
     skip
-    expected = "" \
-      "I know an old lady who swallowed a spider.\n" \
-      "It wriggled and jiggled and tickled inside her.\n" \
-      "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die."
+expected = <<-SONG.strip
+I know an old lady who swallowed a spider.
+It wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+    SONG
     assert_equal expected, FoodChain.verse(2)
   end
 
   def test_bird
     skip
-    expected = "" \
-      "I know an old lady who swallowed a bird.\n" \
-      "How absurd to swallow a bird!\n" \
-      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
-      "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die."
+expected = <<-SONG.strip
+I know an old lady who swallowed a bird.
+How absurd to swallow a bird!
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+    SONG
     assert_equal expected, FoodChain.verse(3)
   end
 
   def test_cat
     skip
-    expected = "" \
-      "I know an old lady who swallowed a cat.\n" \
-      "Imagine that, to swallow a cat!\n" \
-      "She swallowed the cat to catch the bird.\n" \
-      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
-      "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die."
+expected = <<-SONG.strip
+I know an old lady who swallowed a cat.
+Imagine that, to swallow a cat!
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+    SONG
     assert_equal expected, FoodChain.verse(4)
   end
 
   def test_dog
     skip
-    expected = "" \
-      "I know an old lady who swallowed a dog.\n" \
-      "What a hog, to swallow a dog!\n" \
-      "She swallowed the dog to catch the cat.\n" \
-      "She swallowed the cat to catch the bird.\n" \
-      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
-      "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die."
+expected = <<-SONG.strip
+I know an old lady who swallowed a dog.
+What a hog, to swallow a dog!
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+    SONG
     assert_equal expected, FoodChain.verse(5)
   end
 
   def test_goat
     skip
-    expected = "" \
-      "I know an old lady who swallowed a goat.\n" \
-      "Just opened her throat and swallowed a goat!\n" \
-      "She swallowed the goat to catch the dog.\n" \
-      "She swallowed the dog to catch the cat.\n" \
-      "She swallowed the cat to catch the bird.\n" \
-      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
-      "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die."
+expected = <<-SONG.strip
+I know an old lady who swallowed a goat.
+Just opened her throat and swallowed a goat!
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+    SONG
     assert_equal expected, FoodChain.verse(6)
   end
 
   def test_cow
     skip
-    expected = "" \
-      "I know an old lady who swallowed a cow.\n" \
-      "I don't know how she swallowed a cow!\n" \
-      "She swallowed the cow to catch the goat.\n" \
-      "She swallowed the goat to catch the dog.\n" \
-      "She swallowed the dog to catch the cat.\n" \
-      "She swallowed the cat to catch the bird.\n" \
-      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
-      "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die."
+expected = <<-SONG.strip
+I know an old lady who swallowed a cow.
+I don't know how she swallowed a cow!
+She swallowed the cow to catch the goat.
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+    SONG
     assert_equal expected, FoodChain.verse(7)
   end
 
   def test_horse
     skip
-    expected = "" \
-      "I know an old lady who swallowed a horse.\n" \
-      "She's dead, of course!"
+expected = <<-SONG.strip
+I know an old lady who swallowed a horse.
+She's dead, of course!
+    SONG
     assert_equal expected, FoodChain.verse(8)
   end
 
