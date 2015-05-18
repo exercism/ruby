32452a1c504dda6bddbdbe4b1f56b1dfeb8333f1
Katrina Owen <katrina.owen@gmail.com>
2015-05-18 06:30:42 -0700

    Add instructions to food-chain.
    Since we're expecting people to write their own tests we need to remind them
    that this is possible.

--- food-chain/food_chain_test.rb
+++ food-chain/food_chain_test.rb
@@ -2,16 +2,23 @@ require 'minitest/autorun'
 
 require_relative 'food_chain'
 
-# rubocop:disable Metrics/MethodLength, Metrics/LineLength
 class FoodChainTest < Minitest::Test
+
+  # This test is an acceptance test.
+  #
+  # If you find it difficult to work the problem with so much
+  # output, go ahead and add a `skip`, and write whatever
+  # unit tests will help you. Then unskip it again
+  # to make sure you got it right.
+  # There's no need to submit the tests you write, unless you
+  # specifically want feedback on them.
   def test_the_whole_song
-    # Following line to accomodate automated testing
     song_file = File.expand_path('../song.txt', __FILE__)
     expected = IO.read(song_file)
     assert_equal expected, FoodChain.song
   end
 
-  # this is some simple book-keeping to let people who are
+  # This is some simple book-keeping to let people who are
   # giving feedback know which version of the exercise you solved.
   def test_version
     skip
