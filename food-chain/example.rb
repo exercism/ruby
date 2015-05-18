80e9555bf364e678e530f0eb53e718361b2b891c
Victor Goff <keeperotphones@gmail.com>
2015-05-17 22:31:55 -0400

    Clean strings -- continued
    re: #133
    
    This shows a test without heredoc, may be a good sample.  Also removes
    th intermediate tests to allow for the self-drive TDD suggested.
    Corrects a pretty core problem with newlines IMO.  And removes the
    `.strip` and added newine in the example code, each suggesting issues
    with forcing newlines and doing things in an unnatural way.
    
    Provides a text file with the full song, using that as the final test to
    show the "not clever" way to keep the tests concise, and clear, while
    hopefully not biasing a solution.

--- food-chain/example.rb
+++ food-chain/example.rb
@@ -6,62 +6,78 @@ module FoodChain
   end
 
   def self.verses(lower_bound, upper_bound)
-    (lower_bound..upper_bound).map { |i| verse(i) }.join("\n\n")
+    (lower_bound..upper_bound).map { |i| verse(i) }.join("\n")
   end
 
   def self.verse(number)
     case number
     when 1
-      "I know an old lady who swallowed a fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die."
+      <<-LINES
+I know an old lady who swallowed a fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+      LINES
     when 2
-      "I know an old lady who swallowed a spider.\n" \
-      "It wriggled and jiggled and tickled inside her.\n" \
-      "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die."
+      <<-LINES
+I know an old lady who swallowed a spider.
+It wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+      LINES
     when 3
-      "I know an old lady who swallowed a bird.\n" \
-      "How absurd to swallow a bird!\n" \
-      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
-      "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die."
+      <<-LINES
+I know an old lady who swallowed a bird.
+How absurd to swallow a bird!
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+      LINES
     when 4
-      "I know an old lady who swallowed a cat.\n" \
-      "Imagine that, to swallow a cat!\n" \
-      "She swallowed the cat to catch the bird.\n" \
-      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
-      "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die."
+      <<-LINES
+I know an old lady who swallowed a cat.
+Imagine that, to swallow a cat!
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+      LINES
     when 5
-      "I know an old lady who swallowed a dog.\n" \
-      "What a hog, to swallow a dog!\n" \
-      "She swallowed the dog to catch the cat.\n" \
-      "She swallowed the cat to catch the bird.\n" \
-      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
-      "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die."
+      <<-LINES
+I know an old lady who swallowed a dog.
+What a hog, to swallow a dog!
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+      LINES
     when 6
-      "I know an old lady who swallowed a goat.\n" \
-      "Just opened her throat and swallowed a goat!\n" \
-      "She swallowed the goat to catch the dog.\n" \
-      "She swallowed the dog to catch the cat.\n" \
-      "She swallowed the cat to catch the bird.\n" \
-      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
-      "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die."
+      <<-LINES
+I know an old lady who swallowed a goat.
+Just opened her throat and swallowed a goat!
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+      LINES
     when 7
-      "I know an old lady who swallowed a cow.\n" \
-      "I don't know how she swallowed a cow!\n" \
-      "She swallowed the cow to catch the goat.\n" \
-      "She swallowed the goat to catch the dog.\n" \
-      "She swallowed the dog to catch the cat.\n" \
-      "She swallowed the cat to catch the bird.\n" \
-      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
-      "She swallowed the spider to catch the fly.\n" \
-      "I don't know why she swallowed the fly. Perhaps she'll die."
+      <<-LINES
+I know an old lady who swallowed a cow.
+I don't know how she swallowed a cow!
+She swallowed the cow to catch the goat.
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.
+      LINES
     when 8
-      "I know an old lady who swallowed a horse.\n" \
-      "She's dead, of course!"
+      <<-LINES
+I know an old lady who swallowed a horse.
+She's dead, of course!
+      LINES
     end
   end
 end
