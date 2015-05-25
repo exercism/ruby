eb939825f4d1e7613886ab376180e8ebf85b5580
Katrina Owen <katrina.owen@gmail.com>
2015-05-25 17:10:23 -0600

    Simplify twelve-days
    This takes the same approach as food-chain, reducing the required API to a single
    module-level or class-level method, and moving the lyrics into a static file.
    
    Coincidentally, this fixes style violations ('long line') in the test suite.

--- twelve-days/example.rb
+++ twelve-days/example.rb
@@ -1,15 +1,17 @@
-class TwelveDaysSong
-  def sing
+module TwelveDays
+  VERSION = 1
+
+  def self.song
     verses(1, 12)
   end
 
-  def verses(first, last)
+  def self.verses(first, last)
     first.upto(last).map do |number|
       verse(number)
-    end.join("\n") + "\n"
+    end.join("\n")
   end
 
-  def verse(number)
+  def self.verse(number)
     case number
     when 1
       "On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree.\n"
