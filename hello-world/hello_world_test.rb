07a9d25834383448d3859cb76e629f23a99e1673
Victor Goff <keeperotphones@gmail.com>
2015-05-26 12:57:48 -0400

    Require Minitest gem, w/ explanatory error message

--- hello-world/hello_world_test.rb
+++ hello-world/hello_world_test.rb
@@ -1,6 +1,10 @@
-require 'minitest/autorun'
 begin
+gem 'minitest', '>= 5.0.0'
+require 'minitest/autorun'
   require_relative 'hello_world'
+rescue Gem::LoadError => e
+  puts "\n\n#{e.backtrace.first} #{e.message}"
+  puts "Minitest 5.0 gem must be installed for the xRuby track."
 rescue LoadError => e
   puts "\n\n#{e.backtrace.first} #{e.message}"
   puts DATA.read
