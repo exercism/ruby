ecdf7fb173ebcbc9fd7aeb03ad22b9a8ebdb6058
Victor Goff <keeperotphones@gmail.com>
2015-06-16 00:45:43 -0400

    No longer drives the solution
    Does not rely on on any specifically named 'helper' methods.  Tests
    things through the implementation via public facing API
    
    re: #10

--- largest-series-product/example.rb
+++ largest-series-product/example.rb
@@ -3,34 +3,37 @@
 class Series
   attr_reader :digits
   def initialize(numeric_string)
-    @digits = convert_to_digits(numeric_string)
+    @digits = numeric_string
   end
 
   def largest_product(length)
-    fail ArgumentError.new('Not enough digits') if length > digits.length
-    products = []
-    slices(length).each do |slice|
-      products << slice.inject(1) do |product, n|
-        product * n
-      end
-    end
-    products.sort.last
-  end
-
-  def slices(length)
-    result = []
-    i = -1
-    begin
-      i += 1
-      i2 = i + length - 1
-      result << digits[i..i2]
-    end while i2 < digits.size - 1
-    result
+    @length = length
+    fail ArgumentError.new('Not enough digits') if @length > digits.length
+    analyzer
   end
 
   private
 
-  def convert_to_digits(s)
-    s.chars.to_a.map(&:to_i)
+  def analyzer
+    collection_of_digits
+    reduce_to_product { validate { separate } }.max
+  end
+
+  def validate
+    yield.take_while { |array| array.size == @length }
+  end
+
+  def reduce_to_product
+    yield.map { |array| array.inject(1, :*) }
+  end
+
+  def separate
+    0.upto(digits.length - 1).map.with_index do |_, index|
+      digits[index, @length]
+    end
+  end
+
+  def collection_of_digits
+    @digits = digits.chars.map(&:to_i)
   end
 end
