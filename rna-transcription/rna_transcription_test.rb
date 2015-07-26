8eac25f65fcdbae4595204b0a48fb4cf35df4f62
Norbert Melzer <timmelzer@gmail.com>
2015-07-26 14:20:55 +0200

    removed trailing whitespace as reported by rubocop on travis

--- rna-transcription/rna_transcription_test.rb
+++ rna-transcription/rna_transcription_test.rb
@@ -62,17 +62,17 @@ class ComplementTest < Minitest::Test
     skip
     assert_raises(ArgumentError) { Complement.of_rna('T') }
   end
-  
+
   def test_rna_raises_argument_error_on_completely_invalid_input
     skip
     assert_raises(ArgumentError) { Complement.of_rna('XXX') }
   end
-  
+
   def tes_dna_raises_argument_error_on_completely_invalid_input
     skip
     assert_raises(ArgumentError) { Complement.of_dna('XXX') }
   end
-  
+
   # This test is for the sake of people providing feedback, so they
   # know which version of the exercise you are solving.
   def test_bookkeeping
