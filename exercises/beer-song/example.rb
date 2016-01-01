019f65469a8f8247fe47deae8757425765970789
Katrina Owen <katrina.owen@gmail.com>
2016-01-01 14:35:48 -0700

    improve beer song API and tests
    The final beer-song test was not actually testing anything.
    This is much more straight-forward, asserting that the entire song's
    output matches the expected lyrics.

--- exercises/beer-song/example.rb
+++ exercises/beer-song/example.rb
@@ -1,18 +1,21 @@
 class BeerSong
-  def sing
+  VERSION = 2
+
+  def lyrics
     verses(99, 0)
   end
 
-  def verses(starting, ending)
-    (ending..starting).to_a.reverse.map { |n| verse(n) }.join("\n") + "\n"
+  def verses(upper_bound, lower_bound)
+    upper_bound.downto(lower_bound).map { |i| verse(i) }.join("\n")
   end
 
   def verse(number)
-    if number == 0
+    case number
+    when 0
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    elsif number == 1
+    when 1
       "%s bottle of beer on the wall, %s bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n" % [number, number]
-    elsif number == 2
+    when 2
       "%s bottles of beer on the wall, %s bottles of beer.\nTake one down and pass it around, %s bottle of beer on the wall.\n" % [number, number, number - 1]
     else
       "%s bottles of beer on the wall, %s bottles of beer.\nTake one down and pass it around, %s bottles of beer on the wall.\n" % [number, number, number - 1]
