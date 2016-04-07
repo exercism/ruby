df120d6389b9ea692c84b23169c990f0e8e5a4f7
callahat <tim.callahan25@yahoo.com>
2016-04-06 23:03:49 -0400

    Test for name collisions
    This commit adds a test which checks that the Robot class handles the case
    when the first name initially chosen has already been taken (otherwise known
    as a name collision).
    Also added a version number check which was previously missing.

--- exercises/robot-name/robot_name_test.rb
+++ exercises/robot-name/robot_name_test.rb
@@ -6,6 +6,9 @@ require 'minitest/autorun'
 require_relative 'robot_name'
 
 class RobotTest < Minitest::Test
+  DIFFERENT_ROBOT_NAME_SEED = 1234
+  SAME_INITIAL_ROBOT_NAME_SEED = 1000
+
   COMMAND_QUERY = <<-MSG.freeze
     Command/Query Separation:
     Query methods should generally not change object state.
@@ -26,11 +29,13 @@ class RobotTest < Minitest::Test
 
   def test_different_robots_have_different_names
     skip
+    Kernel.srand DIFFERENT_ROBOT_NAME_SEED
     refute_equal Robot.new.name, Robot.new.name
   end
 
   def test_reset_name
     skip
+    Kernel.srand DIFFERENT_ROBOT_NAME_SEED
     robot = Robot.new
     name = robot.name
     robot.reset
@@ -38,5 +43,19 @@ class RobotTest < Minitest::Test
     assert name != name2
     assert_equal name2, robot.name, COMMAND_QUERY
     assert_match NAME_REGEXP, name2
+  end
+
+  def test_different_name_when_chosen_name_is_taken
+    skip
+    Kernel.srand SAME_INITIAL_ROBOT_NAME_SEED
+    name1 = Robot.new.name
+    Kernel.srand SAME_INITIAL_ROBOT_NAME_SEED
+    name2 = Robot.new.name
+    refute_equal name1, name2
+  end
+
+  def test_version
+    skip
+    assert_equal 1, Robot::VERSION
   end
 end
