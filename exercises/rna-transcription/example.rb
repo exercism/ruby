60c0080054daec54e99fa76a5a8fe4e92ef9c3f9
Cohen Carlisle <cohen.carlisle@gmail.com>
2016-05-25 21:19:18 -0400

    update rna transcription

--- exercises/rna-transcription/example.rb
+++ exercises/rna-transcription/example.rb
@@ -1,18 +1,9 @@
 module BookKeeping
-  VERSION = 3
+  VERSION = 4
 end
 
-class Complement
+module Complement
   def self.of_dna(strand)
-    DNA.new(strand).tr('CGTA', 'GCAU')
-  end
-end
-
-class DNA < String
-  def initialize strand
-    strand.chars.map do |n|
-      raise ArgumentError, "Illegal DNA Nucleotide: #{n}" unless ['G', 'C', 'T', 'A'].include? n
-    end
-    super
+    strand =~ /[^CGTA]/ ? '' : strand.tr('CGTA', 'GCAU')
   end
 end
