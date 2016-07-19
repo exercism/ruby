e672bbe32c1fdbfa9553225fc3664cf97db79d51
Victor Goff <keeperotphones@gmail.com>
2016-07-19 15:57:29 -0400

    Style checks

--- exercises/connect/connect_test.rb
+++ exercises/connect/connect_test.rb
@@ -9,83 +9,83 @@ class ConnectTest < Minitest::Test
   def test_an_empty_board_has_no_winner
     # skip
     board = [
-      ". . . . .",
-      ". . . . .",
-      ". . . . .",
-      ". . . . .",
-      ". . . . ."
+      '. . . . .',
+      '. . . . .',
+      '. . . . .',
+      '. . . . .',
+      '. . . . .'
     ]
     game = Board.new(board)
-    assert_equal "", game.winner, "an empty board has no winner"
+    assert_equal '', game.winner, 'an empty board has no winner'
   end
 
   def test_only_edges_does_not_make_a_winner
     skip
     board = [
-      ". . . . .",
-      ". . . . X",
-      ". O . X .",
-      ". . O . .",
-      ". . . O ."
+      '. . . . .',
+      '. . . . X',
+      '. O . X .',
+      '. . O . .',
+      '. . . O .'
     ]
     game = Board.new(board)
-    assert_equal "", game.winner, "only edges does not make a winner"
+    assert_equal '', game.winner, 'only edges does not make a winner'
   end
 
   def test_rectangle_black___black_wins
     skip
     board = [
-      ". O . .",
-      "O X X X",
-      "O X O .",
-      "X X O X",
-      ". O X ."
+      '. O . .',
+      'O X X X',
+      'O X O .',
+      'X X O X',
+      '. O X .'
     ]
     game = Board.new(board)
-    assert_equal "X", game.winner, "Rectangle black - black wins"
+    assert_equal 'X', game.winner, 'Rectangle black - black wins'
   end
 
   def test_rectangle_white___white_wins
     skip
     board = [
-      ". O . .",
-      "O X X X",
-      "O O O .",
-      "X X O X",
-      ". O X ."
+      '. O . .',
+      'O X X X',
+      'O O O .',
+      'X X O X',
+      '. O X .'
     ]
     game = Board.new(board)
-    assert_equal "O", game.winner, "Rectangle white - white wins"
+    assert_equal 'O', game.winner, 'Rectangle white - white wins'
   end
 
   def test_convoluted_path_black_wins
     skip
     board = [
-      ". X X . .",
-      "X . X . X",
-      ". X . X .",
-      ". X X . .",
-      "O O O O O"
+      '. X X . .',
+      'X . X . X',
+      '. X . X .',
+      '. X X . .',
+      'O O O O O'
     ]
     game = Board.new(board)
-    assert_equal "X", game.winner, "Convoluted path black wins"
+    assert_equal 'X', game.winner, 'Convoluted path black wins'
   end
 
   def test_aspiral_black_wins
     skip
     board = [
-      "O X X X X X X X X",
-      "O X O O O O O O O",
-      "O X O X X X X X O",
-      "O X O X O O O X O",
-      "O X O X X X O X O",
-      "O X O O O X O X O",
-      "O X X X X X O X O",
-      "O O O O O O O X O",
-      "X X X X X X X X O"
+      'O X X X X X X X X',
+      'O X O O O O O O O',
+      'O X O X X X X X O',
+      'O X O X O O O X O',
+      'O X O X X X O X O',
+      'O X O O O X O X O',
+      'O X X X X X O X O',
+      'O O O O O O O X O',
+      'X X X X X X X X O'
     ]
     game = Board.new(board)
-    assert_equal "X", game.winner, "Aspiral black wins"
+    assert_equal 'X', game.winner, 'Aspiral black wins'
   end
 
   # Problems in exercism evolve over time, as we find better ways to ask
