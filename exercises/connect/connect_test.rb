a3e1e78054f4ddd957e4b8d893e77ef5c65c605f
Jônatas Davi Paganini <jonatasdp@gmail.com>
2016-07-19 11:17:24 -0300

    generated example.

--- exercises/connect/connect_test.rb
+++ exercises/connect/connect_test.rb
@@ -3,76 +3,89 @@ gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
 require_relative 'connect'
 
+# Test data version:
+# 8268727
 class ConnectTest < Minitest::Test
-  EMPTY = [
-    '. . . . .',
-    '. . . . .',
-    '. . . . .',
-    '. . . . .',
-    ' . . . . .'
-  ].freeze
-
-  EDGES_POPULATED_BUT_NO_WINNER = [
-    '. . . . .',
-    '. . . . X',
-    '. O . X .',
-    '. . O . .',
-    '. . . O .'
-  ].freeze
-
-  RECTANGLE_BLACK = [
-    '. O . .',
-    'O X X X',
-    'O X O .',
-    'X X O X',
-    '. O X .'
-  ].freeze
-
-  RECTANGLE_WHITE = [
-    '. O . .',
-    'O X X X',
-    'O O O .',
-    'X X O X',
-    '. O X .'
-  ].freeze
-
-  CONVOLUTED_PATH = [
-    '. X X . .',
-    'X . X . X',
-    '. X . X .',
-    '. X X . .',
-    'O O O O O'
-  ].freeze
-
-  ASPIRAL = [
-    'O X X X X X X X X',
-    'O X O O O O O O O',
-    'O X O X X X X X O',
-    'O X O X O O O X O',
-    'O X O X X X O X O',
-    'O X O O O X O X O',
-    'O X X X X X O X O',
-    'O O O O O O O X O',
-    'X X X X X X X X O'
-  ].freeze
-
-  TEST_CASES = [
-    [EMPTY, '', 'an empty board has no winner'],
-    [EDGES_POPULATED_BUT_NO_WINNER, '', 'only edges does not make a winner'],
-    [['X'], 'X', 'a one cell board with a X should get the winner easy'],
-    [['O'], 'O', 'a one cell board with a O should get the winner easy'],
-    [[' . '], '', 'a one cell board empty has no winner'],
-    [CONVOLUTED_PATH, 'X', 'Convoluted path x wins'],
-    [RECTANGLE_BLACK, 'X', 'Rectangle black - black wins'],
-    [RECTANGLE_WHITE, 'O', 'Rectangle white - white wins'],
-    [ASPIRAL, 'X', 'Aspiral black wins']
-  ].freeze
-
-  TEST_CASES.each do |game, expected_result, error_message|
-    define_method("test_#{error_message.tr(' ', '_')}") do
-      board = Board.new game
-      msg = "#{error_message}\nCurrent board: \n #{board}"
-      assert_equal expected_result, board.winner, msg
-    end
+  def test_an_empty_board_has_no_winner
+    # skip
+    board = [
+      ". . . . .",
+      ". . . . .",
+      ". . . . .",
+      ". . . . .",
+      ". . . . ."
+    ]
+    game = Board.new(board)
+    assert_equal "", game.winner, "an empty board has no winner"
   end
+
+  def test_only_edges_does_not_make_a_winner
+    skip
+    board = [
+      ". . . . .",
+      ". . . . X",
+      ". O . X .",
+      ". . O . .",
+      ". . . O ."
+    ]
+    game = Board.new(board)
+    assert_equal "", game.winner, "only edges does not make a winner"
+  end
+
+  def test_rectangle_black___black_wins
+    skip
+    board = [
+      ". O . .",
+      "O X X X",
+      "O X O .",
+      "X X O X",
+      ". O X ."
+    ]
+    game = Board.new(board)
+    assert_equal "X", game.winner, "Rectangle black - black wins"
+  end
+
+  def test_rectangle_white___white_wins
+    skip
+    board = [
+      ". O . .",
+      "O X X X",
+      "O O O .",
+      "X X O X",
+      ". O X ."
+    ]
+    game = Board.new(board)
+    assert_equal "O", game.winner, "Rectangle white - white wins"
+  end
+
+  def test_convoluted_path_black_wins
+    skip
+    board = [
+      ". X X . .",
+      "X . X . X",
+      ". X . X .",
+      ". X X . .",
+      "O O O O O"
+    ]
+    game = Board.new(board)
+    assert_equal "X", game.winner, "Convoluted path black wins"
+  end
+
+  def test_aspiral_black_wins
+    skip
+    board = [
+      "O X X X X X X X X",
+      "O X O O O O O O O",
+      "O X O X X X X X O",
+      "O X O X O O O X O",
+      "O X O X X X O X O",
+      "O X O O O X O X O",
+      "O X X X X X O X O",
+      "O O O O O O O X O",
+      "X X X X X X X X O"
+    ]
+    game = Board.new(board)
+    assert_equal "X", game.winner, "Aspiral black wins"
+  end
+
 end
