883cf906fe41d862e6738c61737c6aa8ff92f01d
Jônatas Davi Paganini <jonatasdp@gmail.com>
2016-07-19 11:18:24 -0300

    adding generator for connect cases from json file

---
+++ lib/connect_cases.rb
@@ -0,0 +1,34 @@
+class ConnectCase < OpenStruct
+  def name
+    'test_%s' % description
+      .gsub(/[() -]/, '_')
+      .chomp('_')
+      .downcase
+  end
+
+  def test_body
+    [
+      "board = [",
+      "  "+board.map(&:inspect).join(",\n      "),
+      "]",
+      "game = Board.new(board)",
+      "assert_equal #{expected.inspect}, game.winner, #{ description.inspect}"
+    ]
+  end
+
+  def skipped
+    index.zero? ? '# skip' : 'skip'
+  end
+end
+
+ConnectCases = proc do |data|
+  i = 0
+  json = JSON.parse(data)
+  cases = []
+  json['cases'].each do |row|
+    row = row.merge(row.merge('index' => i))
+    cases << ConnectCase.new(row)
+    i += 1
+  end
+  cases
+end
