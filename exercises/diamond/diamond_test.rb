3231f88491895960e4dea2e35298b3094e8c518d
leonardo <l.castillo1989@Hotmail.com>
2016-08-02 14:08:35 -0400

    rubocop friendly diamond minitest and solution

---
+++ exercises/diamond/diamond_test.rb
@@ -0,0 +1,43 @@
+# rubocop:disable Metrics/MethodLength
+# !/usr/bin/env ruby
+gem 'minitest', '>= 5.0.0'
+require 'minitest/autorun'
+require_relative 'diamond'
+
+class DiamondTest < Minitest::Test
+  def test_letter_a
+    answer = Diamond.make_diamond('A')
+    assert_equal "A\n", answer
+  end
+
+  def test_letter_c
+    skip
+    answer = Diamond.make_diamond('C')
+    string = "  A  \n"\
+             " B B \n"\
+             "C   C\n"\
+             " B B \n"\
+             "  A  \n"
+    assert_equal string, answer
+  end
+
+  def test_letter_e
+    skip
+    answer = Diamond.make_diamond('E')
+    string = "    A    \n"\
+             "   B B   \n"\
+             "  C   C  \n"\
+             " D     D \n"\
+             "E       E\n"\
+             " D     D \n"\
+             "  C   C  \n"\
+             "   B B   \n"\
+             "    A    \n"
+    assert_equal string, answer
+  end
+
+  def test_bookkeeping
+    skip
+    assert_equal 1, Bookkeeping::VERSION
+  end
+end
