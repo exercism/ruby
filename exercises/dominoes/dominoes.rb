50278bca04ebdd7afcfa6a6ac7c7999c664da7c1
seanreed1111 <seanreed1111@gmail.com>
2016-08-31 10:41:46 -0400

    refactors can_chain? using Tute's suggestions

--- exercises/dominoes/dominoes.rb
+++ exercises/dominoes/dominoes.rb
@@ -4,14 +4,8 @@ class Dominoes
   def can_chain?(dominoes_list)
     result = false
     
-    return true if dominoes_list.length == 0 # empty list
-
-    if ((dominoes_list.length == 1) && (dominoes_list[0][0] == dominoes_list[0][1])) #singletons
-      return true
-    else
-      return false
-    end
-
+    return true if dominoes_list.length.zero? # empty list
+    return (dominoes_list[0][0] == dominoes_list[0][1]) if dominoes_list.length == 1 #singletons
 
     result
   end
