51e5025aa05afff97d25eeec08fe178404124b0d
Charlie Cook <turingbombe@gmail.com>
2016-09-08 17:37:33 -0400

    Added iterations

--- exercises/sublist/sublist_test.rb
+++ exercises/sublist/sublist_test.rb
@@ -7,13 +7,30 @@ class SublistTest < Minitest::Test
   SUPERLIST = 1
   EQUAL = 2
   UNEQUAL = 3
-  
+
   def test_empty_lists
     assert_equal EQUAL, Sublist.new.check_lists([],[])
   end
 
+  def test_empty_list_within
+    assert_equal SUBLIST, Sublist.new.check_lists([], [1,2,3])
+  end
 
+  def test_within_empty_list
+    assert_equal SUPERLIST, Sublist.new.check_lists([1,2,3], [])
+  end
 
+  def test_list_equals_itself
+    assert_equal EQUAL, Sublist.new.check_lists([1,2,3], [1,2,3])
+  end
+
+  def test_different_lists
+    assert_equal UNEQUAL, Sublist.new.check_lists([1,2,3], [2,3,4])
+  end
+
+  def test_consecutive
+    assert_equal SUBLIST, Sublist.new.check_lists([1, 1, 2], [0, 1, 1, 1, 2, 1, 2])
+  end
 
 
 
