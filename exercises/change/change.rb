4e99b92d06fee9acc01b42800f65144dcb5904a7
laushinka <laurie.malau@gmail.com>
2016-09-19 18:55:56 -0400

    Implement code to come up with coin combination for a supply of coins. Pending implementation for shortest combination.

--- exercises/change/change.rb
+++ exercises/change/change.rb
@@ -1,37 +1,38 @@
+require 'byebug'
 class Change
   class << self
-    def generate(amount, coins)
-      # write the algorithm
-      # Given an infinite supply of coins with different values
-      # find the smallest number of coins needed to make a desired
-      # amount of change.
-      # m = [0] * (amount + 1)
-      # m[0] = 1
+    def generate(amount, coins_supply)
+      # Given a supply of coins with different values
+      # Keep track of possible coins combinations
+      # Go through the supply of coins
+        # Keep track of remaining coins
+        # Keep track of the remaining amount
+        # If the remaining amount divided by coin is bigger than 0,
+          # Divide the remaining amount by coin as x,
+          # Store the coin x number of times into the remaining coins arr
+          # The remaining amount is original amount substracted by the total number of remaining coins
+      # Return coins
+      # Check if coins combination length is longer or shorter than the previous one
+        # If shorter, return that one
+        # If longer, return the previous one
+      available_coins = coins_supply.sort {|a, b| b <=> a}
+      coins = []
+      remaining_amount = amount
 
-      m = [[0] * (coins.length + 1), [0] * (amount + 1)]
-      for i in (0...amount + 1)
-        m[0][i] = i
-      end
-      m
-
-      for c in (1...coins.length + 1)
-        for r in (1...amount + 1)
-          if coins[c - 1] == r
-            m[c][r] = 1
-          elsif coins[c - 1] > r
-            m[c][r] = m[c - 1][r]
-          else
-            m[c][r] = [m[c - 1][r], 1 + m[c][r - coins[c - 1]]].min
+      available_coins.each do |coin|
+        if (remaining_amount/coin).to_i > 0
+          ((remaining_amount/coin).to_i).times do
+            coins << coin
+            debugger
           end
         end
+        if coins.length > 0
+          remaining_amount = amount - coins.inject(:+)
+        elsif coins.length == 0
+          remaining_amount = amount
+        end
       end
-      return m[-1][-1]
+      coins.sort
     end
   end
 end
-
-
-# 1) there is one coin equal to the desired change in the array
-#   if so, return coin
-# 2) there are not enough coins to provide accurate change
-# 3) there will require n amount of coins to equal change (the whole array)
