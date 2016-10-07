c9a2b1cf36f136966b40eb915ff49d2e356ab3cd
Bryan Mulvihill <mulvihill@informz.com>
2016-10-07 16:04:33 -0400

    Add acronym generator

--- exercises/acronym/acronym_test.rb
+++ exercises/acronym/acronym_test.rb
@@ -3,34 +3,58 @@ gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
 require_relative 'acronym'
 
+# Test data version:
+# 5b5e807
+# rubocop:disable Metrics/LineLength
 class AcronymTest < Minitest::Test
-  ACRONYM_PAIRS =
-    {
-      'Portable Network Graphics' => 'PNG',
-      'Ruby on Rails' => 'ROR',
-      'HyperText Markup Language' => 'HTML',
-      'First In, First Out' => 'FIFO',
-      'PHP: Hypertext Preprocessor' => 'PHP',
-      'Complementary metal-oxide semiconductor' => 'CMOS'
-    }.freeze
-
-  def test_acronyms
-    ACRONYM_PAIRS.each do |given, expected|
-      assert_equal expected, Acronym.abbreviate(given), <<-MSG
-        The acronym of '#{given}' should be '#{expected}'.
-      MSG
-    end
+  def test_basic
+    assert_equal 'PNG', Acronym.abbreviate('Portable Network Graphics')
   end
 
-  # Problems in exercism evolve over time,
-  # as we find better ways to ask questions.
+  def test_lowercase_words
+    skip
+    assert_equal 'ROR', Acronym.abbreviate('Ruby on Rails')
+  end
+
+  def test_camelcase
+    skip
+    assert_equal 'HTML', Acronym.abbreviate('HyperText Markup Language')
+  end
+
+  def test_punctuation
+    skip
+    assert_equal 'FIFO', Acronym.abbreviate('First In, First Out')
+  end
+
+  def test_all_caps_words
+    skip
+    assert_equal 'PHP', Acronym.abbreviate('PHP: Hypertext Preprocessor')
+  end
+
+  def test_hyphenated
+    skip
+    assert_equal 'CMOS', Acronym.abbreviate('Complementary metal-oxide semiconductor')
+  end
+
+  # Problems in exercism evolve over time, as we find better ways to ask
+  # questions.
   # The version number refers to the version of the problem you solved,
   # not your solution.
   #
-  # Define a constant named VERSION inside of BookKeeping.
+  # Define a constant named VERSION inside of the top level BookKeeping
+  # module, which may be placed near the end of your file.
+  #
+  # In your file, it will look like this:
+  #
+  # module BookKeeping
+  #   VERSION = 1 # Where the version number matches the one in the test.
+  # end
+  #
   # If you are curious, read more about constants on RubyDoc:
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
-  def test_version
-    assert_equal 1, BookKeeping::VERSION
+
+  def test_bookkeeping
+    skip
+    assert_equal 2, BookKeeping::VERSION
   end
 end
---
+++ lib/acronym_cases.rb
@@ -0,0 +1,19 @@
+class AcronymCase < OpenStruct
+  def name
+    'test_%s' % description.tr(' ', '_')
+  end
+
+  def assertion
+    "Acronym.abbreviate('#{phrase}')"
+  end
+
+  def skipped?
+    index > 0
+  end
+end
+
+AcronymCases = proc do |data|
+  JSON.parse(data)['abbreviate']['cases'].map.with_index do |row, i|
+    AcronymCase.new(row.merge('index' => i))
+  end
+end
