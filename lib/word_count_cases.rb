37fa3b4f4c9a25587bb32cc7c0c63d4d76bf74af
Pablo Vicente <vicapa99@gmail.com>
2016-10-19 23:18:47 +0200

    Add word count generator (#448)
    * Generate word count test cases
    
    * Interpolate and escape quotes
    
    * Keep escaped characters for interpolation
    
    * Use inspect instead of to_json

---
+++ lib/word_count_cases.rb
@@ -0,0 +1,19 @@
+class WordCountCase < OpenStruct
+  def name
+    'test_%s' % description.tr(' ', '_')
+  end
+
+  def object_under_test
+    %Q(Phrase.new(#{input.inspect}))
+  end
+
+  def skipped?
+    index.nonzero?
+  end
+end
+
+WordCountCases = proc do |data|
+  JSON.parse(data)['cases'].map.with_index do |row, i|
+    WordCountCase.new(row.merge('index' => i))
+  end
+end
