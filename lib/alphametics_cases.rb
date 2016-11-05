5ac769ececd70ac4463fda1a28fa937f493c65c2
Geoff Hubbard <Insti@users.noreply.github.com>
2016-11-05 10:53:57 +0100

    [alphametics] Warn about long running tests in the common test data. (#469)
    Refactored lib/alphametics_cases.rb.
    Use the common generator method names.
    Tidied up expected value string generation code.
    Add warning comment to potentially slow test cases.
    
    Re-generate tests based on version based on canonical-data.json
    
    Update `example.rb` using a fast solver that uses partials to limit the set of viable permutations.

--- lib/alphametics_cases.rb
+++ lib/alphametics_cases.rb
@@ -1,30 +1,66 @@
 class AlphameticsCase < OpenStruct
-  PAIRS_PER_LINE = 4
-  SPACE = ->(num) { ' ' * num }
-
   def test_name
     "test_#{description.tr(' ', '_')}"
-  end
-
-  def work_load
-    "Alphametics.new.solve('#{puzzle}')"
-  end
-
-  def expect
-    return 'nil' if expected.nil?
-    expected_values
-  end
-
-  def expected_values
-    "{\n" << expected.each_slice(PAIRS_PER_LINE).map do |pairs|
-      '%s'.prepend(SPACE[6]) %
-      pairs.map { |k, v| "'#{k}' => #{v}" }.join(', ')
-    end.join(",\n") << "\n    }"
   end
 
   def skipped
     index.zero? ? '# skip' : 'skip'
   end
+
+  def input
+    "'#{puzzle}'"
+  end
+
+  def expect
+    expected.nil? ? {} : expected_values
+  end
+
+  def workload
+    body = 
+      "input = %s\n" % input,
+      "expected = %s\n" % expect,
+      "assert_equal expected, Alphametics.solve(input)"
+    indent(body,4)
+  end
+
+  def runtime_comment
+    if slow?
+      comments = 
+        '',
+        "# The obvious algorithm can take a long time to solve this puzzle,\n",
+        "# but an optimised solution can solve it fairly quickly.\n",
+        "# (It's OK to submit your solution without getting this test to pass.)\n"
+      indent(comments,2)
+    end
+  end
+
+  private
+
+  def slow?
+    (expected||{}).size > 7 
+  end
+
+  def expected_values
+    "{ #{indent(expected_values_as_lines,17)} }"
+  end
+
+  def expected_values_as_lines
+    lines = expected_values_as_strings.each_slice(4).map { |line| line.join(', ') }
+    add_trailing_comma_and_newline(lines)
+  end
+
+  def expected_values_as_strings
+    expected.sort.map { |(key,value)| "'#{key}' => #{value}" }
+  end
+
+  def add_trailing_comma_and_newline(lines)
+    lines[0...-1].map { |line| "#{line},\n" }.push(lines.last)
+  end
+
+  def indent(lines, spaces)
+    lines.join(' ' * spaces)
+  end
+
 end
 
 AlphameticsCases = proc do |data|
