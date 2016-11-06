34df08fe35361318a23c548dc12076bc1f1f08d3
Geoff Hubbard <ghub@instihost.com>
2016-11-06 19:29:49 +0000

    Pangram: Add failure messages to tests
    Also:
    Refactored generator.
    Renamed generator methods to match common interface.
    Updated tests to match the xcommon canonical-data.json
    Renamed method `pangram?` to match Ruby style.

--- exercises/pangram/pangram_test.rb
+++ exercises/pangram/pangram_test.rb
@@ -4,61 +4,66 @@ gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
 require_relative 'pangram'
 
-# Test data version:
-# deb225e Implement canonical dataset for scrabble-score problem (#255)
-
+# Test data version: # 2adfe21
 class PangramTest < Minitest::Test
   def test_sentence_empty
-    str = ''
-    refute Pangram.is_pangram?(str)
+    # skip
+    phrase = ''
+    refute Pangram.pangram?(phrase), "#{phrase.inspect} is NOT a pangram"
   end
 
   def test_pangram_with_only_lower_case
     skip
-    str = 'the quick brown fox jumps over the lazy dog'
-    assert Pangram.is_pangram?(str)
+    phrase = 'the quick brown fox jumps over the lazy dog'
+    assert Pangram.pangram?(phrase), "#{phrase.inspect} IS a pangram"
   end
 
   def test_missing_character_x
     skip
-    str = 'a quick movement of the enemy will jeopardize five gunboats'
-    refute Pangram.is_pangram?(str)
+    phrase = 'a quick movement of the enemy will jeopardize five gunboats'
+    refute Pangram.pangram?(phrase), "#{phrase.inspect} is NOT a pangram"
   end
 
   def test_another_missing_character_x
     skip
-    str = 'the quick brown fish jumps over the lazy dog'
-    refute Pangram.is_pangram?(str)
+    phrase = 'the quick brown fish jumps over the lazy dog'
+    refute Pangram.pangram?(phrase), "#{phrase.inspect} is NOT a pangram"
   end
 
   def test_pangram_with_underscores
     skip
-    str = 'the_quick_brown_fox_jumps_over_the_lazy_dog'
-    assert Pangram.is_pangram?(str)
+    phrase = 'the_quick_brown_fox_jumps_over_the_lazy_dog'
+    assert Pangram.pangram?(phrase), "#{phrase.inspect} IS a pangram"
   end
 
   def test_pangram_with_numbers
     skip
-    str = 'the 1 quick brown fox jumps over the 2 lazy dogs'
-    assert Pangram.is_pangram?(str)
+    phrase = 'the 1 quick brown fox jumps over the 2 lazy dogs'
+    assert Pangram.pangram?(phrase), "#{phrase.inspect} IS a pangram"
   end
 
   def test_missing_letters_replaced_by_numbers
     skip
-    str = '7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog'
-    refute Pangram.is_pangram?(str)
+    phrase = '7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog'
+    refute Pangram.pangram?(phrase), "#{phrase.inspect} is NOT a pangram"
   end
 
   def test_pangram_with_mixed_case_and_punctuation
     skip
-    str = '"Five quacking Zephyrs jolt my wax bed."'
-    assert Pangram.is_pangram?(str)
+    phrase = '"Five quacking Zephyrs jolt my wax bed."'
+    assert Pangram.pangram?(phrase), "#{phrase.inspect} IS a pangram"
   end
 
   def test_pangram_with_non_ascii_characters
     skip
-    str = 'Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.'
-    assert Pangram.is_pangram?(str)
+    phrase = 'Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.'
+    assert Pangram.pangram?(phrase), "#{phrase.inspect} IS a pangram"
+  end
+
+  def test_panagram_in_alphabet_other_than_ascii
+    skip
+    phrase = 'Широкая электрификация южных губерний даст мощный толчок подъёму сельского хозяйства.'
+    refute Pangram.pangram?(phrase), "#{phrase.inspect} is NOT a pangram"
   end
 
   # Problems in exercism evolve over time, as we find better ways to ask
@@ -67,8 +72,9 @@ class PangramTest < Minitest::Test
   # not your solution.
   #
   # Define a constant named VERSION inside of the top level BookKeeping
-  # module.
-  #  In your file, it will look like this:
+  # module, which may be placed near the end of your file.
+  #
+  # In your file, it will look like this:
   #
   # module BookKeeping
   #   VERSION = 1 # Where the version number matches the one in the test.
@@ -76,9 +82,8 @@ class PangramTest < Minitest::Test
   #
   # If you are curious, read more about constants on RubyDoc:
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
-
   def test_bookkeeping
     skip
-    assert_equal 2, BookKeeping::VERSION
+    assert_equal 3, BookKeeping::VERSION
   end
 end
--- lib/pangram_cases.rb
+++ lib/pangram_cases.rb
@@ -1,14 +1,29 @@
 class PangramCase < OpenStruct
   def name
-    'test_%s' % description.delete("'").gsub(/[ -]/, '_')
+    'test_%s' % description.downcase.tr_s(" -'", '_').sub(/_$/,'')
   end
 
-  def do
-    'Pangram.is_pangram?(str)'
+  def workload
+    [
+    "phrase = '#{input}'\n",
+    "    #{assertion} Pangram.pangram?(phrase), \"#{message}\""
+    ].join
+  end
+
+  def message
+    "\#{phrase.inspect} #{is_or_isnt} a pangram"
+  end
+
+  def is_or_isnt
+    expected ? 'IS' : 'is NOT'
+  end
+
+  def assertion
+    expected ? 'assert' : 'refute'
   end
 
   def skipped?
-    index > 0
+    index.zero? ? '# skip' : 'skip'
   end
 end
 
