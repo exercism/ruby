Insti/exercism-ruby

Add rake and simplecov for beter testing.

Rakefile
@@ -1,0 +1,6 @@
+require 'rake'
+require 'rake/testtask'
+
+Rake::TestTask.new do |task|
+  task.pattern = 'tests/*_test.rb'
+end

exercism/ruby

Rakefile
@@ -1,0 +1,6 @@
+require 'rake'
+require 'rake/testtask'
+
+Rake::TestTask.new do |task|
+  task.pattern = 'test/*_test.rb'
+end