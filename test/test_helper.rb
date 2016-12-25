5a49b65d7ad501c24387cb667311c69df688ec3c
Tommy Schaefer <tommy.schaefer@teecom.com>
2016-12-25 09:39:38 -0600

    Fix typo in SimpleCov filter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 SimpleCov.start do
-  add_filter '/tests/'
+  add_filter '/test/'
   add_group 'Utilities' do |file|
     !(file.filename =~ /_cases\.rb$/)
   end
