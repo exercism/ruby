92f2660bc9c95e25b545d301d49ea2ebacb3018a
Geoff Hubbard <ghub@instihost.com>
2017-01-17 10:55:51 +0000

    Re-generate pangram tests.

--- exercises/pangram/pangram_test.rb
+++ exercises/pangram/pangram_test.rb
@@ -4,66 +4,69 @@ gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
 require_relative 'pangram'
 
-# Test data version: # 2adfe21
+# Test data version: # f04fb3d
 class PangramTest < Minitest::Test
   def test_sentence_empty
     # skip
     phrase = ''
-    refute Pangram.pangram?(phrase), "#{phrase.inspect} is NOT a pangram"
+    result = Pangram.pangram?(phrase)
+    refute result, "Expected false, got: #{result.inspect}. #{phrase.inspect} is NOT a pangram"
   end
 
   def test_pangram_with_only_lower_case
     skip
     phrase = 'the quick brown fox jumps over the lazy dog'
-    assert Pangram.pangram?(phrase), "#{phrase.inspect} IS a pangram"
+    result = Pangram.pangram?(phrase)
+    assert result, "Expected true, got: #{result.inspect}. #{phrase.inspect} IS a pangram"
   end
 
   def test_missing_character_x
     skip
     phrase = 'a quick movement of the enemy will jeopardize five gunboats'
-    refute Pangram.pangram?(phrase), "#{phrase.inspect} is NOT a pangram"
+    result = Pangram.pangram?(phrase)
+    refute result, "Expected false, got: #{result.inspect}. #{phrase.inspect} is NOT a pangram"
   end
 
   def test_another_missing_character_x
     skip
     phrase = 'the quick brown fish jumps over the lazy dog'
-    refute Pangram.pangram?(phrase), "#{phrase.inspect} is NOT a pangram"
+    result = Pangram.pangram?(phrase)
+    refute result, "Expected false, got: #{result.inspect}. #{phrase.inspect} is NOT a pangram"
   end
 
   def test_pangram_with_underscores
     skip
     phrase = 'the_quick_brown_fox_jumps_over_the_lazy_dog'
-    assert Pangram.pangram?(phrase), "#{phrase.inspect} IS a pangram"
+    result = Pangram.pangram?(phrase)
+    assert result, "Expected true, got: #{result.inspect}. #{phrase.inspect} IS a pangram"
   end
 
   def test_pangram_with_numbers
     skip
     phrase = 'the 1 quick brown fox jumps over the 2 lazy dogs'
-    assert Pangram.pangram?(phrase), "#{phrase.inspect} IS a pangram"
+    result = Pangram.pangram?(phrase)
+    assert result, "Expected true, got: #{result.inspect}. #{phrase.inspect} IS a pangram"
   end
 
   def test_missing_letters_replaced_by_numbers
     skip
     phrase = '7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog'
-    refute Pangram.pangram?(phrase), "#{phrase.inspect} is NOT a pangram"
+    result = Pangram.pangram?(phrase)
+    refute result, "Expected false, got: #{result.inspect}. #{phrase.inspect} is NOT a pangram"
   end
 
   def test_pangram_with_mixed_case_and_punctuation
     skip
     phrase = '"Five quacking Zephyrs jolt my wax bed."'
-    assert Pangram.pangram?(phrase), "#{phrase.inspect} IS a pangram"
+    result = Pangram.pangram?(phrase)
+    assert result, "Expected true, got: #{result.inspect}. #{phrase.inspect} IS a pangram"
   end
 
-  def test_pangram_with_non_ascii_characters
+  def test_upper_and_lower_case_versions_of_the_same_character_should_not_be_counted_separately
     skip
-    phrase = 'Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.'
-    assert Pangram.pangram?(phrase), "#{phrase.inspect} IS a pangram"
-  end
-
-  def test_panagram_in_alphabet_other_than_ascii
-    skip
-    phrase = 'Широкая электрификация южных губерний даст мощный толчок подъёму сельского хозяйства.'
-    refute Pangram.pangram?(phrase), "#{phrase.inspect} is NOT a pangram"
+    phrase = 'the quick brown fox jumped over the lazy FOX'
+    result = Pangram.pangram?(phrase)
+    refute result, "Expected false, got: #{result.inspect}. #{phrase.inspect} is NOT a pangram"
   end
 
   # Problems in exercism evolve over time, as we find better ways to ask
@@ -84,6 +87,6 @@ class PangramTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
   def test_bookkeeping
     skip
-    assert_equal 3, BookKeeping::VERSION
+    assert_equal 4, BookKeeping::VERSION
   end
 end
