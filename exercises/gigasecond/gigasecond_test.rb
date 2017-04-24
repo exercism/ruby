6e535a9b430d36e64f8d63c8b85e526b05d6e54c
Hilary Holz <hilary@hholz.com>
2017-04-23 17:05:01 -0700

    update gigasecond tests (was not generatable)

--- exercises/gigasecond/gigasecond_test.rb
+++ exercises/gigasecond/gigasecond_test.rb
@@ -1,36 +1,31 @@
 require 'minitest/autorun'
 require_relative 'gigasecond'
 
-# Test data version: 9049dc7
-
+# Common test data version: 61e7d70
 class GigasecondTest < Minitest::Test
-  def test_2011_04_25
-    result = Gigasecond.from(Time.utc(2011, 4, 25, 0, 0, 0))
-    assert_equal Time.utc(2043, 1, 1, 1, 46, 40), result
+  def test_date_only_specification_of_time
+    # skip
+    assert_equal Time.utc(2043, 1, 1, 1, 46, 40), Gigasecond.from(Time.utc(2011, 4, 25, 0, 0, 0))
   end
 
-  def test_1977_06_13
+  def test_second_test_for_date_only_specification_of_time
     skip
-    result = Gigasecond.from(Time.utc(1977, 6, 13, 0, 0, 0))
-    assert_equal Time.utc(2009, 2, 19, 1, 46, 40), result
+    assert_equal Time.utc(2009, 2, 19, 1, 46, 40), Gigasecond.from(Time.utc(1977, 6, 13, 0, 0, 0))
   end
 
-  def test_1959_07_19
+  def test_third_test_for_date_only_specification_of_time
     skip
-    result = Gigasecond.from(Time.utc(1959, 7, 19, 0, 0, 0))
-    assert_equal Time.utc(1991, 3, 27, 1, 46, 40), result
+    assert_equal Time.utc(1991, 3, 27, 1, 46, 40), Gigasecond.from(Time.utc(1959, 7, 19, 0, 0, 0))
   end
 
   def test_full_time_specified
     skip
-    result = Gigasecond.from(Time.utc(2015, 1, 24, 22, 0, 0))
-    assert_equal Time.utc(2046, 10, 2, 23, 46, 40), result
+    assert_equal Time.utc(2046, 10, 2, 23, 46, 40), Gigasecond.from(Time.utc(2015, 1, 24, 22, 0, 0))
   end
 
   def test_full_time_with_day_roll_over
     skip
-    result = Gigasecond.from(Time.utc(2015, 1, 24, 23, 59, 59))
-    assert_equal Time.utc(2046, 10, 3, 1, 46, 39), result
+    assert_equal Time.utc(2046, 10, 3, 1, 46, 39), Gigasecond.from(Time.utc(2015, 1, 24, 23, 59, 59))
   end
 
   # Test your 1Gs anniversary
@@ -56,6 +51,6 @@ class GigasecondTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    assert_equal 5, BookKeeping::VERSION
+    assert_equal 6, BookKeeping::VERSION
   end
 end
