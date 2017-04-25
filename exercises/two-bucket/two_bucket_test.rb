7d6ab33c5e3fb0b56d1ed15da409f3ac31552c29
Hilary Holz <hilary@hholz.com>
2017-04-25 14:38:31 -0700

    convert (and fix) two-bucket generator

--- exercises/two-bucket/two_bucket_test.rb
+++ exercises/two-bucket/two_bucket_test.rb
@@ -3,18 +3,17 @@ gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
 require_relative 'two_bucket'
 
-# Test data version:
-# deb225e Implement canonical dataset for scrabble-score problem (#255)
-
+# Common test data version: 7aa0b42
 class TwoBucketTest < Minitest::Test
-  def test_bucket_one_3_bucket_two_5_goal_1_starting_one
+  def test_bucket_one_size_3_bucket_two_size_5_goal_1_start_with_bucket_one
+    # skip
     two_bucket = TwoBucket.new(3, 5, 1, 'one')
     assert_equal 4, two_bucket.moves
     assert_equal 'one', two_bucket.goal_bucket
     assert_equal 5, two_bucket.other_bucket
   end
 
-  def test_bucket_one_3_bucket_two_5_goal_1_starting_two
+  def test_bucket_one_size_3_bucket_two_size_5_goal_1_start_with_bucket_two
     skip
     two_bucket = TwoBucket.new(3, 5, 1, 'two')
     assert_equal 8, two_bucket.moves
@@ -22,7 +21,7 @@ class TwoBucketTest < Minitest::Test
     assert_equal 3, two_bucket.other_bucket
   end
 
-  def test_bucket_one_7_bucket_two_11_goal_2_starting_one
+  def test_bucket_one_size_7_bucket_two_size_11_goal_2_start_with_bucket_one
     skip
     two_bucket = TwoBucket.new(7, 11, 2, 'one')
     assert_equal 14, two_bucket.moves
@@ -30,7 +29,7 @@ class TwoBucketTest < Minitest::Test
     assert_equal 11, two_bucket.other_bucket
   end
 
-  def test_bucket_one_7_bucket_two_11_goal_2_starting_two
+  def test_bucket_one_size_7_bucket_two_size_11_goal_2_start_with_bucket_two
     skip
     two_bucket = TwoBucket.new(7, 11, 2, 'two')
     assert_equal 18, two_bucket.moves
@@ -44,8 +43,9 @@ class TwoBucketTest < Minitest::Test
   # not your solution.
   #
   # Define a constant named VERSION inside of the top level BookKeeping
-  # module.
-  #  In your file, it will look like this:
+  # module, which may be placed near the end of your file.
+  #
+  # In your file, it will look like this:
   #
   # module BookKeeping
   #   VERSION = 1 # Where the version number matches the one in the test.
--- lib/two_bucket_cases.rb
+++ lib/two_bucket_cases.rb
@@ -1,30 +1,20 @@
 require 'exercise_cases'
 
-class TwoBucketCase < OpenStruct
-  def test_name
-    "test_bucket_one_#{bucket_one}_bucket_two_"\
-    "#{bucket_two}_goal_#{goal}_starting_#{start_bucket}"
+class TwoBucketCase < ExerciseCase
+  def name
+    "test_bucket_one_size_#{bucket_one}_bucket_two_"\
+    "size_#{bucket_two}_goal_#{goal}_start_with_bucket_#{start_bucket}"
   end
 
-  def do
-    "TwoBucket.new(#{bucket_one}, #{bucket_two}, #{goal}, '#{start_bucket}')"
-  end
-
-  def test_body
+  def workload
     "two_bucket = #{self.do}
     assert_equal #{expected['moves']}, two_bucket.moves
     assert_equal '#{expected['goal_bucket']}', two_bucket.goal_bucket
     assert_equal #{expected['other_bucket']}, two_bucket.other_bucket"
   end
 
-  def skipped?
-    index > 0
+  def do
+    "TwoBucket.new(#{bucket_one}, #{bucket_two}, #{goal}, '#{start_bucket}')"
   end
-end
 
-TwoBucketCases = proc do |data|
-  JSON.parse(data)['two_bucket']['cases'].map.with_index do |row, i|
-    row = row.merge('index' => i)
-    TwoBucketCase.new(row)
-  end
 end
