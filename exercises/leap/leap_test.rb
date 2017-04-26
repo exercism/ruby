4470a07b80c240fc1d8ab5be05e9603f5ddf0f7b
Hilary Holz <hilary@hholz.com>
2017-04-25 18:14:03 -0700

    convert (and update) leap

--- exercises/leap/leap_test.rb
+++ exercises/leap/leap_test.rb
@@ -3,8 +3,7 @@ gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
 require_relative 'leap'
 
-# Test data version:
-# 7b0949e
+# Common test data version: cc65ebe
 class Date
   def leap?
     raise RuntimeError, "Implement this yourself instead of using Ruby's implementation."
@@ -15,40 +14,26 @@ class Date
 end
 
 class YearTest < Minitest::Test
-  def test_leap_year
+  def test_year_not_divisible_by_4_common_year
     # skip
-    assert Year.leap?(1996), "Expected 'true', 1996 is a leap year."
+    refute Year.leap?(2015), "Expected 'false', 2015 is not a leap year."
   end
 
-  def test_standard_and_odd_year
+  def test_year_divisible_by_4_not_divisible_by_100_leap_year
     skip
-    refute Year.leap?(1997), "Expected 'false', 1997 is not a leap year."
+    assert Year.leap?(2016), "Expected 'true', 2016 is a leap year."
   end
 
-  def test_standard_even_year
+  def test_year_divisible_by_100_not_divisible_by_400_common_year
     skip
-    refute Year.leap?(1998), "Expected 'false', 1998 is not a leap year."
+    refute Year.leap?(2100), "Expected 'false', 2100 is not a leap year."
   end
 
-  def test_standard_nineteenth_century
-    skip
-    refute Year.leap?(1900), "Expected 'false', 1900 is not a leap year."
-  end
-
-  def test_standard_eighteenth_century
-    skip
-    refute Year.leap?(1800), "Expected 'false', 1800 is not a leap year."
-  end
-
-  def test_leap_twenty_fourth_century
-    skip
-    assert Year.leap?(2400), "Expected 'true', 2400 is a leap year."
-  end
-
-  def test_leap_y2k
+  def test_year_divisible_by_400_leap_year
     skip
     assert Year.leap?(2000), "Expected 'true', 2000 is a leap year."
   end
+
   # Problems in exercism evolve over time, as we find better ways to ask
   # questions.
   # The version number refers to the version of the problem you solved,
@@ -65,8 +50,9 @@ class YearTest < Minitest::Test
   #
   # If you are curious, read more about constants on RubyDoc:
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
+
   def test_bookkeeping
     skip
-    assert_equal 2, BookKeeping::VERSION
+    assert_equal 3, BookKeeping::VERSION
   end
 end
--- lib/leap_cases.rb
+++ lib/leap_cases.rb
@@ -1,25 +1,12 @@
 require 'exercise_cases'
 
-class LeapCase < OpenStruct
-  def name
-    'test_%s' % description.downcase.gsub(/[ -]/, '_')
-  end
+class LeapCase < ExerciseCase
 
-  def do
-    "Year.leap?(#{input})"
-  end
-
-  def skip
-    index.zero? ? '# skip' : 'skip'
+  def workload
+    "#{assert} Year.leap?(#{input.inspect})"
   end
 
   def failure_message
     "Expected '#{expected}', #{input} is #{expected ? '' : 'not '}a leap year."
-  end
-end
-
-LeapCases = proc do |data|
-  JSON.parse(data)['cases'].map.with_index do |row, i|
-    LeapCase.new(row.merge('index' => i))
   end
 end
