eb993bede74d1a6f740c950751153c0a33f5ef49
Hilary Holz <hilary@hholz.com>
2017-04-26 12:23:36 -0700

    convert (and fix) triangle generator

--- lib/triangle_cases.rb
+++ lib/triangle_cases.rb
@@ -1,11 +1,11 @@
 require 'exercise_cases'
 
-class TriangleCase < OpenStruct
-  def test_name
+class TriangleCase < ExerciseCase
+  def name
     initial = description.downcase
     replaced = initial.gsub(/(true|false)/, expected_type)
-    if initial.eql?(replaced) && !initial.include?(triangle)
-      replaced = triangle + ' triangle ' + initial
+    if initial.eql?(replaced) && !initial.include?(property)
+      replaced = property + ' triangle ' + initial
     end
     'test_%s' % replaced.tr_s(', -', '_')
   end
@@ -13,16 +13,14 @@ class TriangleCase < OpenStruct
   def workload
     [
       "triangle = Triangle.new(#{sides})",
-      indent("#{assert_or_refute} triangle.#{triangle}?, #{failure_message}")
+      indent("#{assert} triangle.#{property}?, #{failure_message}")
     ].join("\n")
   end
 
+  private
+
   def indent(line)
     ' ' * 4 + line
-  end
-
-  def assert_or_refute
-    expected ? 'assert' : 'refute'
   end
 
   def failure_message
@@ -30,24 +28,7 @@ class TriangleCase < OpenStruct
   end
 
   def expected_type
-    "triangle is #{expected ? '' : 'not '}#{triangle}"
+    "triangle is #{expected ? '' : 'not '}#{property}"
   end
 
-  def skipped
-    index.zero? ? '# skip' : 'skip'
-  end
-end
-
-TriangleCases = proc do |data|
-  i = 0
-  cases = []
-  data = JSON.parse(data).select { |key, value| key.to_s.match(/[^#]+/) }
-  data.keys.each do |triangle|
-    data[triangle]['cases'].each do |row|
-      row = row.merge(row.merge('index' => i, 'triangle' => triangle))
-      cases << TriangleCase.new(row)
-      i += 1
-    end
-  end
-  cases
 end
