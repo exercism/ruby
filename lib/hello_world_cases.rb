5c0b3d8fe8fedef704a705c561d661518763906f
Hilary Holz <hilary@hholz.com>
2017-04-25 17:29:51 -0700

    convert hello-world

--- lib/hello_world_cases.rb
+++ lib/hello_world_cases.rb
@@ -1,21 +1,9 @@
 require 'exercise_cases'
 
-class HelloWorldCase < OpenStruct
-  def test_name
-    'test_%s' % property.gsub(/[ -]/, '_')
-  end
+class HelloWorldCase < ExerciseCase
 
   def workload
-    "assert_equal #{expected.inspect}, HelloWorld.hello"
+    assert_equal { "HelloWorld.hello" }
   end
 
-  def skipped
-    index.zero? ? '# skip' : 'skip'
-  end
-end
-
-HelloWorldCases = proc do |data|
-  JSON.parse(data)['cases'].map.with_index do |row, i|
-    HelloWorldCase.new(row.merge('index' => i))
-  end
 end
