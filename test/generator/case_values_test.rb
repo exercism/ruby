8b2ae2afb7479b12c8bef042e6dbe2419b9de222
Geoff Hubbard <ghub@instihost.com>
2017-04-26 11:02:29 +0100

    Extract complex json to fixture file.
    Renamed 'gamma' to 'complex'.

--- test/generator/case_values_test.rb
+++ test/generator/case_values_test.rb
@@ -1,8 +1,8 @@
 require_relative '../test_helper'
 
-class GammaCase < ExerciseCase
+class ComplexCase < ExerciseCase
   def workload
-    assert { Gamma.foo(bar) }
+    assert { Complex.foo(bar) }
   end
 end
 
@@ -19,68 +19,19 @@ module Generator
     class AutoExtractorTest < Minitest::Test
       def test_multi_level_auto_extraction
         cases = AutoExtractor.new(
-          exercise_name: 'gamma', exercise_data: complex_canonical_data
+          exercise_name: 'complex',
+          exercise_data: File.read('test/fixtures/metadata/exercises/complex/canonical-data.json')
         ).extract
 
         expected = [
-          GammaCase.new(description: 'first generic verse', property: 'verse', number: 99,
-                        expected: '99 bottles of beer on the wall, YAAAR', index: 0),
-          GammaCase.new(description: 'last generic verse', property: 'verse', number: 3,
-                        expected: '3 bottles of beer on the wall, YAAAR', index: 1),
-          GammaCase.new(description: 'first two verses', property: 'verses', beginning: 99, end: 98,
-                        expected: "99 bottles of beer on the wall, YAR, PIRATES CAN'T COUNT", index: 2)
+          ComplexCase.new(description: 'first generic verse', property: 'verse', number: 99,
+                          expected: '99 bottles of beer on the wall, YAAAR', index: 0),
+          ComplexCase.new(description: 'last generic verse', property: 'verse', number: 3,
+                          expected: '3 bottles of beer on the wall, YAAAR', index: 1),
+          ComplexCase.new(description: 'first two verses', property: 'verses', beginning: 99, end: 98,
+                          expected: "99 bottles of beer on the wall, YAR, PIRATES CAN'T COUNT", index: 2)
         ]
         assert_equal expected.to_s, cases.to_s
-      end
-
-      def complex_canonical_data
-        <<-TEXT
-{
-  "exercise": "beer-song",
-  "version": "1.0.0",
-  "cases": [
-    {
-      "description": "verse",
-      "cases": [
-        {
-          "description": "single verse",
-          "cases": [
-            {
-              "description": "first generic verse",
-              "property": "verse",
-              "number": 99,
-              "expected": "99 bottles of beer on the wall, YAAAR"
-            },
-            {
-              "description": "last generic verse",
-              "property": "verse",
-              "number": 3,
-              "expected": "3 bottles of beer on the wall, YAAAR"
-            }
-          ]
-        }
-      ]
-    },
-    {
-      "description": "lyrics",
-      "cases": [
-        {
-          "description": "multiple verses",
-          "cases": [
-            {
-              "description": "first two verses",
-              "property": "verses",
-              "beginning": 99,
-              "end": 98,
-              "expected": "99 bottles of beer on the wall, YAR, PIRATES CAN'T COUNT"
-            }
-          ]
-        }
-      ]
-    }
-  ]
-}
-TEXT
       end
     end
 
