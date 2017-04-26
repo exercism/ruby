b72a89de206b74b8f9a17fecff2239b4d14a24bc
Hilary Holz <hilary@hholz.com>
2017-04-25 18:38:11 -0700

    convert rna-transcription

--- lib/rna_transcription_cases.rb
+++ lib/rna_transcription_cases.rb
@@ -1,21 +1,9 @@
 require 'exercise_cases'
 
-class RnaTranscriptionCase < OpenStruct
-  def test_name
-    'test_%s' % description.gsub(/[ -]/, '_')
-  end
+class RnaTranscriptionCase < ExerciseCase
 
   def workload
-    "Complement.of_dna('#{dna}')"
+    "assert_equal '#{expected}', Complement.of_dna('#{dna}')"
   end
 
-  def skipped
-    index.zero? ? '# skip' : 'skip'
-  end
-end
-
-RnaTranscriptionCases = proc do |data|
-  JSON.parse(data)['cases'].map.with_index do |row, i|
-    RnaTranscriptionCase.new(row.merge('index' => i))
-  end
 end
