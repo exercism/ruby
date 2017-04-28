4292f7ff25c0d1e577c7b72ee8f4e5a53f23844f
Hilary Holz <hilary@hholz.com>
2017-04-27 18:47:54 -0700

    remove non custom test template for rna-transcription

--- exercises/rna-transcription/rna_transcription_test.rb
+++ exercises/rna-transcription/rna_transcription_test.rb
@@ -4,7 +4,7 @@ require 'minitest/autorun'
 require_relative 'rna_transcription'
 
 # Common test data version: 0b20fff
-class ComplementTest < Minitest::Test
+class RnaTranscriptionTest < Minitest::Test
   def test_rna_complement_of_cytosine_is_guanine
     # skip
     assert_equal 'G', Complement.of_dna('C')
