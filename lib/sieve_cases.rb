0f77e323741db1a906cded2acccc828dc135e0e8
Hilary Holz <hilary@hholz.com>
2017-04-27 18:59:41 -0700

    remove non custom test template for sieve

--- lib/sieve_cases.rb
+++ lib/sieve_cases.rb
@@ -6,14 +6,21 @@ class SieveCase < ExerciseCase
   NEW_ARRAY_ROW = ",\n\s\s\s\s\s\s".freeze
   ARRAY_ELEMENTS_PER_ROW = 17.freeze
 
+  def workload
+    indent_lines([
+      "expected = #{expected_string}",
+      "assert_equal expected, Sieve.new(#{limit}).primes"
+    ], 4)
+  end
+
+  private
+
   def expected_string
     return expected unless needs_indentation?
 
     array_rows = expected.each_slice(ARRAY_ELEMENTS_PER_ROW).map { |elements| elements.join(', ') }
     "#{OPEN_ARRAY}#{array_rows.join(NEW_ARRAY_ROW)}#{CLOSE_ARRAY}"
   end
-
-  private
 
   def needs_indentation?
     expected.size > ARRAY_ELEMENTS_PER_ROW
