51acb84b127219787456423faed9033d6299a711
Hilary Holz <hilary@hholz.com>
2017-04-30 14:27:48 -0700

    convert tournament to use default test template

--- exercises/tournament/.meta/generator/tournament_cases.rb
+++ exercises/tournament/.meta/generator/tournament_cases.rb
@@ -3,32 +3,15 @@ require 'generator/exercise_cases'
 class TournamentCase < Generator::ExerciseCase
 
   def workload
-    'Tournament.tally(input)'
-  end
-
-  def expect
-    indented_heredoc(expected, 'TALLY')
-  end
-
-  def input_text
-    indented_heredoc(input, 'INPUT')
+    %Q(input = #{indented_heredoc(input, 'INPUT', 0, delimiter_mod)}\n) +
+      %Q(    actual = Tournament.tally(input)\n) +
+      %Q(    expected = #{indented_heredoc(expected, 'TALLY', 0, delimiter_mod)}\n) +
+      '    assert_equal expected, actual'
   end
 
   private
 
-  def indented_heredoc(string, delimiter)
-    [
-      "<<-#{delimiter}.gsub(/^ */, '')",
-      indent_lines(string),
-      indent_line(delimiter)
-    ].join("\n")
-  end
-
-  def indent_lines(lines, indent = 3)
-    lines.map { |line| indent_line(line, indent) }.join("\n")
-  end
-
-  def indent_line(line, indent = 2)
-    ' ' * indent * 2 + line
+  def delimiter_mod
+    ".gsub(/^ */, '')"
   end
 end
