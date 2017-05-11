597e2f6f9fa9b49429441a05f57f68da82fdd56e
Geoff Hubbard <ghub@instihost.com>
2017-05-11 08:49:45 +0100

    Remove redundant test cases.

--- test/generator/files/generate_cases_test.rb
+++ test/generator/files/generate_cases_test.rb
@@ -3,16 +3,6 @@ require_relative '../../test_helper.rb'
 module Generator
   module Files
     class GeneratorCasesTest < Minitest::Test
-      def test_no_cases_found
-        track_path = 'test/fixtures/nonexistant'
-        assert_equal [], GeneratorCases.available(track_path)
-      end
-
-      def test_cases_found
-        track_path = 'test/fixtures/xruby'
-        assert_equal %w(alpha beta), GeneratorCases.available(track_path).sort
-      end
-
       def test_available_returns_exercise_names
         track_path = 'test/fixtures/xruby'
         Dir.stub :glob, %w(/alpha_cases.rb hy_phen_ated_cases.rb) do
