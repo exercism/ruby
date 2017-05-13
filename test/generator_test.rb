34d15bc14148a1f7a4676cb7619cae12e1032085
Hilary Holz <hilary@hholz.com>
2017-05-13 15:44:42 -0700

    rename Repo => Exercise in generator.rb

--- test/generator_test.rb
+++ test/generator_test.rb
@@ -3,27 +3,27 @@ require_relative 'test_helper'
 module Generator
   class UpdateVersionAndGenerateTestsTest < Minitest::Test
     def test_call
-      mock_repository = Minitest::Mock.new
-      mock_repository.expect :update_tests_version, nil
-      mock_repository.expect :update_example_solution, nil
-      mock_repository.expect :create_tests_file, nil
+      mock_exercise = Minitest::Mock.new
+      mock_exercise.expect :update_tests_version, nil
+      mock_exercise.expect :update_example_solution, nil
+      mock_exercise.expect :create_tests_file, nil
 
-      subject = UpdateVersionAndGenerateTests.new(mock_repository)
+      subject = UpdateVersionAndGenerateTests.new(mock_exercise)
       subject.call
 
-      mock_repository.verify
+      mock_exercise.verify
     end
   end
 
   class UpdateVersionAndGenerateTestsFrozenVersionTest < Minitest::Test
     def test_call
-      mock_repository = Minitest::Mock.new
-      mock_repository.expect :create_tests_file, nil
+      mock_exercise = Minitest::Mock.new
+      mock_exercise.expect :create_tests_file, nil
 
-      subject = GenerateTests.new(mock_repository)
+      subject = GenerateTests.new(mock_exercise)
       subject.call
 
-      mock_repository.verify
+      mock_exercise.verify
     end
   end
 
