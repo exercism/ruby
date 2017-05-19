e9f8fcacdc8ffba426e10d3effa49adf35b2cc74
Geoff Hubbard <ghub@instihost.com>
2017-05-19 09:32:34 +0100

    Pass slug as the argument to the implementation factory method

--- lib/generator/command_line.rb
+++ lib/generator/command_line.rb
@@ -16,7 +16,7 @@ module Generator
     attr_reader :paths
 
     def generators
-      implementations.map { |slug| generator(implementation(exercise(slug))) }
+      implementations.map { |slug| generator(implementation(slug)) }
     end
 
     def implementations
@@ -35,11 +35,8 @@ module Generator
       @options[:freeze] || @options[:all]
     end
 
-    def exercise(slug)
-      Exercise.new(slug: slug)
-    end
-
-    def implementation(exercise)
+    def implementation(slug)
+      exercise = Exercise.new(slug: slug)
       LoggingImplementation.new(
         implementation: Implementation.new(paths: paths, exercise: exercise),
         logger: logger
