5de05077e1464bc4bb8f06d8a2cc8dc7f4f177db
Geoff Hubbard <ghub@instihost.com>
2017-05-21 22:28:15 +0100

    Rename error_message to warning
    Since we're not actually raising an error, just outputting a warning
    message to $stderr.

--- lib/generator/command_line/generator_optparser.rb
+++ lib/generator/command_line/generator_optparser.rb
@@ -83,9 +83,9 @@ module Generator
 
     def validate_cases
       return true if available_generators.include?(options[:slug])
-      error_message = "A generator does not currently exist for #{options[:slug]}!"
+      warning = "A generator does not currently exist for #{options[:slug]}!"
       expected_locations = "Expecting it to be at: #{Files::GeneratorCases.source_filepath(@paths.track, options[:slug])}"
-      $stderr.puts [error_message, expected_locations].join("\n")
+      $stderr.puts [warning, expected_locations].join("\n")
       false
     end
   end
