77404e7176d635d4cb3d1cc52ac6fccca9ad2c62
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2017-07-25 23:18:25 +0800

    Add Solution to Change

--- exercises/change/.meta/generator/change_case.rb
+++ exercises/change/.meta/generator/change_case.rb
@@ -3,7 +3,7 @@ require 'generator/exercise_case'
 class ChangeCase < Generator::ExerciseCase
 
   def workload
-    assert_equal { "Change.generate(#{coins.inspect}, #{target.inspect})" }
+    assert_equal { "Change.generate(#{coins}, #{target})" }
   end
 
 end
--- exercises/change/.meta/solutions/change.rb
+++ exercises/change/.meta/solutions/change.rb
@@ -3,7 +3,56 @@ module BookKeeping
 end
 
 class Change
-  def self.generate(coins, target)
+  attr_reader :coins, :target
 
+  def initialize(coins, target)
+    @coins  = coins.sort.reverse
+    @target = target
+    @total_change = []
   end
+
+  def generate
+    return [] if target.zero?
+
+    calculate_change(coins, [], target)
+
+    total_change.any? ? total_change.sort : -1
+  end
+
+  def self.generate(coins, target)
+    new(coins, target).generate
+  end
+
+  private
+
+  attr_accessor :total_change
+
+  def calculate_change(current_coins, current_change, current_value)
+    available_coins = current_coins.reject {|d| d > current_value } 
+
+    save_change(current_change) if current_value.zero?
+
+    return if has_more_coins?(current_change)
+
+    each_group(available_coins) do |coin, group|
+      calculate_change(group, current_change + [coin], current_value - coin)
+    end
+  end
+
+  def save_change(contents)
+    return if has_more_coins?(contents)
+
+    self.total_change = contents
+  end
+
+  def has_more_coins?(contents)
+    total_change.any? && (total_change.length < contents.length)
+  end
+
+  def each_group(array)
+    array.length.times do |n|
+      yield(array[n], array[n..-1])
+    end
+  end
+
 end
