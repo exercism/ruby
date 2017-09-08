822a6841e178433d10e1880b68036db9d96d3268
Rudresh Amin <rudresh4@gmail.com>
2017-09-08 09:41:12 -0700

    Reinsert skip statements to Two Fer tests.

--- exercises/two-fer/two_fer_test.rb
+++ exercises/two-fer/two_fer_test.rb
@@ -4,14 +4,17 @@ require_relative 'two_fer'
 # Common test data version: 1.1.0 c080bdf
 class TwoFerTest < Minitest::Test
   def test_no_name_given
+    # skip
     assert_equal "One for you, one for me.", TwoFer.getResponse('')
   end
 
   def test_a_name_given
+    skip
     assert_equal "One for Alice, one for me.", TwoFer.getResponse('Alice')
   end
 
   def test_another_name_given
+    skip
     assert_equal "One for Bob, one for me.", TwoFer.getResponse('Bob')
   end
 
