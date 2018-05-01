7add3e97975d0a4802977482b7d81afa24366c89
Ryan Workman <rdavid1099@gmail.com>
2018-05-01 10:30:00 -0600

    Fix tests to be one assertion each

--- exercises/alphanumeric-pattern-generator/alphanumeric_pattern_generator_test.rb
+++ exercises/alphanumeric-pattern-generator/alphanumeric_pattern_generator_test.rb
@@ -1,156 +1,124 @@
 require 'minitest/autorun'
 require_relative 'alphanumeric_pattern_generator'
 
-class PatternGeneratorTest < Minitest::Test
+class AlphanumericPatternGeneratorTest < Minitest::Test
   def test_it_can_verify_a_single_alphabetic_character
+    # skip
     pattern = '.'
-    pg = PatternGenerator.new(pattern)
+    pg = AlphanumericPatternGenerator.new(pattern)
 
     assert_equal true, pg.verify('F')
-    assert_equal false, pg.verify('0')
   end
 
-  def test_it_verifies_upper_and_lowercase_characters
+  def test_it_verifies_lowercase_characters
+    # skip
     pattern = '.'
-    pg = PatternGenerator.new(pattern)
+    pg = AlphanumericPatternGenerator.new(pattern)
 
     assert_equal true, pg.verify('r')
-    assert_equal true, pg.verify('R')
   end
 
   def test_it_generates_a_single_character_pattern
+    # skip
     pattern = '.'
-    pg = PatternGenerator.new(pattern)
+    pg = AlphanumericPatternGenerator.new(pattern)
 
-    assert /[A-Z]/.match(pg.generate)
-    refute /[0-9]/.match(pg.generate)
+    assert_match /[A-Z]/, pg.generate
   end
 
   def test_it_can_verify_a_single_numeric_character
+    # skip
     pattern = '#'
-    pg = PatternGenerator.new(pattern)
+    pg = AlphanumericPatternGenerator.new(pattern)
 
     assert_equal true, pg.verify('3')
-    assert_equal false, pg.verify('W')
   end
 
   def test_it_generates_a_single_numeric_pattern
+    # skip
     pattern = '#'
-    pg = PatternGenerator.new(pattern)
+    pg = AlphanumericPatternGenerator.new(pattern)
 
-    assert /[0-9]/.match(pg.generate)
-    refute /[A-Z]/.match(pg.generate)
+    assert_match /[0-9]/, pg.generate
   end
 
   def test_it_verifies_more_complex_patterns
+    # skip
     pattern = '.#.'
-    pg = PatternGenerator.new(pattern)
+    pg = AlphanumericPatternGenerator.new(pattern)
 
     assert_equal true, pg.verify('A3A')
-    assert_equal true, pg.verify('a2a')
-    assert_equal false, pg.verify('AAA')
   end
 
   def test_it_generates_more_complex_patterns
+    # skip
     pattern = '.#.'
-    pg = PatternGenerator.new(pattern)
+    pg = AlphanumericPatternGenerator.new(pattern)
 
-    assert /[A-Z][0-9][A-Z]/.match(pg.generate)
+    assert_match /[A-Z][0-9][A-Z]/, pg.generate
   end
 
   def test_it_verifies_patterns_with_specific_elements
+    # skip
     pattern = '.##ZA3.#'
-    pg = PatternGenerator.new(pattern)
+    pg = AlphanumericPatternGenerator.new(pattern)
 
-    assert_equal true, pg.verify('a23za3h1')
-    assert_equal true, pg.verify('a23ZA3h1')
-    assert_equal false, pg.verify('a23az3h1')
+    assert_equal true, pg.verify('A23ZA3H1')
   end
 
   def test_it_verifies_patterns_with_specific_elements_in_different_spots
+    # skip
     pattern = 'A#23.9.F'
-    pg = PatternGenerator.new(pattern)
+    pg = AlphanumericPatternGenerator.new(pattern)
 
     assert_equal true, pg.verify('A523B9CF')
-    assert_equal false, pg.verify('a123a8hf')
   end
 
   def test_it_generates_patterns_with_constants
+    # skip
     pattern = 'A#23..#'
-    pg = PatternGenerator.new(pattern)
+    pg = AlphanumericPatternGenerator.new(pattern)
 
-    assert /A[0-9]23[A-Z][A-Z][0-9]/.match(pg.generate)
+    assert_match /A[0-9]23[A-Z][A-Z][0-9]/, pg.generate
   end
 
   def test_it_generates_nth_value_of_patterns
+    # skip
     pattern = '.#.'
-    pg = PatternGenerator.new(pattern)
+    pg = AlphanumericPatternGenerator.new(pattern)
 
-    assert_equal 'A0A', pg.generate(0)
-    assert_equal 'A1B', pg.generate(27)
-    assert_equal 'Z9Z', pg.generate(6759)
-    assert_equal 'Z9Y', pg.generate(6758)
+    assert_equal 'A1D', pg.generate(29)
   end
 
-  def test_it_raises_exception_if_invalid_valure_is_passed_to_generate
+  def test_it_raises_exception_if_invalid_value_is_passed_to_generate
+    # skip
     pattern = '.#.'
-    pg = PatternGenerator.new(pattern)
+    pg = AlphanumericPatternGenerator.new(pattern)
 
     assert_raises(ArgumentError) { pg.generate(10000) }
-    assert_raises(ArgumentError) { pg.generate(-25) }
-  end
-
-  def test_it_generates_nth_value_of_patterns
-    pattern = '.#.'
-    pg = PatternGenerator.new(pattern)
-
-    assert_equal 'A0A', pg.generate(0)
-    assert_equal 'A1B', pg.generate(27)
-    assert_equal 'Z9Z', pg.generate(6759)
-    assert_equal 'Z9Y', pg.generate(6758)
   end
 
   def test_it_generates_nth_value_of_patterns_with_constants
+    # skip
     pattern = 'R#..W'
-    pg = PatternGenerator.new(pattern)
+    pg = AlphanumericPatternGenerator.new(pattern)
 
-    assert_equal 'R0AAW', pg.generate(0)
-    assert_equal 'R0BAW', pg.generate(26)
-    assert_equal 'R9ZYW', pg.generate(6758)
+    assert_equal 'R9ZEW', pg.generate(6738)
   end
 
   def test_it_generates_the_total_available_patterns
+    # skip
     pattern = '.#.'
-    pg = PatternGenerator.new(pattern)
+    pg = AlphanumericPatternGenerator.new(pattern)
 
     assert_equal 6760, pg.total_available
   end
 
   def test_it_generates_the_total_available_patterns_with_constants
+    # skip
     pattern = '.1.'
-    pg = PatternGenerator.new(pattern)
+    pg = AlphanumericPatternGenerator.new(pattern)
 
     assert_equal 676, pg.total_available
-  end
-
-  # Problems in exercism evolve over time, as we find better ways to ask
-  # questions.
-  # The version number refers to the version of the problem you solved,
-  # not your solution.
-  #
-  # Define a constant named VERSION inside of the top level BookKeeping
-  # module, which may be placed near the end of your file.
-  #
-  # In your file, it will look like this:
-  #
-  # module BookKeeping
-  #   VERSION = 1 # Where the version number matches the one in the test.
-  # end
-  #
-  # If you are curious, read more about constants on RubyDoc:
-  # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
-
-  def test_bookkeeping
-    assert_equal 1, BookKeeping::VERSION
   end
 end
