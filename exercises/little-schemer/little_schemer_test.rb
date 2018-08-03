53d11ba815ef5dc69740673b82a516d9f448a47c
Fronk Lom Lom <ryzingsun11@yahoo.com>
2018-08-03 22:19:32 +0800

    Brute force generator/Generate tests from canonical data

--- exercises/little-schemer/little_schemer_test.rb
+++ exercises/little-schemer/little_schemer_test.rb
@@ -1,94 +1,126 @@
 require 'minitest/autorun'
 require_relative 'little_schemer'
 
-class ListTest < Minitest::Test
-  def test_null_of_simple_list
+# Common test data version: 1.0.0 61b6bee
+class LittleSchemerTest < Minitest::Test
+  def test_null_of_non_empty_list
+    # skip
     list = [1]
     refute null?(list)
   end
 
   def test_null_of_empty_list
+    skip
     list = []
     assert null?(list)
   end
 
   def test_null_of_non_list
+    skip
     list = 'Zang'
-    assert_raises(ArgumentError) { null?(list) }
+    assert_raises ArgumentError do
+      null?(list)
+    end
   end
 
   def test_car_of_simple_list
+    skip
     list = [1]
     assert_equal car(list), 1
   end
 
   def test_car_of_small_list
-    list = ['Immaculate', 23, 'G.O.A.T']
+    skip
+    list = ["Immaculate", 23, "G.O.A.T"]
     assert_equal car(list), 'Immaculate'
   end
 
   def test_car_of_nested_list
+    skip
     list = [[4, 5], 6, 7, [8, 9]]
     assert_equal car(list), [4, 5]
   end
 
   def test_car_of_null_list
-    null_list = Array.new
+    skip
+    list = []
     assert_raises ArgumentError do
-      car(null_list)
+      car(list)
     end
   end
 
   def test_cdr_of_simple_list
+    skip
     list = [1]
     assert_equal cdr(list), []
   end
 
   def test_cdr_of_small_list
-    list = ['Immaculate', 23, 'G.O.A.T']
-    assert_equal cdr(list), [23, 'G.O.A.T']
+    skip
+    list = ["Immaculate", 23, "G.O.A.T"]
+    assert_equal cdr(list), [23, "G.O.A.T"]
   end
 
   def test_cdr_of_nested_list
+    skip
     list = [[4, 5], 6, 7, [8, 9]]
     assert_equal cdr(list), [6, 7, [8, 9]]
   end
 
   def test_cdr_of_null_list
-    null_list = Array.new
+    skip
+    list = []
     assert_raises ArgumentError do
-      cdr(null_list)
+      cdr(list)
     end
   end
 
   def test_atom_of_a_list
-    list = ['Lyn', 6]
-    assert_equal atom?(list), false
+    skip
+    list = ["Immaculate", 23]
+    refute atom?(list)
   end
 
-  def test_atom_of_car
-    list = ['Lyn', 6]
-    assert_equal atom?(car(list)), true
+  def test_atom_of_a_string
+    skip
+    list = 'Immaculate'
+    assert atom?(list)
   end
 
-  def test_atom_of_cdr
-    list = ['Lyn', 6]
-    assert_equal atom?(cdr(list)), false
+  def test_atom_of_an_integer
+    skip
+    list = 23
+    assert atom?(list)
   end
 
-  def test_lat_of_simple_list
-    list = ['Lyn', 6]
-    assert_equal lat?(list), false
+  def test_atom_of_null_list
+    skip
+    list = []
+    refute atom?(list)
+  end
+
+  def test_lat_of_a_simple_list
+    skip
+    list = ["Immaculate", 23]
+    assert lat?(list)
   end
 
   def test_lat_of_another_list
-    list = ['Lyn', 'Bicycle']
-    assert_equal lat?(list), true
+    skip
+    list = ["Immaculate", "Bicycle"]
+    assert lat?(list)
   end
 
   def test_lat_of_nested_list
-    nested_list = ['Help', 'I', 'am', ['nested']]
-    assert_equal lat?(nested_list), false
+    skip
+    list = ["Help", "I", "am", ["nested"]]
+    refute lat?(list)
+  end
+
+  def test_lat_of_null_list
+    skip
+    list = []
+    assert lat?(list)
   end
 
   # Problems in exercism evolve over time, as we find better ways to ask
@@ -97,8 +129,9 @@ class ListTest < Minitest::Test
   # not your solution.
   #
   # Define a constant named VERSION inside of the top level BookKeeping
-  # module.
-  #  In your file, it will look like this:
+  # module, which may be placed near the end of your file.
+  #
+  # In your file, it will look like this:
   #
   # module BookKeeping
   #   VERSION = 1 # Where the version number matches the one in the test.
