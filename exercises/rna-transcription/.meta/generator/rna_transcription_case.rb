0fbbd1af13b562bd9e744452524f55d6e47ae54d
tylerferraro <tyler.ferraro@gmail.com>
2018-08-23 23:57:19 -0700

    Remove exception test generator for rna transcription exercise

--- exercises/rna-transcription/.meta/generator/rna_transcription_case.rb
+++ exercises/rna-transcription/.meta/generator/rna_transcription_case.rb
@@ -2,20 +2,6 @@ require 'generator/exercise_case'
 
 class RnaTranscriptionCase < Generator::ExerciseCase
   def workload
-    if expects_error?
-      assert_raises(ArgumentError) { test_case }
-    else
-      assert_equal { test_case }
-    end
-  end
-
-  private
-
-  def test_case
     "assert_equal '#{expected}', Complement.of_dna('#{dna}')"
-  end
-
-  def expects_error?
-    expected.is_a? Hash
   end
 end
