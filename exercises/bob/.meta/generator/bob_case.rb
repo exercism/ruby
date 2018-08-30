bce386396b45e9474707e537c0e4ebc751d53750
Geoff Hubbard <ghub@instihost.com>
2018-08-30 11:49:10 +0100

    bob: Update generator

--- exercises/bob/.meta/generator/bob_case.rb
+++ exercises/bob/.meta/generator/bob_case.rb
@@ -2,9 +2,10 @@ require 'generator/exercise_case'
 
 class BobCase < Generator::ExerciseCase
   def workload
-    indent_lines(["remark = #{input["heyBob"].inspect}",
-      "assert_equal #{expected.inspect}, Bob.hey(remark), %q{Bob hears #{input["heyBob"].inspect}, and..}"
-    ], 4)
+    [
+     "remark = #{input["heyBob"].inspect}\n",
+     "assert_equal #{expected.inspect}, Bob.hey(remark), %q{Bob hears #{input["heyBob"].inspect}, and..}\n"
+    ].join
   end
 end
 
