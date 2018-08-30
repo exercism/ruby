9574053bdc43f5722cdbf0e96c5b1fde3b7fad89
Geoff Hubbard <ghub@instihost.com>
2018-08-30 11:49:11 +0100

    etl: Update generator

--- exercises/etl/.meta/generator/etl_case.rb
+++ exercises/etl/.meta/generator/etl_case.rb
@@ -2,27 +2,23 @@ require 'generator/exercise_case'
 
 class EtlCase < Generator::ExerciseCase
   def workload
-    indent_lines([
-      "old = {\n      #{format(input)}\n    }",
-      "expected = {\n      #{format(expected)}\n    }",
-      "assert_equal expected, ETL.transform(old)"
-    ], 4)
+    [
+      "old = {\n#{indent_by(2, format(input))}\n}\n",
+      "expected = {\n#{indent_by(2,format(expected))}\n}\n",
+      "assert_equal expected, ETL.transform(old)\n"
+    ].join
   end
 
   private
 
   def format(obj)
-    case
-    when obj.respond_to?(:each_pair)
-      indent_lines(
-        obj.each_with_object([]) {|(k, v), string| string << "#{format(k)} => #{format(v)}" },
-        6,
-        ",\n"
-      )
-    when obj.respond_to?(:each) then obj
-    when obj.to_s =~ /\d+/ then obj.to_i
-    else %Q('#{obj}')
+    case obj
+    when Hash
+     obj.reduce([]) {|lines, (k, v)| lines << "#{format(k)} => #{format(v)}" }.join(",\n")
+    when String
+      obj =~ /\d+/ ? obj.to_i : "'#{obj}'"
+    else
+      obj
     end
   end
-
 end
