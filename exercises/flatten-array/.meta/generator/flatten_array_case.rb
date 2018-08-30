7f55ec14efdf27d71379d731c35b82dee7965eb0
Geoff Hubbard <ghub@instihost.com>
2018-08-30 11:49:11 +0100

    flatten-array: Update generator

--- exercises/flatten-array/.meta/generator/flatten_array_case.rb
+++ exercises/flatten-array/.meta/generator/flatten_array_case.rb
@@ -1,11 +1,14 @@
 require 'generator/exercise_case'
 
 class FlattenArrayCase < Generator::ExerciseCase
-
-  def workload
-    declaration = "fa = FlattenArray.flatten(#{canonical.input})"
-    assertion = "assert_equal #{canonical.expected}, fa"
-    indent_lines([declaration, assertion], 4)
+  def description
+    super.gsub(/null/,'nil')
   end
 
+  def workload
+    [
+      "flat_array = FlattenArray.flatten(#{input_array})",
+      "assert_equal #{expected}, flat_array"
+    ]
+  end
 end
