ea20f89d113863d1556fd697ecaaed4b97b1c4be
Geoff Hubbard <ghub@instihost.com>
2018-08-30 11:49:10 +0100

    acronym: Update generator

--- exercises/acronym/.meta/generator/acronym_case.rb
+++ exercises/acronym/.meta/generator/acronym_case.rb
@@ -1,9 +1,7 @@
 require 'generator/exercise_case'
 
 class AcronymCase < Generator::ExerciseCase
-
   def workload
-    assert_equal { "Acronym.abbreviate('#{phrase}')" }
+    assert_equal(expected,  "Acronym.abbreviate('#{phrase}')")
   end
-
 end
