e082e804f4e6e54fbaceef63ff3cb680158a5f14
Geoff Hubbard <ghub@instihost.com>
2018-08-30 11:49:12 +0100

    sieve: Update generator

--- exercises/sieve/.meta/generator/sieve_case.rb
+++ exercises/sieve/.meta/generator/sieve_case.rb
@@ -1,25 +1,26 @@
 require 'generator/exercise_case'
 
 class SieveCase < Generator::ExerciseCase
-  OPEN_ARRAY = "[\n\s\s\s\s\s\s".freeze
-  CLOSE_ARRAY = "\n\s\s\s\s]".freeze
-  NEW_ARRAY_ROW = ",\n\s\s\s\s\s\s".freeze
   ARRAY_ELEMENTS_PER_ROW = 17.freeze
 
   def workload
-    indent_lines([
-      "expected = #{expected_string}",
+    [
+      formatted_expected,
       "assert_equal expected, Sieve.new(#{limit}).primes"
-    ], 4)
+    ]
   end
 
   private
 
-  def expected_string
-    return expected unless needs_indentation?
+  def formatted_expected
+    return "expected = #{expected}" unless needs_indentation?
 
     array_rows = expected.each_slice(ARRAY_ELEMENTS_PER_ROW).map { |elements| elements.join(', ') }
-    "#{OPEN_ARRAY}#{array_rows.join(NEW_ARRAY_ROW)}#{CLOSE_ARRAY}"
+    [
+      "expected = [\n",
+      indent_by(2,array_rows.join(",\n")) + "\n",
+      "]\n"
+    ].join
   end
 
   def needs_indentation?
