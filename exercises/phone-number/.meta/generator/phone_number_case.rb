e5a76dcf3e3d08e856dbcea204d077561bd2497f
Geoff Hubbard <ghub@instihost.com>
2018-08-30 11:49:12 +0100

    phone-number: Update generator

--- exercises/phone-number/.meta/generator/phone_number_case.rb
+++ exercises/phone-number/.meta/generator/phone_number_case.rb
@@ -2,6 +2,6 @@ require 'generator/exercise_case'
 
 class PhoneNumberCase < Generator::ExerciseCase
   def workload
-     assert_equal { "PhoneNumber.clean(#{phrase.inspect})" }
+     assert_equal(expected, "PhoneNumber.clean(#{phrase.inspect})")
   end
 end
