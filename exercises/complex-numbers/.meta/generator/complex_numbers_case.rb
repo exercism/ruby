6003d7e88f27e888bfc83b653c0fe9e40eb2c16d
Geoff Hubbard <ghub@instihost.com>
2018-08-30 11:49:11 +0100

    complex-numbers: Update generator

--- exercises/complex-numbers/.meta/generator/complex_numbers_case.rb
+++ exercises/complex-numbers/.meta/generator/complex_numbers_case.rb
@@ -1,44 +1,47 @@
 require 'generator/exercise_case'
 
 class ComplexNumbersCase < Generator::ExerciseCase
+  MATH_SYMBOLS = {
+    'e' => 'Math::E',
+    'pi' => 'Math::PI',
+    'ln(2)' => 'Math.log(2)'
+  }.freeze
 
-  def workload
-    indent_text(4, testcase)
+  def description
+    super.gsub(/'/,'')
   end
 
-  def number
-    (respond_to? :input) ? input : z1
+  def workload
+    [
+      "expected = #{expected_value}\n",
+      "assert_equal expected, #{subject}#{operation}\n"
+    ].join
+  end
+
+  def expected_value
+    if expected.is_a? Array
+      complex_number(*expected)
+    else
+      expected
+    end
+  end
+
+  def subject
+    complex_number(*(input['z'] || z1))
+  end
+
+  def complex_number(real, imaginary)
+    real, imaginary = [real, imaginary].map { |el| MATH_SYMBOLS[el] || el }
+    "ComplexNumber.new(#{real}, #{imaginary})"
   end
 
   def operation
     case property
-    when "add" then " + #{construct_complex(z2)}"
-    when "sub" then " - #{construct_complex(z2)}" 
-    when "mul" then " * #{construct_complex(z2)}" 
-    when "div" then " / #{construct_complex(z2)}" 
+    when 'add' then " + #{complex_number(*z2)}"
+    when 'sub' then " - #{complex_number(*z2)}"
+    when 'mul' then " * #{complex_number(*z2)}"
+    when 'div' then " / #{complex_number(*z2)}"
     else ".#{property}"
     end
   end
-
-  def testcase
-    "expected = #{expected_value}\n" +
-    "assert_equal expected, #{construct_complex(number)}#{operation}"
-  end
-
-  def construct_complex(value)
-    "ComplexNumber.new(#{parse_number(value)})"
-  end
-
-  def expected_value
-    (expected.is_a? Numeric) ? expected : construct_complex(expected)
-  end
-
-  def parse_number(number)
-    return number if number.is_a? Numeric
-    Array(number).map do |elem|
-      (elem.is_a? Numeric) ? elem.to_s : "Math::#{elem.upcase}"
-    end.join(", ")
-  end
-
 end
-
