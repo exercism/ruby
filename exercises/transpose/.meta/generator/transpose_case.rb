1b6e4800eb1e191bf9797f1e371139b31662bbef
Geoff Hubbard <ghub@instihost.com>
2018-08-30 11:49:12 +0100

    transpose: Update generator

--- exercises/transpose/.meta/generator/transpose_case.rb
+++ exercises/transpose/.meta/generator/transpose_case.rb
@@ -3,16 +3,19 @@ require 'generator/exercise_case'
 class TransposeCase < Generator::ExerciseCase
 
   def workload
-    %Q(input = #{indent_heredoc(input_lines, 'INPUT', 6, delimiter_mod)}\n) +
-      %Q(    actual = Transpose.transpose(input)\n) +
-      %Q(    expected = #{indent_heredoc(expected, 'EXPECTED', 6, delimiter_mod)}\n) +
-    '    assert_equal expected.strip, actual'
+    [
+      "input = #{indent_heredoc(input_lines, 'INPUT', 2, delimiter_mod)}",
+      "",
+      "expected = #{indent_heredoc(expected, 'EXPECTED', 2, delimiter_mod)}",
+      "",
+      "assert_equal expected, Transpose.transpose(input)",
+    ]
   end
 
   private
 
   def delimiter_mod
-    ".gsub(/^ {6}/, '')"
+    ".gsub(/^ {6}/, '').strip"
   end
 
 end
