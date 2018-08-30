fee84ea01f7e1cb110fe97371b1c8a5161c21a25
Geoff Hubbard <ghub@instihost.com>
2018-08-30 11:49:13 +0100

    wordy: Update generator

--- exercises/wordy/.meta/generator/wordy_case.rb
+++ exercises/wordy/.meta/generator/wordy_case.rb
@@ -3,44 +3,43 @@ require 'generator/exercise_case'
 class WordyCase < Generator::ExerciseCase
 
   def workload
-    [
-      "question = '#{input}'",
-      indent(4, assertion),
-    ].join("\n")
+    assertion
   end
 
   private
 
-  def indent(size, lines)
-    lines.lines.each_with_object('') { |line, obj| obj << ' ' * size + line }
+  def error_expected?
+    expected == false
   end
 
   def assertion
-    return error_assertion if expected == false
+    return error_assertion if error_expected?
     return message_assertion if message
 
-    "assert_equal(#{expected}, WordProblem.new(question).answer)"
+    [
+      "problem = WordProblem.new(#{question.inspect})",
+      "assert_equal(#{expected}, problem.answer)"
+    ]
   end
 
   def error_assertion
     [
-      'assert_raises ArgumentError do',
-      indent(2, 'WordProblem.new(question).answer'),
-      'end',
-    ].join("\n")
+      "problem = WordProblem.new(#{question.inspect})",
+      assert_raises(ArgumentError, 'problem.answer')
+    ]
   end
 
   def message_assertion
     [
-      'answer = WordProblem.new(question).answer',
-      "message = \"#{message % '#{answer}'}\"",
-      "assert_equal(#{expected}, answer, message)",
+      "problem = WordProblem.new(#{question.inspect})",
+      "message = \"#{message % '#{problem.answer}'}\"",
+      "assert_equal(#{expected}, problem.answer, message)",
     ].join("\n")
   end
 
   def message
-    return unless input == 'What is -3 plus 7 multiplied by -2?'
-
-    'You should ignore order of precedence. -3 + 7 * -2 = -8, not %s'
+    if question == 'What is -3 plus 7 multiplied by -2?'
+      'You should ignore order of precedence. -3 + 7 * -2 = -8, not %s'
+    end
   end
 end
