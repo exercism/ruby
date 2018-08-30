f5120976c09859088ad0787e216a2368a7a18a49
Geoff Hubbard <ghub@instihost.com>
2018-08-30 11:49:12 +0100

    two-fer: Regenerate tests

--- exercises/two-fer/two_fer_test.rb
+++ exercises/two-fer/two_fer_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require_relative 'two_fer'
 
-# Common test data version: 1.1.0 c080bdf
+# Common test data version: 1.2.0 4fc1acb
 class TwoFerTest < Minitest::Test
   def test_no_name_given
     # skip
