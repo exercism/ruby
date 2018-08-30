10a867cc9ae8fd2df5831696a3b7e6c00fa3ca8b
Geoff Hubbard <ghub@instihost.com>
2018-08-30 11:49:10 +0100

    binary: Update generator

--- exercises/binary/.meta/generator/binary_case.rb
+++ exercises/binary/.meta/generator/binary_case.rb
@@ -3,24 +3,30 @@ require 'generator/exercise_case'
 class BinaryCase < Generator::ExerciseCase
 
   def workload
-    raises_error? ? error_assertion : equality_assertion
+    error_expected? ? error_assertion : equality_assertion
   end
 
   private
 
+  # "binary" is using a non-standard canonical data format so we need
+  # to override this method.
+  def error_expected?
+    expected.nil?
+  end
+
   def error_assertion
-    "assert_raises(ArgumentError) { #{test_case} }"
+    [
+    "assert_raises(ArgumentError) do\n",
+    "  #{subject_of_test}\n",
+    "end\n"
+    ].join
   end
 
   def equality_assertion
-    "assert_equal #{expected}, #{test_case}"
+    "assert_equal #{expected}, #{subject_of_test}\n"
   end
 
-  def test_case
+  def subject_of_test
     "Binary.to_decimal('#{binary}')"
-  end
-
-  def raises_error?
-    expected.nil?
   end
 end
