e8e26b4d6d162130753d271e4a570d8724a5f64b
Pedro Gaspar <pedro.gaxpar@gmail.com>
2018-10-05 13:34:40 +0100

    largest-series-product: Regenerate Tests
    Update generator to use the standard error indicator.

--- exercises/largest-series-product/largest_series_product_test.rb
+++ exercises/largest-series-product/largest_series_product_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require_relative 'largest_series_product'
 
-# Common test data version: 1.1.0 92b86a8
+# Common test data version: 1.2.0 85da7a5
 class LargestSeriesProductTest < Minitest::Test
   def test_finds_the_largest_product_if_span_equals_length
     # skip
