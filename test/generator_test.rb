43b11e56f59c05b70ec06e3081c0edb8bc366ce0
Jacqueline Potts <jacqueline.a.potts@gmail.com>
2018-10-22 22:36:58 -0600

    Add back generator tests for generator class

---
+++ test/generator_test.rb
@@ -0,0 +1,15 @@
+require_relative 'test_helper'
+
+module Generator
+  class GenerateTestsTest < Minitest::Test
+    def test_call
+      mock_exercise = Minitest::Mock.new
+      mock_exercise.expect :build_tests, nil
+
+      subject = GenerateTests.new(mock_exercise)
+      subject.call
+
+      mock_exercise.verify
+    end
+  end
+end
