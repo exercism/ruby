afef282c2c37af4b73ad22852c518d3d91215fe4
Otavio Henrique <otaviopvaladares@gmail.com>
2018-10-27 02:44:09 -0300

    replacing refuse to assert on isogram  test

--- exercises/isogram/isogram_test.rb
+++ exercises/isogram/isogram_test.rb
@@ -18,13 +18,13 @@ class IsogramTest < Minitest::Test
   def test_word_with_one_duplicated_character
     skip
     input = "eleven"
-    refute Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
+    assert Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
   end
 
   def test_word_with_one_duplicated_character_from_the_end_of_the_alphabet
     skip
     input = "zzyzx"
-    refute Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
+    assert Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
   end
 
   def test_longest_reported_english_isogram
@@ -36,13 +36,13 @@ class IsogramTest < Minitest::Test
   def test_word_with_duplicated_character_in_mixed_case
     skip
     input = "Alphabet"
-    refute Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
+    assert Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
   end
 
   def test_word_with_duplicated_character_in_mixed_case_lowercase_first
     skip
     input = "alphAbet"
-    refute Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
+    assert Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
   end
 
   def test_hypothetical_isogrammic_word_with_hyphen
@@ -66,12 +66,12 @@ class IsogramTest < Minitest::Test
   def test_duplicated_character_in_the_middle
     skip
     input = "accentor"
-    refute Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
+    assert Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
   end
 
   def test_same_first_and_last_characters
     skip
     input = "angola"
-    refute Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
+    assert Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
   end
 end
