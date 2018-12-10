eb5a5ddcd49ff2d06ec5773880ecc7e645a4a890
Tonći Galić <tuxified@gmail.com>
2018-12-10 22:18:35 +0100

    use chars instead of split('')

--- exercises/armstrong-numbers/.meta/solutions/armstrong_numbers.rb
+++ exercises/armstrong-numbers/.meta/solutions/armstrong_numbers.rb
@@ -1,6 +1,6 @@
 class ArmstrongNumbers
   def self.is_valid?(number)
-    digits = number.to_s.split('').map(&:to_i)
+    digits = number.to_s.chars.map(&:to_i)
     exponent = digits.size
 
     digits.reduce(0) do |acc, digit|
