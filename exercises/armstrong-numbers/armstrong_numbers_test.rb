fde015049397f29dca2518b23aefcacc0e4a29af
Tonći Galić <tuxified@gmail.com>
2018-12-10 21:50:38 +0100

    add skip to all but first test

--- exercises/armstrong-numbers/armstrong_numbers_test.rb
+++ exercises/armstrong-numbers/armstrong_numbers_test.rb
@@ -9,42 +9,51 @@ class ArmstrongNumbersTest < Minitest::Test
   end
 
   def test_two_digit_numbers_aren_t_armstrong_numbers
+    skip
     refute ArmstrongNumbers.is_valid?(11)
     refute ArmstrongNumbers.is_valid?(89)
     refute ArmstrongNumbers.is_valid?(44)
   end
 
   def test_three_digit_numbers_that_are_the_armstrong_numbers
+    skip
     assert ArmstrongNumbers.is_valid?(153)
     assert ArmstrongNumbers.is_valid?(370)
   end
 
   def test_three_digit_numbers_that_aren_t_the_armstrong_numbers
+    skip
     refute ArmstrongNumbers.is_valid?(555)
     refute ArmstrongNumbers.is_valid?(662)
   end
 
   def test_four_digit_number_that_is_the_armstrong_numbers
+    skip
     assert ArmstrongNumbers.is_valid?(1_634)
   end
 
   def test_four_digit_number_that_is_not_the_armstrong_numbers
+    skip
     refute ArmstrongNumbers.is_valid?(9_989)
   end
 
   def test_seven_digit_number_that_is_the_armstrong_numbers
+    skip
     assert ArmstrongNumbers.is_valid?(9_926_315)
   end
 
   def test_seven_digit_number_that_is_not_the_armstrong_numbers
+    skip
     refute ArmstrongNumbers.is_valid?(2_369_989)
   end
 
   def test_ten_digit_number_that_is_the_armstrong_numbers
+    skip
     assert ArmstrongNumbers.is_valid?(4_679_307_774)
   end
 
   def test_fourteen_digit_number_that_is_the_armstrong_numbers
+    skip
     assert ArmstrongNumbers.is_valid?(28_116_440_335_967)
   end
 end
