0c6963a9e51a5536ed7ece1e26092a63180b1663
Tonći Galić <tuxified@gmail.com>
2018-12-10 22:16:26 +0100

    rename tests for better English

--- exercises/armstrong-numbers/armstrong_numbers_test.rb
+++ exercises/armstrong-numbers/armstrong_numbers_test.rb
@@ -15,44 +15,44 @@ class ArmstrongNumbersTest < Minitest::Test
     refute ArmstrongNumbers.is_valid?(44)
   end
 
-  def test_three_digit_numbers_that_are_the_armstrong_numbers
+  def test_three_digit_numbers_that_are_armstrong_numbers
     skip
     assert ArmstrongNumbers.is_valid?(153)
     assert ArmstrongNumbers.is_valid?(370)
   end
 
-  def test_three_digit_numbers_that_aren_t_the_armstrong_numbers
+  def test_three_digit_numbers_that_aren_t_armstrong_numbers
     skip
     refute ArmstrongNumbers.is_valid?(555)
     refute ArmstrongNumbers.is_valid?(662)
   end
 
-  def test_four_digit_number_that_is_the_armstrong_numbers
+  def test_four_digit_number_that_is_armstrong_number
     skip
     assert ArmstrongNumbers.is_valid?(1_634)
   end
 
-  def test_four_digit_number_that_is_not_the_armstrong_numbers
+  def test_four_digit_number_that_is_not_armstrong_number
     skip
     refute ArmstrongNumbers.is_valid?(9_989)
   end
 
-  def test_seven_digit_number_that_is_the_armstrong_numbers
+  def test_seven_digit_number_that_is_armstrong_number
     skip
     assert ArmstrongNumbers.is_valid?(9_926_315)
   end
 
-  def test_seven_digit_number_that_is_not_the_armstrong_numbers
+  def test_seven_digit_number_that_is_not_armstrong_number
     skip
     refute ArmstrongNumbers.is_valid?(2_369_989)
   end
 
-  def test_ten_digit_number_that_is_the_armstrong_numbers
+  def test_ten_digit_number_that_is_armstrong_number
     skip
     assert ArmstrongNumbers.is_valid?(4_679_307_774)
   end
 
-  def test_fourteen_digit_number_that_is_the_armstrong_numbers
+  def test_fourteen_digit_number_that_is_armstrong_number
     skip
     assert ArmstrongNumbers.is_valid?(28_116_440_335_967)
   end
