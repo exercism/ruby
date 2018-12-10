96910d4cc4726e7d8ccef6211da6e4f70314a51a
Tonći Galić <tuxified@gmail.com>
2018-12-10 22:31:11 +0100

    more idiomatic by using sum instead of reduce
    and switch to Ruby 2.4 in the process

--- exercises/armstrong-numbers/.meta/solutions/armstrong_numbers.rb
+++ exercises/armstrong-numbers/.meta/solutions/armstrong_numbers.rb
@@ -1,10 +1,7 @@
 class ArmstrongNumbers
   def self.is_valid?(number)
-    digits = number.to_s.chars.map(&:to_i)
-    exponent = digits.size
+    exponent = number.digits.size
 
-    digits.reduce(0) do |acc, digit|
-      digit**exponent + acc
-    end == number
+    number.digits.sum { |digit| digit**exponent } == number
   end
 end
