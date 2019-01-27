8b7b888ff5e4743c309d4c6f357ab68175d3d32c
Chris Couzens <ccouzens@gmail.com>
2019-01-27 22:42:49 +0000

    Regenerate test files
    This regenerates phone_number and space-age tests.
    They're still syntactically correct and the tests still pass for them.
    I've not committed acronym_test, sum_of_multiples or wordy as
    regenerating them breaks them.
    
    Keeping the exercises up to date is generally a good idea. I'm
    particuarly interested in regenerating them so I can know the effect of
    my other changes.
    
    I'll be upgrading the ruby version and the rubocop version. By
    regenerating before hand, I can more easily verify that my changes
    haven't changed the regeneration process.
    
    ```
    ./bin/generate --all
    ```

--- exercises/space-age/space_age_test.rb
+++ exercises/space-age/space_age_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require_relative 'space_age'
 
-# Common test data version: 1.1.0 8d4df79
+# Common test data version: 1.2.0 28b3dac
 class SpaceAgeTest < Minitest::Test
   # assert_in_delta will pass if the difference
   # between the values being compared is less
@@ -28,8 +28,8 @@ class SpaceAgeTest < Minitest::Test
 
   def test_age_on_mars
     skip
-    age = SpaceAge.new(2_329_871_239)
-    assert_in_delta 39.25, age.on_mars, DELTA
+    age = SpaceAge.new(2_129_871_239)
+    assert_in_delta 35.88, age.on_mars, DELTA
   end
 
   def test_age_on_jupiter
@@ -40,19 +40,19 @@ class SpaceAgeTest < Minitest::Test
 
   def test_age_on_saturn
     skip
-    age = SpaceAge.new(3_000_000_000)
-    assert_in_delta 3.23, age.on_saturn, DELTA
+    age = SpaceAge.new(2_000_000_000)
+    assert_in_delta 2.15, age.on_saturn, DELTA
   end
 
   def test_age_on_uranus
     skip
-    age = SpaceAge.new(3_210_123_456)
-    assert_in_delta 1.21, age.on_uranus, DELTA
+    age = SpaceAge.new(1_210_123_456)
+    assert_in_delta 0.46, age.on_uranus, DELTA
   end
 
   def test_age_on_neptune
     skip
-    age = SpaceAge.new(8_210_123_456)
-    assert_in_delta 1.58, age.on_neptune, DELTA
+    age = SpaceAge.new(1_821_023_456)
+    assert_in_delta 0.35, age.on_neptune, DELTA
   end
 end
