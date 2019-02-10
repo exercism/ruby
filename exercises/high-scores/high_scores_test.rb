9a62f49f905776f12cee4e52f7799d848d02ffdd
Pedro Gaspar <pedro.gaxpar@gmail.com>
2019-02-10 00:27:57 +0000

    high-scores: Regenerate tests

--- exercises/high-scores/high_scores_test.rb
+++ exercises/high-scores/high_scores_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require_relative 'high_scores'
 
-# Common test data version: 2.0.0 7a386a2
+# Common test data version: 3.0.0 999ec47
 class HighScoresTest < Minitest::Test
   def test_list_of_scores
     # skip
@@ -24,46 +24,39 @@ class HighScoresTest < Minitest::Test
     assert_equal expected, HighScores.new(scores).personal_best
   end
 
-  def test_personal_top
+  def test_personal_top_three_from_a_list_of_scores
     skip
-    scores = [50, 30, 10]
-    expected = [50, 30, 10]
-    assert_equal expected, HighScores.new(scores).personal_top
+    scores = [10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70]
+    expected = [100, 90, 70]
+    assert_equal expected, HighScores.new(scores).personal_top_three
   end
 
   def test_personal_top_highest_to_lowest
     skip
     scores = [20, 10, 30]
     expected = [30, 20, 10]
-    assert_equal expected, HighScores.new(scores).personal_top
+    assert_equal expected, HighScores.new(scores).personal_top_three
   end
 
   def test_personal_top_when_there_is_a_tie
     skip
     scores = [40, 20, 40, 30]
     expected = [40, 40, 30]
-    assert_equal expected, HighScores.new(scores).personal_top
+    assert_equal expected, HighScores.new(scores).personal_top_three
   end
 
   def test_personal_top_when_there_are_less_than_3
     skip
     scores = [30, 70]
     expected = [70, 30]
-    assert_equal expected, HighScores.new(scores).personal_top
+    assert_equal expected, HighScores.new(scores).personal_top_three
   end
 
   def test_personal_top_when_there_is_only_one
     skip
     scores = [40]
     expected = [40]
-    assert_equal expected, HighScores.new(scores).personal_top
-  end
-
-  def test_personal_top_from_a_long_list
-    skip
-    scores = [10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70]
-    expected = [100, 90, 70]
-    assert_equal expected, HighScores.new(scores).personal_top
+    assert_equal expected, HighScores.new(scores).personal_top_three
   end
 
   def test_message_for_new_personal_best
