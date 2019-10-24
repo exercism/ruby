78e3e97eb9c01b1a63729fc88caca3d2e10c20d3
Victor Goff <keeperotphones@gmail.com>
2019-10-23 23:09:11 -0400

    Update exercises/isbn-verifier/isbn_verifier.rb
    Co-Authored-By: Pedro Gaspar <pedro.gaxpar@gmail.com>

--- exercises/isbn-verifier/isbn_verifier.rb
+++ exercises/isbn-verifier/isbn_verifier.rb
@@ -1,5 +1,5 @@
 =begin
-Write your code for the 'Isbn Verifier' exercise in this file. Make the tests in
+Write your code for the 'ISBN Verifier' exercise in this file. Make the tests in
 `isbn_verifier_test.rb` pass.
 
 To get started with TDD, see the `README.md` file in your
