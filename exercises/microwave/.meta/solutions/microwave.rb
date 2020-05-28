3e249f3ce36427c3015b902e68253c4fe9db46d9
James Gates <gatorjuice@gmail.com>
2020-05-27 20:57:01 -0500

    changed variable name

--- exercises/microwave/.meta/solutions/microwave.rb
+++ exercises/microwave/.meta/solutions/microwave.rb
@@ -1,11 +1,11 @@
 class Microwave
   attr_reader :minutes, :seconds
 
-  def initialize(input)
-    base = input > 99 ? 100 : 60
+  def initialize(digits)
+    base = digits > 99 ? 100 : 60
 
-    @minutes = input / base
-    @seconds = (input - minutes * base) % base
+    @minutes = digits / base
+    @seconds = (digits - minutes * base) % base
   end
 
   def timer
