884db0a46f679f392be9729a7c3874fab933d16b
Alexander Smirnov <sas2job@gmail.com>
2020-06-01 11:42:13 +0300

    Add darts.rb file for Darts (#1032)
    * Add file for Darts with the standard comment for exercise

---
+++ exercises/darts/darts.rb
@@ -0,0 +1,7 @@
+=begin
+Write your code for the 'Darts' exercise in this file. Make the tests in
+`darts_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/darts` directory.
+=end
