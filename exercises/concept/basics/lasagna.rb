1f1601429aa3ed345995907154467cf80f003e7f
Oana <hello2oana@yahoo.com>
2020-07-05 15:40:46 +0100

    Fix typo in function name

--- exercises/concept/basics/lasagna.rb
+++ exercises/concept/basics/lasagna.rb
@@ -3,8 +3,8 @@ class Lasagna
     raise NotImplementedError, 'Please implement the remaining_minutes_in_oven method'
   end
 
-  def preperation_time_in_minutes(layers)
-    raise NotImplementedError, 'Please implement the preperation_time_in_minutes method'
+  def preparation_time_in_minutes(layers)
+    raise NotImplementedError, 'Please implement the preparation_time_in_minutes method'
   end
 
   def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
