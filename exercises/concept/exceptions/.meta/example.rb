51ab145f9fc7ec5daa86c9bb04aa9561bc6b3829
Pablo Vicente <vicapa99@gmail.com>
2020-10-12 19:34:21 +0200

    Remove Integer limitations

--- exercises/concept/exceptions/.meta/example.rb
+++ exercises/concept/exceptions/.meta/example.rb
@@ -4,10 +4,10 @@ class SimpleCalculator
   UnsupportedOperation = Class.new(StandardError)
 
   def self.calculate(first_operand, second_operand, operation)
-    raise ArgumentError.new unless (first_operand.is_a?(Integer) && second_operand.is_a?(Integer))
+    raise ArgumentError.new unless (first_operand.is_a?(Number) && second_operand.is_a?(Number))
     raise UnsupportedOperation.new unless ALLOWED_OPERATIONS.include?(operation)
 
-    result = first_operand.to_i.public_send(operation, second_operand.to_i)
+    result = first_operand.public_send(operation, second_operand)
 
     "#{first_operand} #{operation} #{second_operand} = #{result}"
   rescue ZeroDivisionError
