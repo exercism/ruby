5af68ca26fb30beef5be5a8d3af3d025a3544508
Pablo Vicente <vicapa99@gmail.com>
2020-10-16 20:20:08 +0200

    Update TODOs

--- exercises/concept/exceptions/simple_calculator.rb
+++ exercises/concept/exceptions/simple_calculator.rb
@@ -1,9 +1,6 @@
 class SimpleCalculator
   ALLOWED_OPERATIONS = ['+', '-', '/', '*']
 
-  # TODO: Show how to define custom exceptions
-  # TODO: Explain why custom errors are subclasses of StandardError
-  # TODO: Explain why is not a good idea to rescue from Exception
   UnsupportedOperation = Class.new(StandardError)
 
   def self.calculate(first_operand, second_operand, operation)
