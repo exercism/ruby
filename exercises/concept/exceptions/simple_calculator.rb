d95531b724e4c0cefeb97fc2b5af14ab2ae2faf3
Pablo Vicente <vicapa99@gmail.com>
2020-10-16 20:21:32 +0200

    Remove custom error from stub

--- exercises/concept/exceptions/simple_calculator.rb
+++ exercises/concept/exceptions/simple_calculator.rb
@@ -1,8 +1,6 @@
 class SimpleCalculator
   ALLOWED_OPERATIONS = ['+', '/', '*']
 
-  UnsupportedOperation = Class.new(StandardError)
-
   def self.calculate(first_operand, second_operand, operation)
     raise NotImplementedError, 'Please implement the SimpleCalculator.calculate method'
   end
