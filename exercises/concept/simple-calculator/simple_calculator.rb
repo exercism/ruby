0da758a73575c0aeff3db7baa3a6175f476a548c
Jeremy Walker <jez.walker@gmail.com>
2021-02-07 20:00:31 +0000

    Fix Style/MutableConstant violations (Dangerous)

--- exercises/concept/simple-calculator/simple_calculator.rb
+++ exercises/concept/simple-calculator/simple_calculator.rb
@@ -1,5 +1,5 @@
 class SimpleCalculator
-  ALLOWED_OPERATIONS = ['+', '/', '*']
+  ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
 
   def self.calculate(first_operand, second_operand, operation)
     raise NotImplementedError, 'Please implement the SimpleCalculator.calculate method'
