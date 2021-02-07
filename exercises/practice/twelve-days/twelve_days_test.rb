9453898a15724e8af2fa084548b0249553126c6a
Jeremy Walker <jez.walker@gmail.com>
2021-02-07 19:49:21 +0000

    Fix Style/ExpandPathArguments violations

--- exercises/practice/twelve-days/twelve_days_test.rb
+++ exercises/practice/twelve-days/twelve_days_test.rb
@@ -3,7 +3,7 @@ require_relative 'twelve_days'
 
 class TwelveDaysTest < Minitest::Test
   def test_the_whole_song
-    song_file = File.expand_path('../song.txt', __FILE__)
+    song_file = File.expand_path('song.txt', __dir__)
     expected = IO.read(song_file)
     assert_equal expected, TwelveDays.song
   end
