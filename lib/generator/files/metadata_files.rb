2bdc5cddf00c01fefed0724822f93ea7e4100ee8
Jeremy Walker <jez.walker@gmail.com>
2021-02-07 20:01:27 +0000

    Fix Layout/MultilineMethodCallBraceLayout violations

--- lib/generator/files/metadata_files.rb
+++ lib/generator/files/metadata_files.rb
@@ -6,7 +6,8 @@ module Generator
       def canonical_data
         CanonicalDataFile.new(
           filename: File.join(metadata_path, 'canonical-data.json'),
-          repository_root: paths.metadata)
+          repository_root: paths.metadata
+)
       end
 
       private
