312953999b85b02c0769f327b33a5124b8b947fb
Jeremy Walker <jez.walker@gmail.com>
2021-02-07 19:57:58 +0000

    Fix Minitest/AssertIncludes violations

--- exercises/practice/armstrong-numbers/armstrong_numbers_test.rb
+++ exercises/practice/armstrong-numbers/armstrong_numbers_test.rb
@@ -5,12 +5,12 @@ require_relative 'armstrong_numbers'
 class ArmstrongNumbersTest < Minitest::Test
   def test_zero_is_an_armstrong_number
     # skip
-    assert ArmstrongNumbers.include?(0)
+    assert_includes ArmstrongNumbers, 0
   end
 
   def test_single_digit_numbers_are_armstrong_numbers
     skip
-    assert ArmstrongNumbers.include?(5)
+    assert_includes ArmstrongNumbers, 5
   end
 
   def test_there_are_no_2_digit_armstrong_numbers
@@ -20,7 +20,7 @@ class ArmstrongNumbersTest < Minitest::Test
 
   def test_three_digit_number_that_is_an_armstrong_number
     skip
-    assert ArmstrongNumbers.include?(153)
+    assert_includes ArmstrongNumbers, 153
   end
 
   def test_three_digit_number_that_is_not_an_armstrong_number
@@ -30,7 +30,7 @@ class ArmstrongNumbersTest < Minitest::Test
 
   def test_four_digit_number_that_is_an_armstrong_number
     skip
-    assert ArmstrongNumbers.include?(9_474)
+    assert_includes ArmstrongNumbers, 9_474
   end
 
   def test_four_digit_number_that_is_not_an_armstrong_number
@@ -40,7 +40,7 @@ class ArmstrongNumbersTest < Minitest::Test
 
   def test_seven_digit_number_that_is_an_armstrong_number
     skip
-    assert ArmstrongNumbers.include?(9_926_315)
+    assert_includes ArmstrongNumbers, 9_926_315
   end
 
   def test_seven_digit_number_that_is_not_an_armstrong_number
