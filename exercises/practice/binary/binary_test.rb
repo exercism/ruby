701db5d9533b2b574bdc19cd2cd37b7345b90438
Victor Goff <keeperotphones@gmail.com>
2021-02-11 22:10:45 -0500

    Disable Naming/VariableNumber for binary

--- exercises/practice/binary/binary_test.rb
+++ exercises/practice/binary/binary_test.rb
@@ -3,42 +3,43 @@ require_relative 'binary'
 
 # Common test data version: 1.1.0 dd3c07e
 class BinaryTest < Minitest::Test
-  def test_binary_0_is_decimal_0 # rubocop:disable Naming/VariableNumber
+  # rubocop:disable Naming/VariableNumber
+  def test_binary_0_is_decimal_0
     # skip
     assert_equal 0, Binary.to_decimal('0')
   end
 
-  def test_binary_1_is_decimal_1 # rubocop:disable Naming/VariableNumber
+  def test_binary_1_is_decimal_1
     skip
     assert_equal 1, Binary.to_decimal('1')
   end
 
-  def test_binary_10_is_decimal_2 # rubocop:disable Naming/VariableNumber
+  def test_binary_10_is_decimal_2
     skip
     assert_equal 2, Binary.to_decimal('10')
   end
 
-  def test_binary_11_is_decimal_3 # rubocop:disable Naming/VariableNumber
+  def test_binary_11_is_decimal_3
     skip
     assert_equal 3, Binary.to_decimal('11')
   end
 
-  def test_binary_100_is_decimal_4 # rubocop:disable Naming/VariableNumber
+  def test_binary_100_is_decimal_4
     skip
     assert_equal 4, Binary.to_decimal('100')
   end
 
-  def test_binary_1001_is_decimal_9 # rubocop:disable Naming/VariableNumber
+  def test_binary_1001_is_decimal_9
     skip
     assert_equal 9, Binary.to_decimal('1001')
   end
 
-  def test_binary_11010_is_decimal_26 # rubocop:disable Naming/VariableNumber
+  def test_binary_11010_is_decimal_26
     skip
     assert_equal 26, Binary.to_decimal('11010')
   end
 
-  def test_binary_10001101000_is_decimal_1128 # rubocop:disable Naming/VariableNumber
+  def test_binary_10001101000_is_decimal_1128
     skip
     assert_equal 1128, Binary.to_decimal('10001101000')
   end
@@ -89,4 +90,5 @@ class BinaryTest < Minitest::Test
       Binary.to_decimal('001 nope')
     end
   end
+  # rubocop:enable Naming/VariableNumber
 end
