de70e8cd641f1e05538b93c26adee9b3e4ac6458
Jeremy Walker <jez.walker@gmail.com>
2021-07-06 15:41:41 +0100

    Fix rubocop

--- exercises/concept/boutique-inventory-improvements/boutique_inventory_test.rb
+++ exercises/concept/boutique-inventory-improvements/boutique_inventory_test.rb
@@ -8,7 +8,7 @@ class BoutiqueInventoryTest < Minitest::Test
 
   def test_one_item_name
     items = [
-      {price: 65.00, name: "Red Brown Dress", quantity_by_size: {}},
+      { price: 65.00, name: "Red Brown Dress", quantity_by_size: {} }
     ]
     names = ["Red Brown Dress"]
     assert_equal names, BoutiqueInventory.new(items).item_names
@@ -16,11 +16,11 @@ class BoutiqueInventoryTest < Minitest::Test
 
   def test_three_item_names
     items = [
-      {price: 65.00, name: "Red Brown Dress", quantity_by_size: {}},
-      {price: 50.00, name: "Red Short Skirt", quantity_by_size: {}},
-      {price: 29.99, name: "Black Short Skirt", quantity_by_size: {}},
+      { price: 65.00, name: "Red Brown Dress", quantity_by_size: {} },
+      { price: 50.00, name: "Red Short Skirt", quantity_by_size: {} },
+      { price: 29.99, name: "Black Short Skirt", quantity_by_size: {} }
     ]
-    names = ["Black Short Skirt", "Red Brown Dress", "Red Short Skirt", ]
+    names = ["Black Short Skirt", "Red Brown Dress", "Red Short Skirt"]
     assert_equal names, BoutiqueInventory.new(items).item_names
   end
 
@@ -29,32 +29,24 @@ class BoutiqueInventoryTest < Minitest::Test
   end
 
   def test_total_stock_for_no_stock
-    shoes = {price: 30.00, name: "Shoes", quantity_by_size: {}}
-    coat = {price: 65.00, name: "Coat", quantity_by_size: {}}
+    shoes = { price: 30.00, name: "Shoes", quantity_by_size: {} }
+    coat = { price: 65.00, name: "Coat", quantity_by_size: {} }
     items = [shoes, coat]
     assert_equal 0, BoutiqueInventory.new(items).total_stock
   end
 
   def test_total_stock_for_some_items
-    shoes = {price: 30.00, name: "Shoes", quantity_by_size: {s: 1, xl: 4}}
-    coat = {price: 65.00, name: "Coat", quantity_by_size: {}}
-    handkerchief = {price: 19.99, name: "Handkerchief", quantity_by_size: {}}
+    shoes = { price: 30.00, name: "Shoes", quantity_by_size: { s: 1, xl: 4 } }
+    coat = { price: 65.00, name: "Coat", quantity_by_size: {} }
+    handkerchief = { price: 19.99, name: "Handkerchief", quantity_by_size: {} }
     items = [shoes, coat, handkerchief]
     assert_equal 5, BoutiqueInventory.new(items).total_stock
   end
 
-  def test_total_stock_for_no_items
-    shoes = {price: 30.00, name: "Shoes", quantity_by_size: {s: 1, xl: 4}}
-    coat = {price: 65.00, name: "Coat", quantity_by_size: {m: 1, l: 2}}
-    handkerchief = {price: 19.99, name: "Handkerchief", quantity_by_size: {s: 3, m: 2}}
-    items = [shoes, coat, handkerchief]
-    assert_equal 13, BoutiqueInventory.new(items).total_stock
-  end
-
   def test_items_is_an_array_of_ostruct
-    shoes = {price: 30.00, name: "Shoes", quantity_by_size: {s: 1, xl: 4}}
-    coat = {price: 65.00, name: "Coat", quantity_by_size: {m: 1, l: 2}}
-    handkerchief = {price: 19.99, name: "Handkerchief", quantity_by_size: {s: 3, m: 2}}
+    shoes = { price: 30.00, name: "Shoes", quantity_by_size: { s: 1, xl: 4 } }
+    coat = { price: 65.00, name: "Coat", quantity_by_size: { m: 1, l: 2 } }
+    handkerchief = { price: 19.99, name: "Handkerchief", quantity_by_size: { s: 3, m: 2 } }
     items = [shoes, coat, handkerchief]
     assert_equal Array, BoutiqueInventory.new(items).items.class
     assert_equal OpenStruct, BoutiqueInventory.new(items).items.first.class
