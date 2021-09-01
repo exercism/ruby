357624ddf6a588b79b0bdb2e431bdd9d5bbb8dda
Tim Austin <tim@neenjaw.com>
2021-08-31 21:55:44 -0600

    Update exercises/concept/high-score/high_score.rb
    Co-authored-by: Angelika Tyborska <angelikatyborska@fastmail.com>

--- exercises/concept/high-score/high_score.rb
+++ exercises/concept/high-score/high_score.rb
@@ -1,30 +1,30 @@
 class Scoreboard
   def initialize(initial_scores)
-    raise NotImplementedError, 'Please implement the Scoreboard#initialize method'
+    raise 'Please implement the Scoreboard#initialize method'
   end
 
   def players
-    raise NotImplementedError, 'Please implement the Scoreboard#players method'
+    raise 'Please implement the Scoreboard#players method'
   end
 
   def add_player(name, initial_score)
-    raise NotImplementedError, 'Please implement the Scoreboard#add_player method'
+    raise 'Please implement the Scoreboard#add_player method'
   end
 
   def remove_player(name)
-    raise NotImplementedError, 'Please implement the Scoreboard#remove_player method'
+    raise 'Please implement the Scoreboard#remove_player method'
   end
 
   def reset_player_score(name)
-    raise NotImplementedError, 'Please implement the Scoreboard#reset_player_score method'
+    raise 'Please implement the Scoreboard#reset_player_score method'
   end
 
   def update_player_score(name, score)
-    raise NotImplementedError, 'Please implement the Scoreboard#update_player_score method'
+    raise 'Please implement the Scoreboard#update_player_score method'
   end
 
   def player_score(name)
-    raise NotImplementedError, 'Please implement the Scoreboard#player_score method'
+    raise 'Please implement the Scoreboard#player_score method'
   end
 end
 
