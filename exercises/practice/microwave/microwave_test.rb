d2527fb6b99d4cfce687fb41875d71f30bdc22e6
woarewe <rostislav@hey.com>
2021-09-04 03:01:37 +0300

    Run all tests for Microwave task

--- exercises/practice/microwave/microwave_test.rb
+++ exercises/practice/microwave/microwave_test.rb
@@ -4,57 +4,46 @@ require_relative 'microwave'
 # Common test data version: 1.7.0 cacf1f1
 class MicrowaveTest < Minitest::Test
   def test_one_second
-    # skip
     assert_equal '00:01', Microwave.new(1).timer
   end
 
   def test_fifty_nine_seconds
-    skip
     assert_equal '00:59', Microwave.new(59).timer
   end
 
   def test_sixty_seconds
-    skip
     assert_equal '01:00', Microwave.new(60).timer
   end
 
   def test_one_minute
-    skip
     assert_equal '01:00', Microwave.new(100).timer
   end
 
   def test_ninety_seconds
-    skip
     assert_equal '01:30', Microwave.new(90).timer
   end
 
   def test_one_minute_and_one_second
-    skip
     assert_equal '01:01', Microwave.new(101).timer
   end
 
   def test_sixty_one_seconds
-    skip
     assert_equal '01:01', Microwave.new(61).timer
   end
 
   def test_one_minute_and_fifty_nine_seconds
-    skip
     assert_equal '01:59', Microwave.new(159).timer
   end
 
   def test_two_minutes
-    skip
     assert_equal '02:00', Microwave.new(200).timer
   end
 
   def test_over_ten_minutes
-    # skip
     assert_equal '10:01', Microwave.new(1001).timer
   end
 
   def test_minute_overflow_adds_to_input_minutes
-    skip
     assert_equal '03:12', Microwave.new(272).timer
   end
 end
