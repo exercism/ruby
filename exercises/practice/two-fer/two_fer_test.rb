c84b4bbae62f61f7112feabe97cfcbf212d2f721
Rostislav Zhuravsky <rostislav@hey.com>
2021-09-04 04:11:24 +0300

    Remove skips for TwoFer tests

--- exercises/practice/two-fer/two_fer_test.rb
+++ exercises/practice/two-fer/two_fer_test.rb
@@ -4,17 +4,14 @@ require_relative 'two_fer'
 # Common test data version: 1.2.0 4fc1acb
 class TwoFerTest < Minitest::Test
   def test_no_name_given
-    # skip
     assert_equal "One for you, one for me.", TwoFer.two_fer
   end
 
   def test_a_name_given
-    skip
     assert_equal "One for Alice, one for me.", TwoFer.two_fer("Alice")
   end
 
   def test_another_name_given
-    skip
     assert_equal "One for Bob, one for me.", TwoFer.two_fer("Bob")
   end
 end
