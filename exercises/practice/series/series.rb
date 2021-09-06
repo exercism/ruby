61f2381aeca7a98e90ada853fbec89964f54b576
woarewe <rostislav@hey.com>
2021-09-06 03:18:00 +0300

    Add class interfaces to better web editor experience

--- exercises/practice/series/series.rb
+++ exercises/practice/series/series.rb
@@ -1,7 +1,7 @@
-=begin
-Write your code for the 'Series' exercise in this file. Make the tests in
-`series_test.rb` pass.
+class Series
+  def initialize(string); end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/series` directory.
-=end
+  def slices(size)
+    raise NotImplementedError
+  end
+end
