97babc5b9098b74cbf02d3ad9a504eca87390d96
PaulT89 <paul.d.turner.1989@gmail.com>
2022-04-16 17:29:58 +1000

    [book-store]: Rewrote example.rb and added test
    Changes in example.rb:
    - fixed hardcoding problem
    - reduced the 3 main blocks of loops to 2
    - removed private method, changed each element of GROUP_DISCOUNTS constant to 1 - old value, and put the equation in groups.sum{...}, thus replacing groups.map{...}.inject(0,:+)
    
    Changes in book_store_test.rb:
    - changed numbers to words in function definitions (for consistency's sake)
    - added test_group_of_one_and_four_is_cheaper_than_group_of_two_and_three
    
    Changes in config.json:
    - added my handle as a contributor

--- exercises/practice/book-store/.meta/example.rb
+++ exercises/practice/book-store/.meta/example.rb
@@ -1,46 +1,29 @@
+require 'set'
+
 class BookStore
 
-  GROUP_DISCOUNTS = [0, 0.05, 0.1, 0.2, 0.25]
+  GROUP_DISCOUNTS = [1.0, 0.95, 0.9, 0.8, 0.75]
   INDIVIDUAL_PRICE = 8
 
   def self.calculate_price(basket)
-    #FIXME: find a real solution instead of hardcoding :(
-    return 51.20 if basket == [1, 1, 2, 3, 4, 4, 5, 5]
+    return 0.00 if basket.empty?
 
     groups = []
-    remaining_books = basket.dup
-
-    # Make as many groups of 4 as you can
-    while (largest_group = remaining_books.uniq).length > 3
-      group_of_four = largest_group[0..3]
-      group_of_four.each {|book| remaining_books.delete_at(remaining_books.index(book)) }
-      groups << group_of_four
-    end
-
-    # Bump as many of them to groups of 5 as you can
-    groups.each do |group|
-      fifth_book = (remaining_books - group).first
-      next unless fifth_book
-
-      group << fifth_book
-      remaining_books.delete_at(remaining_books.index(fifth_book))
-    end
 
     # Make the largest groups you can with the remaining books
-    while (new_group = remaining_books.uniq).any?
-      new_group.each {|book| remaining_books.delete_at(remaining_books.index(book)) }
-      groups << new_group
+    while !basket.empty?
+      set = Set.new(basket)
+      groups << set.size
+      set.each{|book| basket.delete_at(basket.index(book))} # Remove books in set from basket
     end
 
-    groups.map {|group| group_price(group.length) }.inject(0,:+)
-  end
+    # Replace as many groups of 3 & 5 as possible with groups of 4 & 4
+    while Set[3, 5] <= Set.new(groups)
+      groups.delete_at(groups.index(3)) # Remove an instance of book 3 from groups
+      groups.delete_at(groups.index(5)) # Remove an instance of book 5 from groups
+      groups.push(4, 4)
+    end
 
-  private
 
-  def self.group_price(group_size)
-    discount = GROUP_DISCOUNTS[group_size - 1]
-
-    group_size * INDIVIDUAL_PRICE * (1 - discount)
-  end
-
+    groups.sum{|group| INDIVIDUAL_PRICE * group * GROUP_DISCOUNTS[group - 1]}
 end
