109ff25f7fd2a7fe2695c4589587ecf0f2e31772
Katrina Owen <katrina.owen@gmail.com>
2022-10-13 19:42:23 +0200

    Sync armstrong numbers
    I synced the docs and tests, and regenerated the test suite.
    
    The test names changed, and there were a couple of new tests.
    One of them is for a very big number, which doesn't need any
    careful handling in Ruby.

--- exercises/practice/armstrong-numbers/armstrong_numbers_test.rb
+++ exercises/practice/armstrong-numbers/armstrong_numbers_test.rb
@@ -12,7 +12,7 @@ class ArmstrongNumbersTest < Minitest::Test
     assert_includes ArmstrongNumbers, 5
   end
 
-  def test_there_are_no_2_digit_armstrong_numbers
+  def test_there_are_no_two_digit_armstrong_numbers
     skip
     refute_includes ArmstrongNumbers, 10
   end
@@ -29,12 +29,12 @@ class ArmstrongNumbersTest < Minitest::Test
 
   def test_four_digit_number_that_is_an_armstrong_number
     skip
-    assert_includes ArmstrongNumbers, 9_474
+    assert_includes ArmstrongNumbers, 9474
   end
 
   def test_four_digit_number_that_is_not_an_armstrong_number
     skip
-    refute_includes ArmstrongNumbers, 9_475
+    refute_includes ArmstrongNumbers, 9475
   end
 
   def test_seven_digit_number_that_is_an_armstrong_number
@@ -45,5 +45,15 @@ class ArmstrongNumbersTest < Minitest::Test
   def test_seven_digit_number_that_is_not_an_armstrong_number
     skip
     refute_includes ArmstrongNumbers, 9_926_314
+  end
+
+  def test_armstrong_number_containing_seven_zeroes
+    skip
+    assert_includes ArmstrongNumbers, 186_709_961_001_538_790_100_634_132_976_990
+  end
+
+  def test_the_largest_and_last_armstrong_number
+    skip
+    assert_includes ArmstrongNumbers, 115_132_219_018_763_992_565_095_597_973_971_522_401
   end
 end
