e30a920b62a9d18220615d5c3b25576f45cce3ce
Katrina Owen <katrina.owen@gmail.com>
2022-10-13 16:21:14 +0200

    Sync two-bucket with problem-specifications
    When syncing the tests.toml there were three possible new tests.
    Two of them added error handling, which I decided to exclude, as
    I don't think error handling makes this exercise more interesting.
    
    Regenerating changed from single quotes to double quotes, which
    I think is fine, since we haven't taken a strong stance on that
    from the Rubocop perspective.

--- exercises/practice/two-bucket/two_bucket_test.rb
+++ exercises/practice/two-bucket/two_bucket_test.rb
@@ -2,51 +2,59 @@ require 'minitest/autorun'
 require_relative 'two_bucket'
 
 class TwoBucketTest < Minitest::Test
-  def test_bucket_one_size_3_bucket_two_size_5_goal_1_start_with_bucket_one
+  def test_measure_using_bucket_one_of_size_3_and_bucket_two_of_size_5_start_with_bucket_one
     # skip
-    subject = TwoBucket.new(3, 5, 1, 'one')
+    subject = TwoBucket.new(3, 5, 1, "one")
     assert_equal 4, subject.moves
-    assert_equal 'one', subject.goal_bucket
+    assert_equal "one", subject.goal_bucket
     assert_equal 5, subject.other_bucket
   end
 
-  def test_bucket_one_size_3_bucket_two_size_5_goal_1_start_with_bucket_two
+  def test_measure_using_bucket_one_of_size_3_and_bucket_two_of_size_5_start_with_bucket_two
     skip
-    subject = TwoBucket.new(3, 5, 1, 'two')
+    subject = TwoBucket.new(3, 5, 1, "two")
     assert_equal 8, subject.moves
-    assert_equal 'two', subject.goal_bucket
+    assert_equal "two", subject.goal_bucket
     assert_equal 3, subject.other_bucket
   end
 
-  def test_bucket_one_size_7_bucket_two_size_11_goal_2_start_with_bucket_one
+  def test_measure_using_bucket_one_of_size_7_and_bucket_two_of_size_11_start_with_bucket_one
     skip
-    subject = TwoBucket.new(7, 11, 2, 'one')
+    subject = TwoBucket.new(7, 11, 2, "one")
     assert_equal 14, subject.moves
-    assert_equal 'one', subject.goal_bucket
+    assert_equal "one", subject.goal_bucket
     assert_equal 11, subject.other_bucket
   end
 
-  def test_bucket_one_size_7_bucket_two_size_11_goal_2_start_with_bucket_two
+  def test_measure_using_bucket_one_of_size_7_and_bucket_two_of_size_11_start_with_bucket_two
     skip
-    subject = TwoBucket.new(7, 11, 2, 'two')
+    subject = TwoBucket.new(7, 11, 2, "two")
     assert_equal 18, subject.moves
-    assert_equal 'two', subject.goal_bucket
+    assert_equal "two", subject.goal_bucket
     assert_equal 7, subject.other_bucket
   end
 
-  def test_bucket_one_size_1_bucket_two_size_3_goal_3_start_with_bucket_two
+  def test_measure_one_step_using_bucket_one_of_size_1_and_bucket_two_of_size_3_start_with_bucket_two
     skip
-    subject = TwoBucket.new(1, 3, 3, 'two')
+    subject = TwoBucket.new(1, 3, 3, "two")
     assert_equal 1, subject.moves
-    assert_equal 'two', subject.goal_bucket
+    assert_equal "two", subject.goal_bucket
     assert_equal 0, subject.other_bucket
   end
 
-  def test_bucket_one_size_2_bucket_two_size_3_goal_3_start_with_bucket_one
+  def test_measure_using_bucket_one_of_size_2_and_bucket_two_of_size_3_start_with_bucket_one_and_end_with_bucket_two
     skip
-    subject = TwoBucket.new(2, 3, 3, 'one')
+    subject = TwoBucket.new(2, 3, 3, "one")
     assert_equal 2, subject.moves
-    assert_equal 'two', subject.goal_bucket
+    assert_equal "two", subject.goal_bucket
     assert_equal 2, subject.other_bucket
+  end
+
+  def test_with_the_same_buckets_but_a_different_goal_then_it_is_possible
+    skip
+    subject = TwoBucket.new(6, 15, 9, "one")
+    assert_equal 10, subject.moves
+    assert_equal "two", subject.goal_bucket
+    assert_equal 0, subject.other_bucket
   end
 end
