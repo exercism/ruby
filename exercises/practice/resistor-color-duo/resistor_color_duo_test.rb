7b17fa17bca1b4974772f1e05175e750612233d6
Katrina Owen <katrina.owen@gmail.com>
2022-10-13 21:54:37 +0200

    Sync resistor-color-duo with problem-specifications
    We got new docs and two new tests.

--- exercises/practice/resistor-color-duo/resistor_color_duo_test.rb
+++ exercises/practice/resistor-color-duo/resistor_color_duo_test.rb
@@ -17,6 +17,11 @@ class ResistorColorDuoTest < Minitest::Test
     assert_equal 47, ResistorColorDuo.value(%w[yellow violet])
   end
 
+  def test_white_and_red
+    skip
+    assert_equal 92, ResistorColorDuo.value(%w[white red])
+  end
+
   def test_orange_and_orange
     skip
     assert_equal 33, ResistorColorDuo.value(%w[orange orange])
@@ -25,5 +30,10 @@ class ResistorColorDuoTest < Minitest::Test
   def test_ignore_additional_colors
     skip
     assert_equal 51, ResistorColorDuo.value(%w[green brown orange])
+  end
+
+  def test_black_and_brown_one_digit
+    skip
+    assert_equal 1, ResistorColorDuo.value(%w[black brown])
   end
 end
