6a57a0a20620b20561da6486f64001f5104e9fd4
Katrina Owen <katrina.owen@gmail.com>
2022-10-13 22:00:42 +0200

    Sync luhn with problem-specifications
    This adds several new tests, but doesn't introduce any changed
    requirements.

--- exercises/practice/luhn/luhn_test.rb
+++ exercises/practice/luhn/luhn_test.rb
@@ -37,14 +37,24 @@ class LuhnTest < Minitest::Test
     refute Luhn.valid?("8273 1232 7352 0569")
   end
 
+  def test_invalid_long_number_with_an_even_remainder
+    skip
+    refute Luhn.valid?("1 2345 6789 1234 5678 9012")
+  end
+
+  def test_invalid_long_number_with_a_remainder_divisible_by_5
+    skip
+    refute Luhn.valid?("1 2345 6789 1234 5678 9013")
+  end
+
   def test_valid_number_with_an_even_number_of_digits
     skip
     assert Luhn.valid?("095 245 88")
   end
 
-  def test_valid_strings_with_a_non_digit_included_become_invalid
+  def test_valid_number_with_an_odd_number_of_spaces
     skip
-    refute Luhn.valid?("055a 444 285")
+    assert Luhn.valid?("234 567 891 234")
   end
 
   def test_valid_strings_with_a_non_digit_added_at_the_end_become_invalid
@@ -59,7 +69,7 @@ class LuhnTest < Minitest::Test
 
   def test_valid_strings_with_symbols_included_become_invalid
     skip
-    refute Luhn.valid?("055£ 444$ 285")
+    refute Luhn.valid?("055# 444$ 285")
   end
 
   def test_single_zero_with_space_is_invalid
@@ -77,8 +87,28 @@ class LuhnTest < Minitest::Test
     assert Luhn.valid?("091")
   end
 
-  def test_strings_with_non_digits_is_invalid
+  def test_very_long_input_is_valid
+    skip
+    assert Luhn.valid?("9999999999 9999999999 9999999999 9999999999")
+  end
+
+  def test_valid_luhn_with_an_odd_number_of_digits_and_non_zero_first_digit
+    skip
+    assert Luhn.valid?("109")
+  end
+
+  def test_using_ascii_value_for_non_doubled_non_digit_isnt_allowed
+    skip
+    refute Luhn.valid?("055b 444 285")
+  end
+
+  def test_using_ascii_value_for_doubled_non_digit_isnt_allowed
     skip
     refute Luhn.valid?(":9")
+  end
+
+  def test_non_numeric_non_space_char_in_the_middle_with_a_sum_thats_divisible_by_10_isnt_allowed
+    skip
+    refute Luhn.valid?("59%59")
   end
 end
