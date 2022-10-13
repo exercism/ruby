623ff5f0c299cd82e5fd0e48435eebfa2a7245ec
Katrina Owen <katrina.owen@gmail.com>
2022-10-13 18:25:48 +0200

    Sync hamming with problem-specifications
    The tests.toml had a number of reimplemented tests,
    but nothing that changes the exercise materially.

--- exercises/practice/hamming/hamming_test.rb
+++ exercises/practice/hamming/hamming_test.rb
@@ -4,40 +4,54 @@ require_relative 'hamming'
 class HammingTest < Minitest::Test
   def test_empty_strands
     # skip
-    assert_equal 0, Hamming.compute('', '')
+    assert_equal 0, Hamming.compute("", "")
   end
 
   def test_single_letter_identical_strands
     skip
-    assert_equal 0, Hamming.compute('A', 'A')
+    assert_equal 0, Hamming.compute("A", "A")
   end
 
   def test_single_letter_different_strands
     skip
-    assert_equal 1, Hamming.compute('G', 'T')
+    assert_equal 1, Hamming.compute("G", "T")
   end
 
   def test_long_identical_strands
     skip
-    assert_equal 0, Hamming.compute('GGACTGAAATCTG', 'GGACTGAAATCTG')
+    assert_equal 0, Hamming.compute("GGACTGAAATCTG", "GGACTGAAATCTG")
   end
 
   def test_long_different_strands
     skip
-    assert_equal 9, Hamming.compute('GGACGGATTCTG', 'AGGACGGATTCT')
+    assert_equal 9, Hamming.compute("GGACGGATTCTG", "AGGACGGATTCT")
   end
 
   def test_disallow_first_strand_longer
     skip
     assert_raises(ArgumentError) do
-      Hamming.compute('AATG', 'AAA')
+      Hamming.compute("AATG", "AAA")
     end
   end
 
   def test_disallow_second_strand_longer
     skip
     assert_raises(ArgumentError) do
-      Hamming.compute('ATA', 'AGTG')
+      Hamming.compute("ATA", "AGTG")
+    end
+  end
+
+  def test_disallow_empty_first_strand
+    skip
+    assert_raises(ArgumentError) do
+      Hamming.compute("", "G")
+    end
+  end
+
+  def test_disallow_empty_second_strand
+    skip
+    assert_raises(ArgumentError) do
+      Hamming.compute("G", "")
     end
   end
 end
