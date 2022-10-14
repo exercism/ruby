afbb284166eee6f3ce8c57b9ac3c440ff51683cd
Katrina Owen <katrina.owen@gmail.com>
2022-10-14 09:46:37 +0200

    Sync flatten-array with problem-specifications
    Syncing brought in new docs, metadata, and tests to the tests.toml.
    
    I chose to exclude tests for "null-like" values, as Ruby only has
    nil and these are already thoroughly tested.
    
    When regenerating the tests I inlined the variable for tests where
    asserting that the result is an empty array.

--- exercises/practice/flatten-array/flatten_array_test.rb
+++ exercises/practice/flatten-array/flatten_array_test.rb
@@ -2,10 +2,20 @@ require 'minitest/autorun'
 require_relative 'flatten_array'
 
 class FlattenArrayTest < Minitest::Test
-  def test_no_nesting
+  def test_empty
     # skip
+    assert_empty FlattenArray.flatten([])
+  end
+
+  def test_no_nesting
+    skip
     flat_array = FlattenArray.flatten([0, 1, 2])
     assert_equal [0, 1, 2], flat_array
+  end
+
+  def test_flattens_a_nested_array
+    skip
+    assert_empty FlattenArray.flatten([[[]]])
   end
 
   def test_flattens_array_with_just_integers_present
@@ -26,15 +36,14 @@ class FlattenArrayTest < Minitest::Test
     assert_equal [1, 2, 3, 4, 5, 6, 7, 8], flat_array
   end
 
-  def test_6_level_nest_list_with_nil_values
+  def test_6_level_nest_list_with_null_values
     skip
     flat_array = FlattenArray.flatten([0, 2, [[2, 3], 8, [[100]], nil, [[nil]]], -2])
     assert_equal [0, 2, 2, 3, 8, 100, -2], flat_array
   end
 
-  def test_all_values_in_nested_list_are_nil
+  def test_all_values_in_nested_list_are_null
     skip
-    flat_array = FlattenArray.flatten([nil, [[[nil]]], nil, nil, [[nil, nil], nil], nil])
-    assert_empty flat_array
+    assert_empty FlattenArray.flatten([nil, [[[nil]]], nil, nil, [[nil, nil], nil], nil])
   end
 end
