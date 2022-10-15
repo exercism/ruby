55cfec70749d70c0dc74efe22acc8318d9dd3aed
Katrina Owen <katrina.owen@gmail.com>
2022-10-15 20:16:05 +0200

    Sync high-scores with problem-specifications
    There were a slew of new tests for methods that were not previously
    a part of the API of the class.
    
    None of the methods are mentioned in the instructions for the exercise,
    and they also don't seem very interesting, so I didn't bring them in.
    
    When regenerating the test suite, the tests for the #latest_is_personal_best?
    method got removed. I deleted the corresponding code in the sample
    solution, but kotp mentioned that this bit of code provides a
    good avenue for digging deeper with some students, so I put it back.
    
    I've now created a custom inclusion for these tests so they get added
    to the test suite when regenerating it.

--- exercises/practice/high-scores/high_scores_test.rb
+++ exercises/practice/high-scores/high_scores_test.rb
@@ -5,57 +5,49 @@ class HighScoresTest < Minitest::Test
   def test_list_of_scores
     # skip
     scores = [30, 50, 20, 70]
-    expected = [30, 50, 20, 70]
-    assert_equal expected, HighScores.new(scores).scores
+    assert_equal [30, 50, 20, 70], HighScores.new(scores).scores
   end
 
   def test_latest_score
     skip
     scores = [100, 0, 90, 30]
-    expected = 30
-    assert_equal expected, HighScores.new(scores).latest
+    assert_equal 30, HighScores.new(scores).latest
   end
 
   def test_personal_best
     skip
     scores = [40, 100, 70]
-    expected = 100
-    assert_equal expected, HighScores.new(scores).personal_best
+    assert_equal 100, HighScores.new(scores).personal_best
   end
 
-  def test_personal_top_three_from_a_list_of_scores
+  def test_top_3_scores_personal_top_three_from_a_list_of_scores
     skip
     scores = [10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70]
-    expected = [100, 90, 70]
-    assert_equal expected, HighScores.new(scores).personal_top_three
+    assert_equal [100, 90, 70], HighScores.new(scores).personal_top_three
   end
 
-  def test_personal_top_highest_to_lowest
+  def test_top_3_scores_personal_top_highest_to_lowest
     skip
     scores = [20, 10, 30]
-    expected = [30, 20, 10]
-    assert_equal expected, HighScores.new(scores).personal_top_three
+    assert_equal [30, 20, 10], HighScores.new(scores).personal_top_three
   end
 
-  def test_personal_top_when_there_is_a_tie
+  def test_top_3_scores_personal_top_when_there_is_a_tie
     skip
     scores = [40, 20, 40, 30]
-    expected = [40, 40, 30]
-    assert_equal expected, HighScores.new(scores).personal_top_three
+    assert_equal [40, 40, 30], HighScores.new(scores).personal_top_three
   end
 
-  def test_personal_top_when_there_are_less_than_3
+  def test_top_3_scores_personal_top_when_there_are_less_than_3
     skip
     scores = [30, 70]
-    expected = [70, 30]
-    assert_equal expected, HighScores.new(scores).personal_top_three
+    assert_equal [70, 30], HighScores.new(scores).personal_top_three
   end
 
-  def test_personal_top_when_there_is_only_one
+  def test_top_3_scores_personal_top_when_there_is_only_one
     skip
     scores = [40]
-    expected = [40]
-    assert_equal expected, HighScores.new(scores).personal_top_three
+    assert_equal [40], HighScores.new(scores).personal_top_three
   end
 
   def test_latest_score_is_not_the_personal_best
