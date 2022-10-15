e64b1ea55ad778d5b730576ff01ead570582c610
Katrina Owen <katrina.owen@gmail.com>
2022-10-15 22:14:17 +0200

    Sync book-store with problem-specifications
    The sync updated the docs and the tests.toml file.
    
    Regenerating the test suite added several new tests.
    One of the tests had a failure due to a rounding difference:
    
        Expected: 145.6
          Actual: 145.60000000000002
    
    I changed the test suite to use assert_in_delta instead.

--- exercises/practice/book-store/book_store_test.rb
+++ exercises/practice/book-store/book_store_test.rb
@@ -5,90 +5,108 @@ class BookStoreTest < Minitest::Test
   def test_only_a_single_book
     # skip
     basket = [1]
-    assert_equal 8.00, BookStore.calculate_price(basket)
+    assert_in_delta 8.00, BookStore.calculate_price(basket), 0.001
   end
 
   def test_two_of_the_same_book
     skip
     basket = [2, 2]
-    assert_equal 16.00, BookStore.calculate_price(basket)
+    assert_in_delta 16.00, BookStore.calculate_price(basket), 0.001
   end
 
   def test_empty_basket
     skip
     basket = []
-    assert_equal 0.00, BookStore.calculate_price(basket)
+    assert_in_delta 0.00, BookStore.calculate_price(basket), 0.001
   end
 
   def test_two_different_books
     skip
     basket = [1, 2]
-    assert_equal 15.20, BookStore.calculate_price(basket)
+    assert_in_delta 15.20, BookStore.calculate_price(basket), 0.001
   end
 
   def test_three_different_books
     skip
     basket = [1, 2, 3]
-    assert_equal 21.60, BookStore.calculate_price(basket)
+    assert_in_delta 21.60, BookStore.calculate_price(basket), 0.001
   end
 
   def test_four_different_books
     skip
     basket = [1, 2, 3, 4]
-    assert_equal 25.60, BookStore.calculate_price(basket)
+    assert_in_delta 25.60, BookStore.calculate_price(basket), 0.001
   end
 
   def test_five_different_books
     skip
     basket = [1, 2, 3, 4, 5]
-    assert_equal 30.00, BookStore.calculate_price(basket)
+    assert_in_delta 30.00, BookStore.calculate_price(basket), 0.001
   end
 
   def test_two_groups_of_four_is_cheaper_than_group_of_five_plus_group_of_three
     skip
     basket = [1, 1, 2, 2, 3, 3, 4, 5]
-    assert_equal 51.20, BookStore.calculate_price(basket)
+    assert_in_delta 51.20, BookStore.calculate_price(basket), 0.001
   end
 
   def test_two_groups_of_four_is_cheaper_than_groups_of_five_and_three
     skip
     basket = [1, 1, 2, 3, 4, 4, 5, 5]
-    assert_equal 51.20, BookStore.calculate_price(basket)
+    assert_in_delta 51.20, BookStore.calculate_price(basket), 0.001
   end
 
   def test_group_of_four_plus_group_of_two_is_cheaper_than_two_groups_of_three
     skip
     basket = [1, 1, 2, 2, 3, 4]
-    assert_equal 40.80, BookStore.calculate_price(basket)
+    assert_in_delta 40.80, BookStore.calculate_price(basket), 0.001
   end
 
-  def test_two_each_of_first_4_books_and_1_copy_each_of_rest
+  def test_two_each_of_first_four_books_and_one_copy_each_of_rest
     skip
     basket = [1, 1, 2, 2, 3, 3, 4, 4, 5]
-    assert_equal 55.60, BookStore.calculate_price(basket)
+    assert_in_delta 55.60, BookStore.calculate_price(basket), 0.001
   end
 
   def test_two_copies_of_each_book
     skip
     basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
-    assert_equal 60.00, BookStore.calculate_price(basket)
+    assert_in_delta 60.00, BookStore.calculate_price(basket), 0.001
   end
 
-  def test_three_copies_of_first_book_and_2_each_of_remaining
+  def test_three_copies_of_first_book_and_two_each_of_remaining
     skip
     basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1]
-    assert_equal 68.00, BookStore.calculate_price(basket)
+    assert_in_delta 68.00, BookStore.calculate_price(basket), 0.001
   end
 
-  def test_three_each_of_first_2_books_and_2_each_of_remaining_books
+  def test_three_each_of_first_two_books_and_two_each_of_remaining_books
     skip
     basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1, 2]
-    assert_equal 75.20, BookStore.calculate_price(basket)
+    assert_in_delta 75.20, BookStore.calculate_price(basket), 0.001
   end
 
   def test_four_groups_of_four_are_cheaper_than_two_groups_each_of_five_and_three
     skip
     basket = [1, 1, 2, 2, 3, 3, 4, 5, 1, 1, 2, 2, 3, 3, 4, 5]
-    assert_equal 102.40, BookStore.calculate_price(basket)
+    assert_in_delta 102.40, BookStore.calculate_price(basket), 0.001
+  end
+
+  def test_check_that_groups_of_four_are_created_properly_even_when_there_are_more_groups_of_three_than_groups_of_five
+    skip
+    basket = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 5, 5]
+    assert_in_delta 145.60, BookStore.calculate_price(basket), 0.001
+  end
+
+  def test_one_group_of_one_and_four_is_cheaper_than_one_group_of_two_and_three
+    skip
+    basket = [1, 1, 2, 3, 4]
+    assert_in_delta 33.60, BookStore.calculate_price(basket), 0.001
+  end
+
+  def test_one_group_of_one_and_two_plus_three_groups_of_four_is_cheaper_than_one_group_of_each_size
+    skip
+    basket = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5]
+    assert_in_delta 100.00, BookStore.calculate_price(basket), 0.001
   end
 end
