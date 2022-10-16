2e132eee2aa5f736aeb6cc6c3ba9c2ec8a216383
Katrina Owen <katrina.owen@gmail.com>
2022-10-16 12:02:42 +0200

    Redesign scale-generator
    This changes the design of the scale-generator class to
    always have a base chromatic scale, and then operate with
    intervals on that scale.
    
    This brings the exercise more in line with the data in
    problem-specifications.

--- exercises/practice/scale-generator/.meta/example.rb
+++ exercises/practice/scale-generator/.meta/example.rb
@@ -1,17 +1,19 @@
 class Scale
   ASCENDING_INTERVALS = %w(m M A)
-  CHROMATIC_SCALE = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+  CHROMATIC_SCALE = %w(C C# D D# E F F# G G# A A# B)
   FLAT_CHROMATIC_SCALE = %w(C Db D Eb E F Gb G Ab A Bb B)
   FLAT_KEYS = %w(F Bb Eb Ab Db Gb d g c f bb eb)
 
-  def initialize(tonic, pattern = nil)
+  def initialize(tonic)
     @tonic = tonic.capitalize
-    @pattern = pattern
     @chromatic_scale = FLAT_KEYS.include?(tonic) ? FLAT_CHROMATIC_SCALE : CHROMATIC_SCALE
   end
 
-  def pitches
-    return reorder_chromatic_scale unless pattern
+  def chromatic
+    reorder_chromatic_scale
+  end
+
+  def interval(pattern)
     last_index = 0
     scale = pattern.each_char.with_object([]) do |c, collector|
       collector << reorder_chromatic_scale[last_index]
@@ -21,10 +23,10 @@ class Scale
 
   private
 
-  attr_reader :tonic, :pattern, :chromatic_scale
+  attr_reader :tonic, :chromatic_scale
 
   def reorder_chromatic_scale
-    return chromatic_scale if tonic == 'C'
+    return chromatic_scale if tonic == "C"
     index = chromatic_scale.index(tonic)
     chromatic_scale[index..-1] + chromatic_scale[0..index - 1]
   end
