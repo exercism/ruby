e5c3c6e9abc2e335a26126031540a0eef4cc4005
Katrina Owen <katrina.owen@gmail.com>
2022-10-16 11:53:43 +0200

    Get rid of scale name in scale-generator
    The code does nothing interesting with the name of the scale.
    It's accepted as an argument to the initialize method, and then
    gets assigned to a value so that it can be read by the test.

--- exercises/practice/scale-generator/.meta/example.rb
+++ exercises/practice/scale-generator/.meta/example.rb
@@ -4,15 +4,10 @@ class Scale
   FLAT_CHROMATIC_SCALE = %w(C Db D Eb E F Gb G Ab A Bb B)
   FLAT_KEYS = %w(F Bb Eb Ab Db Gb d g c f bb eb)
 
-  def initialize(tonic, scale_name, pattern = nil)
+  def initialize(tonic, pattern = nil)
     @tonic = tonic.capitalize
-    @scale_name = scale_name
     @pattern = pattern
     @chromatic_scale = FLAT_KEYS.include?(tonic) ? FLAT_CHROMATIC_SCALE : CHROMATIC_SCALE
-  end
-
-  def name
-    "#{tonic} #{scale_name}"
   end
 
   def pitches
@@ -26,7 +21,7 @@ class Scale
 
   private
 
-  attr_reader :tonic, :scale_name, :pattern, :chromatic_scale
+  attr_reader :tonic, :pattern, :chromatic_scale
 
   def reorder_chromatic_scale
     return chromatic_scale if tonic == 'C'
