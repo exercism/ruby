55066f0d07ed50071787c62ad37363f5d131c58b
Katrina Owen <katrina.owen@gmail.com>
2022-10-16 12:02:42 +0200

    Redesign scale-generator
    This changes the design of the scale-generator class to
    always have a base chromatic scale, and then operate with
    intervals on that scale.
    
    This brings the exercise more in line with the data in
    problem-specifications.

--- exercises/practice/scale-generator/scale_generator_test.rb
+++ exercises/practice/scale-generator/scale_generator_test.rb
@@ -2,131 +2,105 @@ require 'minitest/autorun'
 require_relative 'scale_generator'
 
 class ScaleGeneratorTest < Minitest::Test
-  def test_chromatic_scale
-    skip
-    chromatic = Scale.new('C')
-    expected = %w[C C# D D# E F F# G G# A A# B]
-    actual = chromatic.pitches
-    assert_equal expected, actual
+  def test_chromatic_scales_chromatic_scale_with_sharps
+    # skip
+    scale = Scale.new("C")
+    assert_equal ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"], scale.chromatic
   end
 
-  def test_another_chromatic_scale
+  def test_chromatic_scales_chromatic_scale_with_flats
     skip
-    chromatic = Scale.new('F')
-    expected = %w[F Gb G Ab A Bb B C Db D Eb E]
-    actual = chromatic.pitches
-    assert_equal expected, actual
+    scale = Scale.new("F")
+    assert_equal %w[F Gb G Ab A Bb B C Db D Eb E], scale.chromatic
   end
 
-  def test_major_scale
+  def test_scales_with_specified_intervals_simple_major_scale
     skip
-    major = Scale.new('C', 'MMmMMMm')
-    expected = %w[C D E F G A B]
-    actual = major.pitches
-    assert_equal expected, actual
+    scale = Scale.new("C")
+    assert_equal %w[C D E F G A B], scale.interval("MMmMMMm")
   end
 
-  def test_another_major_scale
+  def test_scales_with_specified_intervals_major_scale_with_sharps
     skip
-    major = Scale.new('G', 'MMmMMMm')
-    expected = %w[G A B C D E F#]
-    actual = major.pitches
-    assert_equal expected, actual
+    scale = Scale.new("G")
+    assert_equal ["G", "A", "B", "C", "D", "E", "F#"], scale.interval("MMmMMMm")
   end
 
-  def test_minor_scale
+  def test_scales_with_specified_intervals_major_scale_with_flats
     skip
-    minor = Scale.new('f#', 'MmMMmMM')
-    expected = %w[F# G# A B C# D E]
-    actual = minor.pitches
-    assert_equal expected, actual
+    scale = Scale.new("F")
+    assert_equal %w[F G A Bb C D E], scale.interval("MMmMMMm")
   end
 
-  def test_another_minor_scale
+  def test_scales_with_specified_intervals_minor_scale_with_sharps
     skip
-    minor = Scale.new('bb', 'MmMMmMM')
-    expected = %w[Bb C Db Eb F Gb Ab]
-    actual = minor.pitches
-    assert_equal expected, actual
+    scale = Scale.new("f#")
+    assert_equal ["F#", "G#", "A", "B", "C#", "D", "E"], scale.interval("MmMMmMM")
   end
 
-  def test_dorian_mode
+  def test_scales_with_specified_intervals_minor_scale_with_flats
     skip
-    dorian = Scale.new('d', 'MmMMMmM')
-    expected = %w[D E F G A B C]
-    actual = dorian.pitches
-    assert_equal expected, actual
+    scale = Scale.new("bb")
+    assert_equal %w[Bb C Db Eb F Gb Ab], scale.interval("MmMMmMM")
   end
 
-  def test_mixolydian_mode
+  def test_scales_with_specified_intervals_dorian_mode
     skip
-    mixolydian = Scale.new('Eb', 'MMmMMmM')
-    expected = %w[Eb F G Ab Bb C Db]
-    actual = mixolydian.pitches
-    assert_equal expected, actual
+    scale = Scale.new("d")
+    assert_equal %w[D E F G A B C], scale.interval("MmMMMmM")
   end
 
-  def test_lydian_mode
+  def test_scales_with_specified_intervals_mixolydian_mode
     skip
-    lydian = Scale.new('a', 'MMMmMMm')
-    expected = %w[A B C# D# E F# G#]
-    actual = lydian.pitches
-    assert_equal expected, actual
+    scale = Scale.new("Eb")
+    assert_equal %w[Eb F G Ab Bb C Db], scale.interval("MMmMMmM")
   end
 
-  def test_phrygian_mode
+  def test_scales_with_specified_intervals_lydian_mode
     skip
-    phrygian = Scale.new('e', 'mMMMmMM')
-    expected = %w[E F G A B C D]
-    actual = phrygian.pitches
-    assert_equal expected, actual
+    scale = Scale.new("a")
+    assert_equal ["A", "B", "C#", "D#", "E", "F#", "G#"], scale.interval("MMMmMMm")
   end
 
-  def test_locrian_mode
+  def test_scales_with_specified_intervals_phrygian_mode
     skip
-    locrian = Scale.new('g', 'mMMmMMM')
-    expected = %w[G Ab Bb C Db Eb F]
-    actual = locrian.pitches
-    assert_equal expected, actual
+    scale = Scale.new("e")
+    assert_equal %w[E F G A B C D], scale.interval("mMMMmMM")
   end
 
-  def test_harmonic_minor
+  def test_scales_with_specified_intervals_locrian_mode
     skip
-    harmonic_minor = Scale.new('d', 'MmMMmAm')
-    expected = %w[D E F G A Bb Db]
-    actual = harmonic_minor.pitches
-    assert_equal expected, actual
+    scale = Scale.new("g")
+    assert_equal %w[G Ab Bb C Db Eb F], scale.interval("mMMmMMM")
   end
 
-  def test_octatonic
+  def test_scales_with_specified_intervals_harmonic_minor
     skip
-    octatonic = Scale.new('C', 'MmMmMmMm')
-    expected = %w[C D D# F F# G# A B]
-    actual = octatonic.pitches
-    assert_equal expected, actual
+    scale = Scale.new("d")
+    assert_equal %w[D E F G A Bb Db], scale.interval("MmMMmAm")
   end
 
-  def test_hexatonic
+  def test_scales_with_specified_intervals_octatonic
     skip
-    hexatonic = Scale.new('Db', 'MMMMMM')
-    expected = %w[Db Eb F G A B]
-    actual = hexatonic.pitches
-    assert_equal expected, actual
+    scale = Scale.new("C")
+    assert_equal ["C", "D", "D#", "F", "F#", "G#", "A", "B"], scale.interval("MmMmMmMm")
   end
 
-  def test_pentatonic
+  def test_scales_with_specified_intervals_hexatonic
     skip
-    pentatonic = Scale.new('A', 'MMAMA')
-    expected = %w[A B C# E F#]
-    actual = pentatonic.pitches
-    assert_equal expected, actual
+    scale = Scale.new("Db")
+    assert_equal %w[Db Eb F G A B], scale.interval("MMMMMM")
   end
 
-  def test_enigmatic
+  def test_scales_with_specified_intervals_pentatonic
     skip
-    enigmatic = Scale.new('G', 'mAMMMmM')
-    expected = %w[G G# B C# D# F F#]
-    actual = enigmatic.pitches
-    assert_equal expected, actual
+    scale = Scale.new("A")
+    assert_equal ["A", "B", "C#", "E", "F#"], scale.interval("MMAMA")
+  end
+
+  def test_scales_with_specified_intervals_enigmatic
+    skip
+    scale = Scale.new("G")
+    assert_equal ["G", "G#", "B", "C#", "D#", "F", "F#"], scale.interval("mAMMMmm")
   end
 end
