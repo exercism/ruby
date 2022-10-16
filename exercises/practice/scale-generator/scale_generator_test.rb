cca5117da62be74f539c3cdccf037915804361ae
Katrina Owen <katrina.owen@gmail.com>
2022-10-16 11:53:43 +0200

    Get rid of scale name in scale-generator
    The code does nothing interesting with the name of the scale.
    It's accepted as an argument to the initialize method, and then
    gets assigned to a value so that it can be read by the test.

--- exercises/practice/scale-generator/scale_generator_test.rb
+++ exercises/practice/scale-generator/scale_generator_test.rb
@@ -2,16 +2,9 @@ require 'minitest/autorun'
 require_relative 'scale_generator'
 
 class ScaleGeneratorTest < Minitest::Test
-  def test_naming_scale
-    chromatic = Scale.new('c', :chromatic)
-    expected = 'C chromatic'
-    actual = chromatic.name
-    assert_equal expected, actual
-  end
-
   def test_chromatic_scale
     skip
-    chromatic = Scale.new('C', :chromatic)
+    chromatic = Scale.new('C')
     expected = %w[C C# D D# E F F# G G# A A# B]
     actual = chromatic.pitches
     assert_equal expected, actual
@@ -19,23 +12,15 @@ class ScaleGeneratorTest < Minitest::Test
 
   def test_another_chromatic_scale
     skip
-    chromatic = Scale.new('F', :chromatic)
+    chromatic = Scale.new('F')
     expected = %w[F Gb G Ab A Bb B C Db D Eb E]
     actual = chromatic.pitches
     assert_equal expected, actual
   end
 
-  def test_naming_major_scale
-    skip
-    major = Scale.new('G', :major, 'MMmMMMm')
-    expected = 'G major'
-    actual = major.name
-    assert_equal expected, actual
-  end
-
   def test_major_scale
     skip
-    major = Scale.new('C', :major, 'MMmMMMm')
+    major = Scale.new('C', 'MMmMMMm')
     expected = %w[C D E F G A B]
     actual = major.pitches
     assert_equal expected, actual
@@ -43,7 +28,7 @@ class ScaleGeneratorTest < Minitest::Test
 
   def test_another_major_scale
     skip
-    major = Scale.new('G', :major, 'MMmMMMm')
+    major = Scale.new('G', 'MMmMMMm')
     expected = %w[G A B C D E F#]
     actual = major.pitches
     assert_equal expected, actual
@@ -51,7 +36,7 @@ class ScaleGeneratorTest < Minitest::Test
 
   def test_minor_scale
     skip
-    minor = Scale.new('f#', :minor, 'MmMMmMM')
+    minor = Scale.new('f#', 'MmMMmMM')
     expected = %w[F# G# A B C# D E]
     actual = minor.pitches
     assert_equal expected, actual
@@ -59,7 +44,7 @@ class ScaleGeneratorTest < Minitest::Test
 
   def test_another_minor_scale
     skip
-    minor = Scale.new('bb', :minor, 'MmMMmMM')
+    minor = Scale.new('bb', 'MmMMmMM')
     expected = %w[Bb C Db Eb F Gb Ab]
     actual = minor.pitches
     assert_equal expected, actual
@@ -67,7 +52,7 @@ class ScaleGeneratorTest < Minitest::Test
 
   def test_dorian_mode
     skip
-    dorian = Scale.new('d', :dorian, 'MmMMMmM')
+    dorian = Scale.new('d', 'MmMMMmM')
     expected = %w[D E F G A B C]
     actual = dorian.pitches
     assert_equal expected, actual
@@ -75,7 +60,7 @@ class ScaleGeneratorTest < Minitest::Test
 
   def test_mixolydian_mode
     skip
-    mixolydian = Scale.new('Eb', :mixolydian, 'MMmMMmM')
+    mixolydian = Scale.new('Eb', 'MMmMMmM')
     expected = %w[Eb F G Ab Bb C Db]
     actual = mixolydian.pitches
     assert_equal expected, actual
@@ -83,7 +68,7 @@ class ScaleGeneratorTest < Minitest::Test
 
   def test_lydian_mode
     skip
-    lydian = Scale.new('a', :lydian, 'MMMmMMm')
+    lydian = Scale.new('a', 'MMMmMMm')
     expected = %w[A B C# D# E F# G#]
     actual = lydian.pitches
     assert_equal expected, actual
@@ -91,7 +76,7 @@ class ScaleGeneratorTest < Minitest::Test
 
   def test_phrygian_mode
     skip
-    phrygian = Scale.new('e', :phrygian, 'mMMMmMM')
+    phrygian = Scale.new('e', 'mMMMmMM')
     expected = %w[E F G A B C D]
     actual = phrygian.pitches
     assert_equal expected, actual
@@ -99,7 +84,7 @@ class ScaleGeneratorTest < Minitest::Test
 
   def test_locrian_mode
     skip
-    locrian = Scale.new('g', :locrian, 'mMMmMMM')
+    locrian = Scale.new('g', 'mMMmMMM')
     expected = %w[G Ab Bb C Db Eb F]
     actual = locrian.pitches
     assert_equal expected, actual
@@ -107,7 +92,7 @@ class ScaleGeneratorTest < Minitest::Test
 
   def test_harmonic_minor
     skip
-    harmonic_minor = Scale.new('d', :harmonic_minor, 'MmMMmAm')
+    harmonic_minor = Scale.new('d', 'MmMMmAm')
     expected = %w[D E F G A Bb Db]
     actual = harmonic_minor.pitches
     assert_equal expected, actual
@@ -115,7 +100,7 @@ class ScaleGeneratorTest < Minitest::Test
 
   def test_octatonic
     skip
-    octatonic = Scale.new('C', :octatonic, 'MmMmMmMm')
+    octatonic = Scale.new('C', 'MmMmMmMm')
     expected = %w[C D D# F F# G# A B]
     actual = octatonic.pitches
     assert_equal expected, actual
@@ -123,7 +108,7 @@ class ScaleGeneratorTest < Minitest::Test
 
   def test_hexatonic
     skip
-    hexatonic = Scale.new('Db', :hexatonic, 'MMMMMM')
+    hexatonic = Scale.new('Db', 'MMMMMM')
     expected = %w[Db Eb F G A B]
     actual = hexatonic.pitches
     assert_equal expected, actual
@@ -131,7 +116,7 @@ class ScaleGeneratorTest < Minitest::Test
 
   def test_pentatonic
     skip
-    pentatonic = Scale.new('A', :pentatonic, 'MMAMA')
+    pentatonic = Scale.new('A', 'MMAMA')
     expected = %w[A B C# E F#]
     actual = pentatonic.pitches
     assert_equal expected, actual
@@ -139,7 +124,7 @@ class ScaleGeneratorTest < Minitest::Test
 
   def test_enigmatic
     skip
-    enigmatic = Scale.new('G', :enigma, 'mAMMMmM')
+    enigmatic = Scale.new('G', 'mAMMMmM')
     expected = %w[G G# B C# D# F F#]
     actual = enigmatic.pitches
     assert_equal expected, actual
