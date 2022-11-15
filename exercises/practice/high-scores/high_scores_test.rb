987a1e8e87367dca1bcd7aaf57575358e76f7f42
Katrina Owen <katrina.owen@gmail.com>
2022-11-15 11:03:16 +0100

    Add immutibility test cases for high-scores
    When I was originally updating this exercise from problem-specifications
    I misunderstood the meaning of the test cases that have properties named
    `*After*`. I completely missed that these were tagged with the
    `immutable` scenario.
    
    These are not actual properties, but named scenarios that
    describe expectations around immutability.
    
    I have now implemented these test cases, since the Ruby implementation
    would allow students to create an implementation that mutates
    internal state in a way that would break the expectations around
    `.latest` and `personal_best`.

--- exercises/practice/high-scores/high_scores_test.rb
+++ exercises/practice/high-scores/high_scores_test.rb
@@ -50,6 +50,34 @@ class HighScoresTest < Minitest::Test
     assert_equal [40], HighScores.new(scores).personal_top_three
   end
 
+  def test_top_3_scores_latest_score_after_personal_top_scores
+    skip
+    high_scores = HighScores.new([70, 50, 20, 30])
+    high_scores.personal_top_three
+    assert_equal 30, high_scores.latest
+  end
+
+  def test_top_3_scores_scores_after_personal_top_scores
+    skip
+    high_scores = HighScores.new([30, 50, 20, 70])
+    high_scores.personal_top_three
+    assert_equal [30, 50, 20, 70], high_scores.scores
+  end
+
+  def test_top_3_scores_latest_score_after_personal_best
+    skip
+    high_scores = HighScores.new([20, 70, 15, 25, 30])
+    high_scores.personal_best
+    assert_equal 30, high_scores.latest
+  end
+
+  def test_top_3_scores_scores_after_personal_best
+    skip
+    high_scores = HighScores.new([20, 70, 15, 25, 30])
+    high_scores.personal_best
+    assert_equal [20, 70, 15, 25, 30], high_scores.scores
+  end
+
   def test_latest_score_is_not_the_personal_best
     skip
     scores = [100, 40, 10, 70]
