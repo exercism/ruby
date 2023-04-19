a1f45de39a9106731efe2c636741a327f1274b00
Katrina Owen <katrina.owen@gmail.com>
2023-04-19 16:08:51 +0200

    Delete old generator
    This generator does not work with V3.
    We are replacing it with a less clever,
    language-agnostic generator.

--- exercises/practice/resistor-color-duo/.meta/generator/resistor_color_duo_case.rb
+++
@@ -1,7 +0,0 @@
-require 'generator/exercise_case'
-
-class ResistorColorDuoCase < Generator::ExerciseCase
-  def workload
-    assert_equal(expected, "ResistorColorDuo.value(#{colors})")
-  end
-end
