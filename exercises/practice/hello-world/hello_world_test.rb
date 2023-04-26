1813f6267ac6040ebccaf96203b6946ef2a1359b
Katrina Owen <katrina.owen@gmail.com>
2023-04-26 10:29:40 +0200

    Delete hand-holding in hello-world
    Exercism has evolved a lot.
    It has become clear that it is about teaching the language
    to programmers.
    It is not about teaching TDD.
    
    If we want to teach TDD, we will do it as a feature of the
    platform, not a hand-crafted course for a single track.

--- exercises/practice/hello-world/hello_world_test.rb
+++ exercises/practice/hello-world/hello_world_test.rb
@@ -17,26 +17,3 @@ class HelloWorldTest < Minitest::Test
     assert_equal "Hello, World!", HelloWorld.hello
   end
 end
-
-__END__
-
-*****************************************************
-You got an error, which is exactly as it should be.
-This is the first step in the Test-Driven Development
-(TDD) process.
-
-The most important part of the error is
-
-   cannot load such file
-
-It's looking for a file named hello_world.rb that doesn't
-exist yet.
-
-To fix the error, create an empty file named hello_world.rb
-in the same directory as the hello_world_test.rb file.
-
-Then run the test again.
-
-For more guidance as you work on this exercise, see
-GETTING_STARTED.md.
-*****************************************************
