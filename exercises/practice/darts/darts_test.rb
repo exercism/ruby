5fb2a1a5fc0e03203f792481c4df413f23912b5c
Katrina Owen <katrina.owen@gmail.com>
2023-04-26 20:04:19 +0200

    Regenerate darts

--- exercises/practice/darts/darts_test.rb
+++ exercises/practice/darts/darts_test.rb
@@ -4,79 +4,66 @@ require_relative 'darts'
 class DartsTest < Minitest::Test
   def test_missed_target
     # skip
-    darts = Darts.new(-9, 9)
-    assert_equal(0, darts.score)
+    assert_equal 0, Darts.new(-9, 9).score
   end
 
   def test_on_the_outer_circle
     skip
-    darts = Darts.new(0, 10)
-    assert_equal(1, darts.score)
+    assert_equal 1, Darts.new(0, 10).score
   end
 
   def test_on_the_middle_circle
     skip
-    darts = Darts.new(-5, 0)
-    assert_equal(5, darts.score)
+    assert_equal 5, Darts.new(-5, 0).score
   end
 
   def test_on_the_inner_circle
     skip
-    darts = Darts.new(0, -1)
-    assert_equal(10, darts.score)
+    assert_equal 10, Darts.new(0, -1).score
   end
 
-  def test_exactly_on_centre
+  def test_exactly_on_center
     skip
-    darts = Darts.new(0, 0)
-    assert_equal(10, darts.score)
+    assert_equal 10, Darts.new(0, 0).score
   end
 
-  def test_near_the_centre
+  def test_near_the_center
     skip
-    darts = Darts.new(-0.1, -0.1)
-    assert_equal(10, darts.score)
+    assert_equal 10, Darts.new(-0.1, -0.1).score
   end
 
   def test_just_within_the_inner_circle
     skip
-    darts = Darts.new(0.7, 0.7)
-    assert_equal(10, darts.score)
+    assert_equal 10, Darts.new(0.7, 0.7).score
   end
 
   def test_just_outside_the_inner_circle
     skip
-    darts = Darts.new(0.8, -0.8)
-    assert_equal(5, darts.score)
+    assert_equal 5, Darts.new(0.8, -0.8).score
   end
 
   def test_just_within_the_middle_circle
     skip
-    darts = Darts.new(-3.5, 3.5)
-    assert_equal(5, darts.score)
+    assert_equal 5, Darts.new(-3.5, 3.5).score
   end
 
   def test_just_outside_the_middle_circle
     skip
-    darts = Darts.new(-3.6, -3.6)
-    assert_equal(1, darts.score)
+    assert_equal 1, Darts.new(-3.6, -3.6).score
   end
 
   def test_just_within_the_outer_circle
     skip
-    darts = Darts.new(-7.0, 7.0)
-    assert_equal(1, darts.score)
+    assert_equal 1, Darts.new(-7.0, 7.0).score
   end
 
   def test_just_outside_the_outer_circle
     skip
-    darts = Darts.new(7.1, -7.1)
-    assert_equal(0, darts.score)
+    assert_equal 0, Darts.new(7.1, -7.1).score
   end
 
   def test_asymmetric_position_between_the_inner_and_middle_circles
     skip
-    darts = Darts.new(0.5, -4)
-    assert_equal(5, darts.score)
+    assert_equal 5, Darts.new(0.5, -4).score
   end
 end
