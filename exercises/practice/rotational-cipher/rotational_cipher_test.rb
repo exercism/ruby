06fbee15ac89a69606cb4481b9da30a019c1b006
Katrina Owen <katrina.owen@gmail.com>
2023-04-27 09:18:46 +0200

    Generate rotational-cipher

--- exercises/practice/rotational-cipher/rotational_cipher_test.rb
+++ exercises/practice/rotational-cipher/rotational_cipher_test.rb
@@ -4,52 +4,61 @@ require_relative 'rotational_cipher'
 class RotationalCipherTest < Minitest::Test
   def test_rotate_a_by_0_same_output_as_input
     # skip
-    assert_equal "a", RotationalCipher.rotate("a", 0)
+    text = "a"
+    assert_equal "a", RotationalCipher.rotate(text, 0)
   end
 
   def test_rotate_a_by_1
     skip
-    assert_equal "b", RotationalCipher.rotate("a", 1)
+    text = "a"
+    assert_equal "b", RotationalCipher.rotate(text, 1)
   end
 
   def test_rotate_a_by_26_same_output_as_input
     skip
-    assert_equal "a", RotationalCipher.rotate("a", 26)
+    text = "a"
+    assert_equal "a", RotationalCipher.rotate(text, 26)
   end
 
   def test_rotate_m_by_13
     skip
-    assert_equal "z", RotationalCipher.rotate("m", 13)
+    text = "m"
+    assert_equal "z", RotationalCipher.rotate(text, 13)
   end
 
   def test_rotate_n_by_13_with_wrap_around_alphabet
     skip
-    assert_equal "a", RotationalCipher.rotate("n", 13)
+    text = "n"
+    assert_equal "a", RotationalCipher.rotate(text, 13)
   end
 
   def test_rotate_capital_letters
     skip
-    assert_equal "TRL", RotationalCipher.rotate("OMG", 5)
+    text = "OMG"
+    assert_equal "TRL", RotationalCipher.rotate(text, 5)
   end
 
   def test_rotate_spaces
     skip
-    assert_equal "T R L", RotationalCipher.rotate("O M G", 5)
+    text = "O M G"
+    assert_equal "T R L", RotationalCipher.rotate(text, 5)
   end
 
   def test_rotate_numbers
     skip
-    assert_equal "Xiwxmrk 1 2 3 xiwxmrk", RotationalCipher.rotate("Testing 1 2 3 testing", 4)
+    text = "Testing 1 2 3 testing"
+    assert_equal "Xiwxmrk 1 2 3 xiwxmrk", RotationalCipher.rotate(text, 4)
   end
 
   def test_rotate_punctuation
     skip
-    assert_equal "Gzo'n zvo, Bmviyhv!", RotationalCipher.rotate("Let's eat, Grandma!", 21)
+    text = "Let's eat, Grandma!"
+    assert_equal "Gzo'n zvo, Bmviyhv!", RotationalCipher.rotate(text, 21)
   end
 
   def test_rotate_all_letters
     skip
-    assert_equal "Gur dhvpx oebja sbk whzcf bire gur ynml qbt.",
-      RotationalCipher.rotate("The quick brown fox jumps over the lazy dog.", 13)
+    text = "The quick brown fox jumps over the lazy dog."
+    assert_equal "Gur dhvpx oebja sbk whzcf bire gur ynml qbt.", RotationalCipher.rotate(text, 13)
   end
 end
