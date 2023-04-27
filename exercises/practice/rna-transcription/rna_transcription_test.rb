bff429ab9d8b84bdea7cb8de8a4169169f55b378
Katrina Owen <katrina.owen@gmail.com>
2023-04-27 09:12:01 +0200

    Generate rna-description

--- exercises/practice/rna-transcription/rna_transcription_test.rb
+++ exercises/practice/rna-transcription/rna_transcription_test.rb
@@ -4,31 +4,31 @@ require_relative 'rna_transcription'
 class RnaTranscriptionTest < Minitest::Test
   def test_empty_rna_sequence
     # skip
-    assert_equal '', Complement.of_dna('')
+    assert_equal "", Complement.of_dna("")
   end
 
   def test_rna_complement_of_cytosine_is_guanine
     skip
-    assert_equal 'G', Complement.of_dna('C')
+    assert_equal "G", Complement.of_dna("C")
   end
 
   def test_rna_complement_of_guanine_is_cytosine
     skip
-    assert_equal 'C', Complement.of_dna('G')
+    assert_equal "C", Complement.of_dna("G")
   end
 
   def test_rna_complement_of_thymine_is_adenine
     skip
-    assert_equal 'A', Complement.of_dna('T')
+    assert_equal "A", Complement.of_dna("T")
   end
 
   def test_rna_complement_of_adenine_is_uracil
     skip
-    assert_equal 'U', Complement.of_dna('A')
+    assert_equal "U", Complement.of_dna("A")
   end
 
   def test_rna_complement
     skip
-    assert_equal 'UGCACCAGAAUU', Complement.of_dna('ACGTGGTCTTAA')
+    assert_equal "UGCACCAGAAUU", Complement.of_dna("ACGTGGTCTTAA")
   end
 end
