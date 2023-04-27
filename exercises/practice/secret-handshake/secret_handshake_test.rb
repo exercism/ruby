d3e5af0203c2cffe5e6dc92024d9ec4c758ee57d
Katrina Owen <katrina.owen@gmail.com>
2023-04-27 09:37:56 +0200

    Generate secret-handshake

--- exercises/practice/secret-handshake/secret_handshake_test.rb
+++ exercises/practice/secret-handshake/secret_handshake_test.rb
@@ -2,51 +2,80 @@ require 'minitest/autorun'
 require_relative 'secret_handshake'
 
 class SecretHandshakeTest < Minitest::Test
-  def test_handshake_1_to_wink
-    handshake = SecretHandshake.new(1)
-    assert_equal ['wink'], handshake.commands
+  def test_wink_for_1
+    # skip
+    secret_handshake = SecretHandshake.new(1)
+    expected = ["wink"]
+    assert_equal expected, secret_handshake.commands
   end
 
-  def test_handshake_10_to_double_blink
+  def test_double_blink_for_10
     skip
-    handshake = SecretHandshake.new(2)
-    assert_equal ['double blink'], handshake.commands
+    secret_handshake = SecretHandshake.new(2)
+    expected = ["double blink"]
+    assert_equal expected, secret_handshake.commands
   end
 
-  def test_handshake_100_to_close_your_eyes
+  def test_close_your_eyes_for_100
     skip
-    handshake = SecretHandshake.new(4)
-    assert_equal ['close your eyes'], handshake.commands
+    secret_handshake = SecretHandshake.new(4)
+    expected = ["close your eyes"]
+    assert_equal expected, secret_handshake.commands
   end
 
-  def test_handshake_1000_to_jump
+  def test_jump_for_1000
     skip
-    handshake = SecretHandshake.new(8)
-    assert_equal ['jump'], handshake.commands
+    secret_handshake = SecretHandshake.new(8)
+    expected = ["jump"]
+    assert_equal expected, secret_handshake.commands
   end
 
-  def test_handshake_11_to_wink_and_double_blink
+  def test_combine_two_actions
     skip
-    handshake = SecretHandshake.new(3)
-    assert_equal ['wink', 'double blink'], handshake.commands
+    secret_handshake = SecretHandshake.new(3)
+    expected = ["wink", "double blink"]
+    assert_equal expected, secret_handshake.commands
   end
 
-  def test_handshake_10011_to_double_blink_and_wink
+  def test_reverse_two_actions
     skip
-    handshake = SecretHandshake.new(19)
-    assert_equal ['double blink', 'wink'], handshake.commands
+    secret_handshake = SecretHandshake.new(19)
+    expected = ["double blink", "wink"]
+    assert_equal expected, secret_handshake.commands
   end
 
-  def test_handshake_11111_to_double_blink_and_wink
+  def test_reversing_one_action_gives_the_same_action
     skip
-    handshake = SecretHandshake.new(31)
-    expected = ['jump', 'close your eyes', 'double blink', 'wink']
-    assert_equal expected, handshake.commands
+    secret_handshake = SecretHandshake.new(24)
+    expected = ["jump"]
+    assert_equal expected, secret_handshake.commands
   end
 
-  def test_invalid_handshake
+  def test_reversing_no_actions_still_gives_no_actions
     skip
-    handshake = SecretHandshake.new('piggies')
-    assert_empty handshake.commands
+    secret_handshake = SecretHandshake.new(16)
+    expected = []
+    assert_equal expected, secret_handshake.commands
+  end
+
+  def test_all_possible_actions
+    skip
+    secret_handshake = SecretHandshake.new(15)
+    expected = ["wink", "double blink", "close your eyes", "jump"]
+    assert_equal expected, secret_handshake.commands
+  end
+
+  def test_reverse_all_possible_actions
+    skip
+    secret_handshake = SecretHandshake.new(31)
+    expected = ["jump", "close your eyes", "double blink", "wink"]
+    assert_equal expected, secret_handshake.commands
+  end
+
+  def test_do_nothing_for_zero
+    skip
+    secret_handshake = SecretHandshake.new(0)
+    expected = []
+    assert_equal expected, secret_handshake.commands
   end
 end
