951452c6e80ff343ecd42929f361dfc1e30b2f84
Katrina Owen <katrina.owen@gmail.com>
2023-04-27 08:18:47 +0200

    Generate flatten-array

--- exercises/practice/flatten-array/flatten_array_test.rb
+++ exercises/practice/flatten-array/flatten_array_test.rb
@@ -4,46 +4,57 @@ require_relative 'flatten_array'
 class FlattenArrayTest < Minitest::Test
   def test_empty
     # skip
-    assert_empty FlattenArray.flatten([])
+    input = []
+    expected = []
+    assert_equal expected, FlattenArray.flatten(input)
   end
 
   def test_no_nesting
     skip
-    flat_array = FlattenArray.flatten([0, 1, 2])
-    assert_equal [0, 1, 2], flat_array
+    input = [0, 1, 2]
+    expected = [0, 1, 2]
+    assert_equal expected, FlattenArray.flatten(input)
   end
 
   def test_flattens_a_nested_array
     skip
-    assert_empty FlattenArray.flatten([[[]]])
+    input = [[[]]]
+    expected = []
+    assert_equal expected, FlattenArray.flatten(input)
   end
 
   def test_flattens_array_with_just_integers_present
     skip
-    flat_array = FlattenArray.flatten([1, [2, 3, 4, 5, 6, 7], 8])
-    assert_equal [1, 2, 3, 4, 5, 6, 7, 8], flat_array
+    input = [1, [2, 3, 4, 5, 6, 7], 8]
+    expected = [1, 2, 3, 4, 5, 6, 7, 8]
+    assert_equal expected, FlattenArray.flatten(input)
   end
 
   def test_5_level_nesting
     skip
-    flat_array = FlattenArray.flatten([0, 2, [[2, 3], 8, 100, 4, [[[50]]]], -2])
-    assert_equal [0, 2, 2, 3, 8, 100, 4, 50, -2], flat_array
+    input = [0, 2, [[2, 3], 8, 100, 4, [[[50]]]], -2]
+    expected = [0, 2, 2, 3, 8, 100, 4, 50, -2]
+    assert_equal expected, FlattenArray.flatten(input)
   end
 
   def test_6_level_nesting
     skip
-    flat_array = FlattenArray.flatten([1, [2, [[3]], [4, [[5]]], 6, 7], 8])
-    assert_equal [1, 2, 3, 4, 5, 6, 7, 8], flat_array
+    input = [1, [2, [[3]], [4, [[5]]], 6, 7], 8]
+    expected = [1, 2, 3, 4, 5, 6, 7, 8]
+    assert_equal expected, FlattenArray.flatten(input)
   end
 
   def test_6_level_nest_list_with_null_values
     skip
-    flat_array = FlattenArray.flatten([0, 2, [[2, 3], 8, [[100]], nil, [[nil]]], -2])
-    assert_equal [0, 2, 2, 3, 8, 100, -2], flat_array
+    input = [0, 2, [[2, 3], 8, [[100]], nil, [[nil]]], -2]
+    expected = [0, 2, 2, 3, 8, 100, -2]
+    assert_equal expected, FlattenArray.flatten(input)
   end
 
   def test_all_values_in_nested_list_are_null
     skip
-    assert_empty FlattenArray.flatten([nil, [[[nil]]], nil, nil, [[nil, nil], nil], nil])
+    input = [nil, [[[nil]]], nil, nil, [[nil, nil], nil], nil]
+    expected = []
+    assert_equal expected, FlattenArray.flatten(input)
   end
 end
