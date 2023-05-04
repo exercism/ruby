dbbbcd5133621b3100e3666d30b383efcbaa3bb1
Katrina Owen <katrina.owen@gmail.com>
2023-05-04 19:11:39 +0200

    Reimplement minesweeper
    This exercise was originally implemented before the
    shared spec was created.
    
    This reimplements the exercise per the spec.
    
    Note that the tests.toml (incorrectly) had all of these
    tests marked as being included, so no changes to the test
    metadata were necessary.

--- exercises/practice/minesweeper/.meta/example.rb
+++ exercises/practice/minesweeper/.meta/example.rb
@@ -1,88 +1,73 @@
-class Board
-  VALID_BORDERS = ['+', '-', '|']
-  VALID_DATA = ['*', '|', '+', '-', ' ']
-
-  def self.transform(input)
-    new(input).transform
+class Minesweeper
+  def self.annotate(minefield)
+    new(minefield).annotate
   end
 
-  def initialize(raw_board)
-    @rows = raw_board
-    validate
+  attr_reader :minefield
+  def initialize(minefield)
+    @minefield = minefield
   end
 
-  def transform
-    rows.map.with_index do |row, i|
-      decorate_row(row, i)
+  def annotate
+    (0..rows).map do |row|
+      (0..columns).map do |column|
+        notation_at(row, column)
+      end.join("")
     end
-  end
-
-  def mine?(char)
-    char == '*'
   end
 
   private
 
-  attr_reader :rows
+  def notation_at(row, column)
+    if mine_coordinates.include?([row, column])
+      return "*"
+    end
+    mines = surrounding_coordinates(row, column).count {|x, y|
+      mine_coordinates.include?([x, y])
+    }
+    if mines.zero?
+      " "
+    else
+      mines.to_s
+    end
+  end
 
-  def decorate_row(row, i)
-    inner = []
-    row.each_char.with_index do |space, j|
-      if space != ' '
-        inner << space
-      else
-        surroundings = surroundings(row, i, j)
-        k = count_mines_nearby(surroundings)
-        if k > 0
-          inner << k.to_s
-        else
-          inner << space
+  def surrounding_coordinates(row, column)
+    [
+      [row-1, column-1],
+      [row-1, column],
+      [row-1, column+1],
+      [row, column-1],
+      [row, column+1],
+      [row+1, column-1],
+      [row+1, column],
+      [row+1, column+1]
+    ].reject {|x, y| invalid_coordinate(x, y)}
+  end
+
+  def invalid_coordinate(x, y)
+    x < 0 || y < 0 || x > rows || y > columns
+  end
+
+  def mine_coordinates
+    return @mine_coordinates if @mine_coordinates
+
+    @mine_coordinates = []
+    minefield.each.with_index do |row, x|
+      row.chars.each.with_index do |cell, y|
+        if cell == "*"
+          @mine_coordinates << [x, y]
         end
       end
     end
-    inner.join
+    @mine_coordinates
   end
 
-  def surroundings(row, i, j)
-    [
-      row[j - 1], row[j + 1], rows[i - 1][j - 1],
-      rows[i - 1][j], rows[i - 1][j + 1],
-      rows[i + 1][j - 1], rows[i + 1][j], rows[i + 1][j + 1]
-    ]
+  def rows
+    @rows ||= minefield.size-1
   end
 
-  def count_mines_nearby(surroundings)
-    surroundings.count { |datum| mine?(datum) }
-  end
-
-  def validate
-    validate_size
-    validate_borders
-    validate_data
-  end
-
-  def validate_size
-    len = rows.first.length
-    if rows.any? { |row| row.length != len }
-      fail ArgumentError, 'Invalid board'
-    end
-  end
-
-  def validate_borders
-    [rows[0], rows[-1]].each do |row|
-      invalid = row.chars.any? do |char|
-        !VALID_BORDERS.include?(char)
-      end
-      fail ArgumentError, 'Invalid board' if invalid
-    end
-  end
-
-  def validate_data
-    rows.each do |row|
-      invalid = row.chars.any? do |char|
-        !VALID_DATA.include?(char)
-      end
-      fail ArgumentError, 'Invalid board' if invalid
-    end
+  def columns
+    @columns ||= minefield.first.size-1
   end
 end
