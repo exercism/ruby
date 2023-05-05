5d52fbe10d1c870875c9183bc3d9b6c3839254da
Katrina Owen <katrina.owen@gmail.com>
2023-05-05 10:45:41 +0200

    Remove redundant tests in gigasecond
    The gigasecond second is fully specified with only the two test
    cases that pass in a fully qualified time.
    
    Let's get rid of the other test cases.

--- exercises/practice/gigasecond/gigasecond_test.rb
+++ exercises/practice/gigasecond/gigasecond_test.rb
@@ -2,21 +2,6 @@ require 'minitest/autorun'
 require_relative 'gigasecond'
 
 class GigasecondTest < Minitest::Test
-  def test_date_only_specification_of_time
-    # skip
-    assert_equal Time.utc(2043, 1, 1, 1, 46, 40), Gigasecond.from(Time.utc(2011, 4, 25, 0, 0, 0))
-  end
-
-  def test_second_test_for_date_only_specification_of_time
-    skip
-    assert_equal Time.utc(2009, 2, 19, 1, 46, 40), Gigasecond.from(Time.utc(1977, 6, 13, 0, 0, 0))
-  end
-
-  def test_third_test_for_date_only_specification_of_time
-    skip
-    assert_equal Time.utc(1991, 3, 27, 1, 46, 40), Gigasecond.from(Time.utc(1959, 7, 19, 0, 0, 0))
-  end
-
   def test_full_time_specified
     skip
     assert_equal Time.utc(2046, 10, 2, 23, 46, 40), Gigasecond.from(Time.utc(2015, 1, 24, 22, 0, 0))
