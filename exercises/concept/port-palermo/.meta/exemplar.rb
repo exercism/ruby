ba816d46ee08a8b76b9b516b2d16e73573c80b07
Gabo <gabrielomar2809@gmail.com>
2023-08-21 11:27:56 -0700

    [Port Of Palermo] Remove redundant returns in exemplar (#1563)

--- exercises/concept/port-palermo/.meta/exemplar.rb
+++ exercises/concept/port-palermo/.meta/exemplar.rb
@@ -2,11 +2,11 @@ module Port
   IDENTIFIER = :PALE
 
   def self.get_identifier(city)
-    return city[0..3].upcase.to_sym
+    city[0..3].upcase.to_sym
   end
 
   def self.get_terminal(ship_identifier)
     cargo = ship_identifier.to_s[0..2]
-    return cargo == "OIL" || cargo == "GAS" ? :A : :B
+    cargo == "OIL" || cargo == "GAS" ? :A : :B
   end
 end
