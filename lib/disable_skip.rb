b398ad449572d645631e31391a99a1e5c550fca6
Victor Goff <keeperotphones@gmail.com>
2023-08-22 03:03:52 -0400

    Fix typo's in comments for skip tests Minitest Patch (#1564)

--- lib/disable_skip.rb
+++ lib/disable_skip.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
-# You can use this to disabe all skips in the current exercise by issuing the
+# You can use this to disable all skips in the current exercise by issuing the
 # following command:
-#     ruby -I../lib -rdisable_skip <fiename_test.rb>
+#     ruby -I../lib -rdisable_skip <filename_test.rb>
 
 module Minitest
   class Test
