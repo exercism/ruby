2909844ddbc59602feee78022a6f9d6b3ee91e77
Felipe Vogel <felipe.vogel@sanabenefits.com>
2023-10-18 02:45:24 -0400

    Write example solution

--- exercises/practice/gilded-rose/.meta/example.rb
+++ exercises/practice/gilded-rose/.meta/example.rb
@@ -1,4 +1,109 @@
-Item = Struct.new(:name, :sell_in, :quality)
+class AbstractItem
+  attr_reader :name, :sell_in, :quality
+
+  def initialize(name:, sell_in:, quality:)
+    @name, @sell_in, @quality = name, sell_in, quality
+    @conjured = name.start_with? "Conjured"
+  end
+
+  def conjured? = @conjured
+
+  def update!
+    update_quality!
+    @quality = 0 if @quality < 0
+    @quality = max_quality if quality > max_quality
+
+    update_sell_in!
+  end
+
+  private
+
+  def max_quality = 50
+
+  def update_quality! = raise NotImplementedError
+
+  def update_sell_in! = @sell_in -= 1
+end
+
+class NormalItem < AbstractItem
+  def self.name_regex = /.+/ # Should be checked last, after other item types.
+
+  private
+
+  def update_quality!
+    quality_change = -1
+    quality_change -= 1 if sell_in <= 0
+    quality_change *= 2 if conjured?
+
+    @quality += quality_change
+  end
+end
+
+class AgedBrie < AbstractItem
+  def self.name_regex = /Aged Brie/
+
+  private
+
+  def update_quality!
+    quality_change = 1
+    quality_change += 1 if sell_in <= 0
+    quality_change *= -2 if conjured? && sell_in <= 0
+
+    @quality += quality_change
+  end
+end
+
+class Sulfuras < AbstractItem
+  def self.name_regex = /Sulfuras, Hand of Ragnaros/
+
+  private
+
+  def max_quality = 80
+
+  def update_quality!
+    quality_change = 0
+    quality_change = -4 if conjured? && sell_in <= 0
+
+    @quality += quality_change
+  end
+
+  def update_sell_in!
+    sell_in_change = 0
+    sell_in_change = -1 if conjured?
+
+    @sell_in += sell_in_change
+  end
+end
+
+class BackstagePass < AbstractItem
+  def self.name_regex = /Backstage passes to a TAFKAL80ETC concert/i
+
+  private
+
+  def update_quality!
+    @quality = 0 and return if sell_in <= 0
+
+    quality_change = 1
+    quality_change += 1 if sell_in <= 10
+    quality_change += 1 if sell_in <= 5
+    quality_change -= 1 if conjured?
+
+    @quality += quality_change
+  end
+end
+
+# An item factory. Not named ItemFactory only because the tests expect `Item`.
+module Item
+  ITEM_CLASSES = [AgedBrie, Sulfuras, BackstagePass, NormalItem]
+
+  def self.new(name:, sell_in:, quality:)
+    item_class(name).new(name:, sell_in:, quality:)
+  end
+
+  private_class_method def self.item_class(name)
+    ITEM_CLASSES.find { name.match?(_1.name_regex) }
+  end
+end
 
 class GildedRose
   def initialize(items)
@@ -7,51 +112,7 @@ class GildedRose
 
   def update!
     @items.each do |item|
-      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality -= 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-          end
-        end
-      end
-
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in -= 1
-      end
-
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality -= 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality += 1
-          end
-        end
-      end
+      item.update!
     end
   end
 end
