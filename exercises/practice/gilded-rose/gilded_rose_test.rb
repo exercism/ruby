b61d90987def717c75c0ee39567c18ed3dcae216
Felipe Vogel <felipe.vogel@sanabenefits.com>
2023-10-20 11:01:52 -0400

    Unskip tests on unimplemented feature

--- exercises/practice/gilded-rose/gilded_rose_test.rb
+++ exercises/practice/gilded-rose/gilded_rose_test.rb
@@ -111,151 +111,126 @@ class GildedRoseTest < Minitest::Test
   end
 
   def test_conjured_normal_item_before_sell_date
-    skip
     update_with sell_in: 1, quality: 10, name: "Conjured Rabbit",
       quality_change: -2
   end
 
   def test_conjured_normal_item_on_sell_date
-    skip
     update_with sell_in: 0, quality: 10, name: "Conjured Rabbit",
       quality_change: -10
   end
 
   def test_conjured_normal_item_after_sell_date
-    skip
     update_with sell_in: -1, quality: 10, name: "Conjured Rabbit",
       quality_change: -10
   end
 
   def test_conjured_normal_item_of_zero_quality
-    skip
     update_with sell_in: 1, quality: 0, name: "Conjured Rabbit",
       quality_change: 0
   end
 
   def test_conjured_normal_item_near_zero_quality
-    skip
     update_with sell_in: 1, quality: 1, name: "Conjured Rabbit",
       quality_change: -1
   end
 
   def test_conjured_brie_before_sell_date
-    skip
     update_with sell_in: 1, quality: 0, name: "Conjured Aged Brie",
       quality_change: 1
   end
 
   def test_conjured_brie_on_sell_date
-    skip
     update_with sell_in: 0, quality: 10, name: "Conjured Aged Brie",
       quality_change: -10
   end
 
   def test_conjured_brie_after_sell_date
-    skip
     update_with sell_in: -1, quality: 10, name: "Conjured Aged Brie",
       quality_change: -10
   end
 
   def test_conjured_brie_of_max_quality
-    skip
     update_with sell_in: 1, quality: 50, name: "Conjured Aged Brie",
       quality_change: 0
   end
 
   def test_conjured_brie_of_zero_quality
-    skip
     update_with sell_in: 0, quality: 0, name: "Conjured Aged Brie",
       quality_change: 0
   end
 
   def test_conjured_brie_near_zero_quality
-    skip
     update_with sell_in: 0, quality: 1, name: "Conjured Aged Brie",
       quality_change: -1
   end
 
   def test_conjured_sulfuras_before_sell_date
-    skip
     update_with sell_in: 1, quality: 80, name: "Conjured Sulfuras, Hand of Ragnaros",
       quality_change: 0
   end
 
   def test_conjured_sulfuras_on_sell_date
-    skip
     update_with sell_in: 0, quality: 80, name: "Conjured Sulfuras, Hand of Ragnaros",
       quality_change: -80
   end
 
   def test_conjured_sulfuras_after_sell_date
-    skip
     update_with sell_in: -1, quality: 80, name: "Conjured Sulfuras, Hand of Ragnaros",
       quality_change: -80
   end
 
   def test_conjured_sulfuras_of_zero_quality
-    skip
     update_with sell_in: 0, quality: 0, name: "Conjured Sulfuras, Hand of Ragnaros",
       quality_change: 0
   end
 
   def test_conjured_sulfuras_near_zero_quality
-    skip
     update_with sell_in: 0, quality: 1, name: "Conjured Sulfuras, Hand of Ragnaros",
       quality_change: -1
   end
 
   def test_conjured_backstage_pass_far_from_sell_date
-    skip
     update_with sell_in: 11, quality: 10, name: "Conjured backstage passes to a TAFKAL80ETC concert",
       quality_change: 0
   end
 
   def test_conjured_backstage_pass_medium_from_sell_date_upper_bound
-    skip
     update_with sell_in: 10, quality: 0, name: "Conjured backstage passes to a TAFKAL80ETC concert",
       quality_change: 1
   end
 
   def test_conjured_backstage_pass_medium_from_sell_date_lower_bound
-    skip
     update_with sell_in: 6, quality: 0, name: "Conjured backstage passes to a TAFKAL80ETC concert",
       quality_change: 1
   end
 
   def test_conjured_backstage_pass_close_to_sell_date_upper_bound
-    skip
     update_with sell_in: 5, quality: 0, name: "Conjured backstage passes to a TAFKAL80ETC concert",
       quality_change: 2
   end
 
   def test_conjured_backstage_pass_close_to_sell_date_lower_bound
-    skip
     update_with sell_in: 1, quality: 0, name: "Conjured backstage passes to a TAFKAL80ETC concert",
       quality_change: 2
   end
 
   def test_conjured_backstage_pass_on_sell_date
-    skip
     update_with sell_in: 0, quality: 10, name: "Conjured backstage passes to a TAFKAL80ETC concert",
       quality_change: -10
   end
 
   def test_conjured_backstage_pass_after_sell_date
-    skip
     update_with sell_in: -1, quality: 10, name: "Conjured backstage passes to a TAFKAL80ETC concert",
       quality_change: -10
   end
 
   def test_conjured_backstage_pass_of_max_quality
-    skip
     update_with sell_in: 10, quality: 50, name: "Conjured backstage passes to a TAFKAL80ETC concert",
       quality_change: 0
   end
 
   def test_conjured_backstage_pass_near_max_quality
-    skip
     update_with sell_in: 1, quality: 49, name: "Conjured backstage passes to a TAFKAL80ETC concert",
       quality_change: 1
   end
