2bee1064c42331868c0b886d48e1f1c8e4b77c84
bigzinxb <bigzin369@gmail.com>
2023-10-22 23:32:40 -0300

    Resolve

--- exercises/practice/binary-search/binary_search.rb
+++ exercises/practice/binary-search/binary_search.rb
@@ -1,7 +1,28 @@
-=begin
-Write your code for the 'Binary Search' exercise in this file. Make the tests in
-`binary_search_test.rb` pass.
+class BinarySearch
+    attr_accessor :arr
+    def initialize(arr)
+        @arr = arr
+    end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/binary-search` directory.
-=end
+    def search_for(target)
+        left = 0
+        rigth = @arr.length
+        
+        return 0 if target == @arr[0]
+        
+        while left <= rigth
+            middle = (left + rigth) / 2
+            item = @arr[middle]
+
+            if item == target
+                return middle
+            elsif item > target
+                rigth = middle - 1
+            else
+                left = middle + 1
+            end
+        end
+
+        return nil
+    end
+end
