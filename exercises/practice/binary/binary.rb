412e8d98df897c89942fb9c5a7ea82f555c233ab
bigzinxb <bigzin369@gmail.com>
2023-10-23 12:28:01 -0300

    ....

--- exercises/practice/binary/binary.rb
+++ exercises/practice/binary/binary.rb
@@ -1,7 +1,43 @@
-=begin
-Write your code for the 'Binary' exercise in this file. Make the tests in
-`binary_test.rb` pass.
+module Binary
+    def self.to_decimal(number_decimal)
+        begin
+            eval("BASE = 2")
+            number = number_decimal.to_i
+            binary = []
+            
+            while number > BASE
+                number /= BASE
+                rest = number % BASE
+                binary << rest
+            end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/binary` directory.
-=end
+            return self.reversed(binary)
+        rescue ArgumentError => error
+            puts error
+        end
+    end
+    
+    private
+    def self.reversed(arr)
+        new_arr = []
+        left = 0
+        rigth = (arr.length - 1)
+
+        (arr.length - 1).times do
+            if arr.member?(arr[left]) && arr.member?(arr[rigth])
+                new_arr[left] = arr.delete_at[rigth]
+                new_arr[rigth] = arr.delete_at[left]
+
+                left += 1
+                rigth -= 1
+            end
+        end
+
+        return self.conc(new_arr)
+    end
+
+    def self.conc(arr)
+        return arr.length if arr.length <= 1        
+        return arr.reduce(:+)
+    end
+end
