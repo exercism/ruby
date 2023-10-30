7bf840ef32e73c8be7b2e91a7e3b1e4741dd6090
Carl <meatballsofmeatballs@gmail.com>
2023-10-30 18:40:32 +0100

    Add case concept + exercise

---
+++ exercises/concept/blackjack/blackjack.rb
@@ -0,0 +1,13 @@
+module Blackjack
+  def self.parse_card(card)
+    raise "Please implement the Blackjack.parse_card method"
+  end
+
+  def self.card_range(card1, card2)
+    raise "Please implement the Blackjack.card_range method"
+  end
+
+  def self.first_turn(card1, card2, dealer_card)
+    raise "Please implement the Blackjack.first_turn method"
+  end
+end
