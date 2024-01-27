998738c6b1e6a3a2f93b21e87756d07016e4ba9b
Chris Cadden <cadden.cs@gmail.com>
2024-01-27 14:24:42 -0500

    Add dnd_character.rb stub file
    Co-authored-by: KOTP <keeperotphones@gmail.com>

---
+++ exercises/practice/dnd-character/dnd_character.rb
@@ -0,0 +1,17 @@
+=begin
+Write your code for the 'D&D Character' exercise in this file. Make the tests in
+`dnd_character_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/dnd-character` directory.
+=end
+
+class DndCharacter
+  def self.modifier
+    # Your code here
+  end
+
+  def initialize
+    # Your code here
+  end
+end
