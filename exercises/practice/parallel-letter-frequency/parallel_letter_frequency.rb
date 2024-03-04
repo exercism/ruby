d6f34f851efb19340af553c6da1476fd6cf55336
Chris Cadden <cadden.cs@gmail.com>
2024-03-04 18:16:01 -0500

    Add parallel letter frequency (#1638)
    * bin/configlet create --practice-exercise parallel-letter-frequency
    * Add parallel-letter-frequency difficulty
    * Add parallel_letter_frequency.rb stub file
    * Include instructions.append.md with parallelism/concurrency resources
    * Add parallel_letter_frequency_test.rb
    * Add Minitest benchmarking file for sequential vs parallel execution
    * Add example implementation of parallel_letter_frequency exercise
    * Add authors
    
    ---------
    
    Co-authored-by: KOTP <keeperotphones@gmail.com>

---
+++ exercises/practice/parallel-letter-frequency/parallel_letter_frequency.rb
@@ -0,0 +1,7 @@
+=begin
+Write your code for the 'Parallel Letter Frequency' exercise in this file. Make
+the tests in `parallel_letter_frequency_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/parallel_letter_frequency` directory.
+=end
