ec3859b53bb38e2b2b16d61072371ab07f11800c
Felipe Vogel <fps.vogel@gmail.com>
2024-03-14 21:32:07 -0400

    Add starter comment to knapsack.rb

---
+++ exercises/practice/knapsack/knapsack.rb
@@ -0,0 +1,7 @@
+=begin
+Write your code for the 'Knapsack' exercise in this file. Make the tests in
+`knapsack_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/knapsack` directory.
+=end
