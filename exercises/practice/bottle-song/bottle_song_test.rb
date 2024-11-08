94743036e00679a04d4b85c70f097875f05b4b96
meatball <69751659+meatball133@users.noreply.github.com>
2024-11-08 20:12:35 +0100

    Fix whitespace offence

--- exercises/practice/bottle-song/bottle_song_test.rb
+++ exercises/practice/bottle-song/bottle_song_test.rb
@@ -53,7 +53,7 @@ class BottleSongTest < Minitest::Test
       Ten green bottles hanging on the wall,
       And if one green bottle should accidentally fall,
       There'll be nine green bottles hanging on the wall.
-      
+
       Nine green bottles hanging on the wall,
       Nine green bottles hanging on the wall,
       And if one green bottle should accidentally fall,
@@ -69,12 +69,12 @@ class BottleSongTest < Minitest::Test
       Three green bottles hanging on the wall,
       And if one green bottle should accidentally fall,
       There'll be two green bottles hanging on the wall.
-      
+
       Two green bottles hanging on the wall,
       Two green bottles hanging on the wall,
       And if one green bottle should accidentally fall,
       There'll be one green bottle hanging on the wall.
-      
+
       One green bottle hanging on the wall,
       One green bottle hanging on the wall,
       And if one green bottle should accidentally fall,
@@ -90,47 +90,47 @@ class BottleSongTest < Minitest::Test
       Ten green bottles hanging on the wall,
       And if one green bottle should accidentally fall,
       There'll be nine green bottles hanging on the wall.
-      
+
       Nine green bottles hanging on the wall,
       Nine green bottles hanging on the wall,
       And if one green bottle should accidentally fall,
       There'll be eight green bottles hanging on the wall.
-      
+
       Eight green bottles hanging on the wall,
       Eight green bottles hanging on the wall,
       And if one green bottle should accidentally fall,
       There'll be seven green bottles hanging on the wall.
-      
+
       Seven green bottles hanging on the wall,
       Seven green bottles hanging on the wall,
       And if one green bottle should accidentally fall,
       There'll be six green bottles hanging on the wall.
-      
+
       Six green bottles hanging on the wall,
       Six green bottles hanging on the wall,
       And if one green bottle should accidentally fall,
       There'll be five green bottles hanging on the wall.
-      
+
       Five green bottles hanging on the wall,
       Five green bottles hanging on the wall,
       And if one green bottle should accidentally fall,
       There'll be four green bottles hanging on the wall.
-      
+
       Four green bottles hanging on the wall,
       Four green bottles hanging on the wall,
       And if one green bottle should accidentally fall,
       There'll be three green bottles hanging on the wall.
-      
+
       Three green bottles hanging on the wall,
       Three green bottles hanging on the wall,
       And if one green bottle should accidentally fall,
       There'll be two green bottles hanging on the wall.
-      
+
       Two green bottles hanging on the wall,
       Two green bottles hanging on the wall,
       And if one green bottle should accidentally fall,
       There'll be one green bottle hanging on the wall.
-      
+
       One green bottle hanging on the wall,
       One green bottle hanging on the wall,
       And if one green bottle should accidentally fall,
