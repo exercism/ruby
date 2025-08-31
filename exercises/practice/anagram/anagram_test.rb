470834d232d54e69573743df122bba6e6afc33b6
meatball <69751659+meatball133@users.noreply.github.com>
2025-08-31 23:14:46 +0200

    Sync (#1778)
    * Fix exercise naming in generator (#1770)
    
    * Enhance underscore method to filter invalid characters and update test file naming convention
    * Add test case which test underscore for special characters
    * Use character classes in regex. Also expose description for nested test cases.
    * Underscore must not create multiple underscores around special characters
    
    * Add templates to exercises batch 1 (#1771)
    
    * Make test name unique in allergies
    * Changed exercises to use single quotes. Changed template for allergies so it should match the old version.
    * Update acronym solution to work on the new test
    * Fix import and class in atbash test
    * Fix exercise naming in generator (#1770) (#1774)
    * Enhance underscore method to filter invalid characters and update test file naming convention
    * Add test case which test underscore for special characters
    * Use character classes in regex. Also expose description for nested test cases.
    * Underscore must not create multiple underscores around special characters
    
    ---------
    
    Co-authored-by: Victor Goff <keeperotphones+github@gmail.com>
    
    ---------
    
    Co-authored-by: Victor Goff <keeperotphones+github@gmail.com>

--- exercises/practice/anagram/anagram_test.rb
+++ exercises/practice/anagram/anagram_test.rb
@@ -4,129 +4,145 @@ require_relative 'anagram'
 class AnagramTest < Minitest::Test
   def test_no_matches
     # skip
-    detector = Anagram.new("diaper")
+    detector = Anagram.new('diaper')
     anagrams = detector.match(%w[hello world zombies pants])
-    expected = []
+    expected = %w[]
+
     assert_equal expected, anagrams
   end
 
   def test_detects_two_anagrams
     skip
-    detector = Anagram.new("solemn")
+    detector = Anagram.new('solemn')
     anagrams = detector.match(%w[lemons cherry melons])
     expected = %w[lemons melons]
+
     assert_equal expected, anagrams
   end
 
   def test_does_not_detect_anagram_subsets
     skip
-    detector = Anagram.new("good")
+    detector = Anagram.new('good')
     anagrams = detector.match(%w[dog goody])
-    expected = []
+    expected = %w[]
+
     assert_equal expected, anagrams
   end
 
   def test_detects_anagram
     skip
-    detector = Anagram.new("listen")
+    detector = Anagram.new('listen')
     anagrams = detector.match(%w[enlists google inlets banana])
-    expected = ["inlets"]
+    expected = %w[inlets]
+
     assert_equal expected, anagrams
   end
 
   def test_detects_three_anagrams
     skip
-    detector = Anagram.new("allergy")
+    detector = Anagram.new('allergy')
     anagrams = detector.match(%w[gallery ballerina regally clergy largely leading])
     expected = %w[gallery regally largely]
+
     assert_equal expected, anagrams
   end
 
   def test_detects_multiple_anagrams_with_different_case
     skip
-    detector = Anagram.new("nose")
+    detector = Anagram.new('nose')
     anagrams = detector.match(%w[Eons ONES])
     expected = %w[Eons ONES]
+
     assert_equal expected, anagrams
   end
 
   def test_does_not_detect_non_anagrams_with_identical_checksum
     skip
-    detector = Anagram.new("mass")
-    anagrams = detector.match(["last"])
-    expected = []
+    detector = Anagram.new('mass')
+    anagrams = detector.match(%w[last])
+    expected = %w[]
+
     assert_equal expected, anagrams
   end
 
   def test_detects_anagrams_case_insensitively
     skip
-    detector = Anagram.new("Orchestra")
+    detector = Anagram.new('Orchestra')
     anagrams = detector.match(%w[cashregister Carthorse radishes])
-    expected = ["Carthorse"]
+    expected = %w[Carthorse]
+
     assert_equal expected, anagrams
   end
 
   def test_detects_anagrams_using_case_insensitive_subject
     skip
-    detector = Anagram.new("Orchestra")
+    detector = Anagram.new('Orchestra')
     anagrams = detector.match(%w[cashregister carthorse radishes])
-    expected = ["carthorse"]
+    expected = %w[carthorse]
+
     assert_equal expected, anagrams
   end
 
   def test_detects_anagrams_using_case_insensitive_possible_matches
     skip
-    detector = Anagram.new("orchestra")
+    detector = Anagram.new('orchestra')
     anagrams = detector.match(%w[cashregister Carthorse radishes])
-    expected = ["Carthorse"]
+    expected = %w[Carthorse]
+
     assert_equal expected, anagrams
   end
 
   def test_does_not_detect_an_anagram_if_the_original_word_is_repeated
     skip
-    detector = Anagram.new("go")
-    anagrams = detector.match(["go Go GO"])
-    expected = []
+    detector = Anagram.new('go')
+    anagrams = detector.match(%w[goGoGO])
+    expected = %w[]
+
     assert_equal expected, anagrams
   end
 
   def test_anagrams_must_use_all_letters_exactly_once
     skip
-    detector = Anagram.new("tapper")
-    anagrams = detector.match(["patter"])
-    expected = []
+    detector = Anagram.new('tapper')
+    anagrams = detector.match(%w[patter])
+    expected = %w[]
+
     assert_equal expected, anagrams
   end
 
   def test_words_are_not_anagrams_of_themselves
     skip
-    detector = Anagram.new("BANANA")
-    anagrams = detector.match(["BANANA"])
-    expected = []
+    detector = Anagram.new('BANANA')
+    anagrams = detector.match(%w[BANANA])
+    expected = %w[]
+
     assert_equal expected, anagrams
   end
 
   def test_words_are_not_anagrams_of_themselves_even_if_letter_case_is_partially_different
     skip
-    detector = Anagram.new("BANANA")
-    anagrams = detector.match(["Banana"])
-    expected = []
+    detector = Anagram.new('BANANA')
+    anagrams = detector.match(%w[Banana])
+    expected = %w[]
+
     assert_equal expected, anagrams
   end
 
   def test_words_are_not_anagrams_of_themselves_even_if_letter_case_is_completely_different
     skip
-    detector = Anagram.new("BANANA")
-    anagrams = detector.match(["banana"])
-    expected = []
+    detector = Anagram.new('BANANA')
+    anagrams = detector.match(%w[banana])
+    expected = %w[]
+
     assert_equal expected, anagrams
   end
 
   def test_words_other_than_themselves_can_be_anagrams
     skip
-    detector = Anagram.new("LISTEN")
+    detector = Anagram.new('LISTEN')
     anagrams = detector.match(%w[LISTEN Silent])
-    expected = ["Silent"]
+    expected = %w[Silent]
+
     assert_equal expected, anagrams
   end
 end
