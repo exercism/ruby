80cf5942d5dcb47fefa9bf31698d696886eb9a8a
meatball <69751659+meatball133@users.noreply.github.com>
2025-09-07 19:23:17 +0200

    Add templates to exercises batch 4 (#1775)
    * Enhance underscore method to filter invalid characters and update test file naming convention
    * Add test case which test underscore for special characters
    * Use character classes in regex. Also expose description for nested test cases.
    * Underscore must not create multiple underscores around special characters
    * Make test name unique in allergies
    * Change exercises to use single quotes.
    * Change template for allergies so it should match the old version.
    * Update acronym solution to work on the new test
    * Fix import and class in atbash test
    * Enhance underscore method to filter invalid characters and update test file naming convention
    * Add test case which test underscore for special characters
    * Use character classes in regex. Also expose description for nested test cases.
    * Underscore must not create multiple underscores around special characters
    
    ---------
    
    Co-authored-by: Victor Goff <keeperotphones+github@gmail.com>
    
    ---------
    
    Co-authored-by: Victor Goff <keeperotphones+github@gmail.com>
    
    * Fix formatting in dominoes
    
    * Add EOL
    
    * remove trailing spaces
    
    ---------
    
    Co-authored-by: Victor Goff <keeperotphones+github@gmail.com>

--- exercises/practice/flatten-array/flatten_array_test.rb
+++ exercises/practice/flatten-array/flatten_array_test.rb
@@ -4,7 +4,8 @@ require_relative 'flatten_array'
 class FlattenArrayTest < Minitest::Test
   def test_empty
     # skip
-    assert_empty FlattenArray.flatten([])
+    flat_array = FlattenArray.flatten([])
+    assert_empty flat_array
   end
 
   def test_no_nesting
@@ -15,7 +16,8 @@ class FlattenArrayTest < Minitest::Test
 
   def test_flattens_a_nested_array
     skip
-    assert_empty FlattenArray.flatten([[[]]])
+    flat_array = FlattenArray.flatten([[[]]])
+    assert_empty flat_array
   end
 
   def test_flattens_array_with_just_integers_present
@@ -36,14 +38,27 @@ class FlattenArrayTest < Minitest::Test
     assert_equal [1, 2, 3, 4, 5, 6, 7, 8], flat_array
   end
 
-  def test_6_level_nest_list_with_null_values
+  def test_consecutive_null_values_at_the_front_of_the_array_are_omitted_from_the_final_result
+    skip
+    flat_array = FlattenArray.flatten([nil, nil, 3])
+    assert_equal [3], flat_array
+  end
+
+  def test_consecutive_null_values_in_the_middle_of_the_array_are_omitted_from_the_final_result
+    skip
+    flat_array = FlattenArray.flatten([1, nil, nil, 4])
+    assert_equal [1, 4], flat_array
+  end
+
+  def test_6_level_nested_array_with_null_values
     skip
     flat_array = FlattenArray.flatten([0, 2, [[2, 3], 8, [[100]], nil, [[nil]]], -2])
     assert_equal [0, 2, 2, 3, 8, 100, -2], flat_array
   end
 
-  def test_all_values_in_nested_list_are_null
+  def test_all_values_in_nested_array_are_null
     skip
-    assert_empty FlattenArray.flatten([nil, [[[nil]]], nil, nil, [[nil, nil], nil], nil])
+    flat_array = FlattenArray.flatten([nil, [[[nil]]], nil, nil, [[nil, nil], nil], nil])
+    assert_empty flat_array
   end
 end
--- exercises/practice/gigasecond/gigasecond_test.rb
+++ exercises/practice/gigasecond/gigasecond_test.rb
@@ -1,14 +1,30 @@
 require 'minitest/autorun'
 require_relative 'gigasecond'
+require 'time'
 
 class GigasecondTest < Minitest::Test
-  def test_full_time_specified
+  def test_date_only_specification_of_time
     # skip
-    assert_equal Time.utc(2046, 10, 2, 23, 46, 40), Gigasecond.from(Time.utc(2015, 1, 24, 22, 0, 0))
+    assert_equal Time.parse('2043-01-01T01:46:40 UTC'), Gigasecond.from(Time.parse('2011-04-25T00:00:00 UTC'))
+  end
+
+  def test_second_test_for_date_only_specification_of_time
+    skip
+    assert_equal Time.parse('2009-02-19T01:46:40 UTC'), Gigasecond.from(Time.parse('1977-06-13T00:00:00 UTC'))
+  end
+
+  def test_third_test_for_date_only_specification_of_time
+    skip
+    assert_equal Time.parse('1991-03-27T01:46:40 UTC'), Gigasecond.from(Time.parse('1959-07-19T00:00:00 UTC'))
+  end
+
+  def test_full_time_specified
+    skip
+    assert_equal Time.parse('2046-10-02T23:46:40 UTC'), Gigasecond.from(Time.parse('2015-01-24T22:00:00 UTC'))
   end
 
   def test_full_time_with_day_roll_over
     skip
-    assert_equal Time.utc(2046, 10, 3, 1, 46, 39), Gigasecond.from(Time.utc(2015, 1, 24, 23, 59, 59))
+    assert_equal Time.parse('2046-10-03T01:46:39 UTC'), Gigasecond.from(Time.parse('2015-01-24T23:59:59 UTC'))
   end
 end
