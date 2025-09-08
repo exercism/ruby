0188d99237548ca485260fc0cdf0435550d7dd1a
meatball <meatballsofmeatballs@gmail.com>
2025-09-08 19:43:46 +0200

    Add templates for exercises batch 6

--- exercises/practice/linked-list/linked_list.rb
+++ exercises/practice/linked-list/linked_list.rb
@@ -1,7 +1,85 @@
-=begin
-Write your code for the 'Linked List' exercise in this file. Make the tests in
-`linked_list_test.rb` pass.
+class Element
+  attr_accessor :prev, :next
+  attr_reader :datum
 
-To get started with TDD, see the `README.md` file in your
-`ruby/linked-list` directory.
-=end
+  def initialize(datum, next_element = nil, prev = nil)
+    @datum = datum
+    @next = next_element || self
+    @prev = prev || self
+  end
+end
+
+class Deque
+  def initialize
+    @front = nil
+  end
+
+  def push(value)
+    if @front.nil?
+      @front = Element.new(value)
+    else
+      back = @front.prev
+      n = Element.new(value, @front, back)
+      back.next = n
+      @front.prev = n
+    end
+  end
+
+  def unshift(value)
+    push(value)
+    @front = @front.prev
+  end
+
+  def pop
+    @front = @front.prev
+    shift
+  end
+
+  def shift
+    value = @front.datum
+    front = @front.next
+    back = @front.prev
+    if front.equal?(@front)
+      @front = nil
+    else
+      front.prev = back
+      back.next = front
+      @front = front
+    end
+    value
+  end
+
+  def delete(value)
+    return if @front.nil?
+
+    current = @front
+    loop do
+      if current.datum == value
+        if current.equal?(@front) && current.next.equal?(@front)
+          @front = nil
+          break
+        else
+          current.prev.next = current.next
+          current.next.prev = current.prev
+          @front = current.next if current.equal?(@front)
+          break
+        end
+      end
+      current = current.next
+      break if current.equal?(@front)
+    end
+  end
+
+  def count
+    return 0 if @front.nil?
+
+    count = 0
+    current = @front
+    loop do
+      count += 1
+      current = current.next
+      break if current.equal?(@front)
+    end
+    count
+  end
+end
--- exercises/practice/luhn/luhn_test.rb
+++ exercises/practice/luhn/luhn_test.rb
@@ -4,111 +4,111 @@ require_relative 'luhn'
 class LuhnTest < Minitest::Test
   def test_single_digit_strings_can_not_be_valid
     # skip
-    refute Luhn.valid?("1")
+    refute Luhn.valid?('1')
   end
 
   def test_a_single_zero_is_invalid
     skip
-    refute Luhn.valid?("0")
+    refute Luhn.valid?('0')
   end
 
   def test_a_simple_valid_sin_that_remains_valid_if_reversed
     skip
-    assert Luhn.valid?("059")
+    assert Luhn.valid?('059')
   end
 
   def test_a_simple_valid_sin_that_becomes_invalid_if_reversed
     skip
-    assert Luhn.valid?("59")
+    assert Luhn.valid?('59')
   end
 
   def test_a_valid_canadian_sin
     skip
-    assert Luhn.valid?("055 444 285")
+    assert Luhn.valid?('055 444 285')
   end
 
   def test_invalid_canadian_sin
     skip
-    refute Luhn.valid?("055 444 286")
+    refute Luhn.valid?('055 444 286')
   end
 
   def test_invalid_credit_card
     skip
-    refute Luhn.valid?("8273 1232 7352 0569")
+    refute Luhn.valid?('8273 1232 7352 0569')
   end
 
   def test_invalid_long_number_with_an_even_remainder
     skip
-    refute Luhn.valid?("1 2345 6789 1234 5678 9012")
+    refute Luhn.valid?('1 2345 6789 1234 5678 9012')
   end
 
   def test_invalid_long_number_with_a_remainder_divisible_by_5
     skip
-    refute Luhn.valid?("1 2345 6789 1234 5678 9013")
+    refute Luhn.valid?('1 2345 6789 1234 5678 9013')
   end
 
   def test_valid_number_with_an_even_number_of_digits
     skip
-    assert Luhn.valid?("095 245 88")
+    assert Luhn.valid?('095 245 88')
   end
 
   def test_valid_number_with_an_odd_number_of_spaces
     skip
-    assert Luhn.valid?("234 567 891 234")
+    assert Luhn.valid?('234 567 891 234')
   end
 
   def test_valid_strings_with_a_non_digit_added_at_the_end_become_invalid
     skip
-    refute Luhn.valid?("059a")
+    refute Luhn.valid?('059a')
   end
 
   def test_valid_strings_with_punctuation_included_become_invalid
     skip
-    refute Luhn.valid?("055-444-285")
+    refute Luhn.valid?('055-444-285')
   end
 
   def test_valid_strings_with_symbols_included_become_invalid
     skip
-    refute Luhn.valid?("055# 444$ 285")
+    refute Luhn.valid?('055# 444$ 285')
   end
 
   def test_single_zero_with_space_is_invalid
     skip
-    refute Luhn.valid?(" 0")
+    refute Luhn.valid?(' 0')
   end
 
   def test_more_than_a_single_zero_is_valid
     skip
-    assert Luhn.valid?("0000 0")
+    assert Luhn.valid?('0000 0')
   end
 
   def test_input_digit_9_is_correctly_converted_to_output_digit_9
     skip
-    assert Luhn.valid?("091")
+    assert Luhn.valid?('091')
   end
 
   def test_very_long_input_is_valid
     skip
-    assert Luhn.valid?("9999999999 9999999999 9999999999 9999999999")
+    assert Luhn.valid?('9999999999 9999999999 9999999999 9999999999')
   end
 
   def test_valid_luhn_with_an_odd_number_of_digits_and_non_zero_first_digit
     skip
-    assert Luhn.valid?("109")
+    assert Luhn.valid?('109')
   end
 
   def test_using_ascii_value_for_non_doubled_non_digit_isnt_allowed
     skip
-    refute Luhn.valid?("055b 444 285")
+    refute Luhn.valid?('055b 444 285')
   end
 
   def test_using_ascii_value_for_doubled_non_digit_isnt_allowed
     skip
-    refute Luhn.valid?(":9")
+    refute Luhn.valid?(':9')
   end
 
   def test_non_numeric_non_space_char_in_the_middle_with_a_sum_thats_divisible_by_10_isnt_allowed
     skip
-    refute Luhn.valid?("59%59")
+    refute Luhn.valid?('59%59')
   end
 end
