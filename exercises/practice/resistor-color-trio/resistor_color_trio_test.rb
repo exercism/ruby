f714991cf49fe64230b8cb67c383e403e938de98
meatball <69751659+meatball133@users.noreply.github.com>
2025-09-30 17:16:50 +0200

    Update resistor-color-trio (#1794)

--- exercises/practice/resistor-color-trio/resistor_color_trio_test.rb
+++ exercises/practice/resistor-color-trio/resistor_color_trio_test.rb
@@ -4,26 +4,71 @@ require_relative 'resistor_color_trio'
 class ResistorColorTrioTest < Minitest::Test
   def test_orange_and_orange_and_black
     # skip
-    assert_equal "Resistor value: 33 ohms", ResistorColorTrio.new(%w[orange orange black]).label
+    actual = ResistorColorTrio.new(%w[orange orange black]).label
+    expected = 'Resistor value: 33 ohms'
+    assert_equal expected, actual
   end
 
   def test_blue_and_grey_and_brown
     skip
-    assert_equal "Resistor value: 680 ohms", ResistorColorTrio.new(%w[blue grey brown]).label
+    actual = ResistorColorTrio.new(%w[blue grey brown]).label
+    expected = 'Resistor value: 680 ohms'
+    assert_equal expected, actual
   end
 
   def test_red_and_black_and_red
     skip
-    assert_equal "Resistor value: 2 kiloohms", ResistorColorTrio.new(%w[red black red]).label
+    actual = ResistorColorTrio.new(%w[red black red]).label
+    expected = 'Resistor value: 2 kiloohms'
+    assert_equal expected, actual
   end
 
   def test_green_and_brown_and_orange
     skip
-    assert_equal "Resistor value: 51 kiloohms", ResistorColorTrio.new(%w[green brown orange]).label
+    actual = ResistorColorTrio.new(%w[green brown orange]).label
+    expected = 'Resistor value: 51 kiloohms'
+    assert_equal expected, actual
   end
 
   def test_yellow_and_violet_and_yellow
     skip
-    assert_equal "Resistor value: 470 kiloohms", ResistorColorTrio.new(%w[yellow violet yellow]).label
+    actual = ResistorColorTrio.new(%w[yellow violet yellow]).label
+    expected = 'Resistor value: 470 kiloohms'
+    assert_equal expected, actual
+  end
+
+  def test_blue_and_violet_and_blue
+    skip
+    actual = ResistorColorTrio.new(%w[blue violet blue]).label
+    expected = 'Resistor value: 67 megaohms'
+    assert_equal expected, actual
+  end
+
+  def test_minimum_possible_value
+    skip
+    actual = ResistorColorTrio.new(%w[black black black]).label
+    expected = 'Resistor value: 0 ohms'
+    assert_equal expected, actual
+  end
+
+  def test_maximum_possible_value
+    skip
+    actual = ResistorColorTrio.new(%w[white white white]).label
+    expected = 'Resistor value: 99 gigaohms'
+    assert_equal expected, actual
+  end
+
+  def test_first_two_colors_make_an_invalid_octal_number
+    skip
+    actual = ResistorColorTrio.new(%w[black grey black]).label
+    expected = 'Resistor value: 8 ohms'
+    assert_equal expected, actual
+  end
+
+  def test_ignore_extra_colors
+    skip
+    actual = ResistorColorTrio.new(%w[blue green yellow orange]).label
+    expected = 'Resistor value: 650 kiloohms'
+    assert_equal expected, actual
   end
 end
