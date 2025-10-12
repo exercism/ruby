4474bb9da642fd94a2342dbee618dbc8636330f0
meatball <69751659+meatball133@users.noreply.github.com>
2025-10-12 19:39:54 +0200

    Add templates for exercises batch 12 (#1799)

--- exercises/practice/two-bucket/two_bucket_test.rb
+++ exercises/practice/two-bucket/two_bucket_test.rb
@@ -2,59 +2,75 @@ require 'minitest/autorun'
 require_relative 'two_bucket'
 
 class TwoBucketTest < Minitest::Test
-  def test_measure_using_bucket_one_of_size_3_and_bucket_two_of_size_5_start_with_bucket_one
+  def test_measure_using_bucket_one_of_size_3_and_bucket_two_of_size_5___start_with_bucket_one
     # skip
     subject = TwoBucket.new(3, 5, 1, "one")
-    assert_equal 4, subject.moves
-    assert_equal "one", subject.goal_bucket
-    assert_equal 5, subject.other_bucket
+    actual = [subject.moves, subject.goal_bucket, subject.other_bucket]
+    expected = [4, "one", 5]
+    assert_equal expected, actual
   end
 
-  def test_measure_using_bucket_one_of_size_3_and_bucket_two_of_size_5_start_with_bucket_two
+  def test_measure_using_bucket_one_of_size_3_and_bucket_two_of_size_5___start_with_bucket_two
     skip
     subject = TwoBucket.new(3, 5, 1, "two")
-    assert_equal 8, subject.moves
-    assert_equal "two", subject.goal_bucket
-    assert_equal 3, subject.other_bucket
+    actual = [subject.moves, subject.goal_bucket, subject.other_bucket]
+    expected = [8, "two", 3]
+    assert_equal expected, actual
   end
 
-  def test_measure_using_bucket_one_of_size_7_and_bucket_two_of_size_11_start_with_bucket_one
+  def test_measure_using_bucket_one_of_size_7_and_bucket_two_of_size_11___start_with_bucket_one
     skip
     subject = TwoBucket.new(7, 11, 2, "one")
-    assert_equal 14, subject.moves
-    assert_equal "one", subject.goal_bucket
-    assert_equal 11, subject.other_bucket
+    actual = [subject.moves, subject.goal_bucket, subject.other_bucket]
+    expected = [14, "one", 11]
+    assert_equal expected, actual
   end
 
-  def test_measure_using_bucket_one_of_size_7_and_bucket_two_of_size_11_start_with_bucket_two
+  def test_measure_using_bucket_one_of_size_7_and_bucket_two_of_size_11___start_with_bucket_two
     skip
     subject = TwoBucket.new(7, 11, 2, "two")
-    assert_equal 18, subject.moves
-    assert_equal "two", subject.goal_bucket
-    assert_equal 7, subject.other_bucket
+    actual = [subject.moves, subject.goal_bucket, subject.other_bucket]
+    expected = [18, "two", 7]
+    assert_equal expected, actual
   end
 
-  def test_measure_one_step_using_bucket_one_of_size_1_and_bucket_two_of_size_3_start_with_bucket_two
+  def test_measure_one_step_using_bucket_one_of_size_1_and_bucket_two_of_size_3___start_with_bucket_two
     skip
     subject = TwoBucket.new(1, 3, 3, "two")
-    assert_equal 1, subject.moves
-    assert_equal "two", subject.goal_bucket
-    assert_equal 0, subject.other_bucket
+    actual = [subject.moves, subject.goal_bucket, subject.other_bucket]
+    expected = [1, "two", 0]
+    assert_equal expected, actual
   end
 
-  def test_measure_using_bucket_one_of_size_2_and_bucket_two_of_size_3_start_with_bucket_one_and_end_with_bucket_two
+  def test_measure_using_bucket_one_of_size_2_and_bucket_two_of_size_3___start_with_bucket_one_and_end_with_bucket_two
     skip
     subject = TwoBucket.new(2, 3, 3, "one")
-    assert_equal 2, subject.moves
-    assert_equal "two", subject.goal_bucket
-    assert_equal 2, subject.other_bucket
+    actual = [subject.moves, subject.goal_bucket, subject.other_bucket]
+    expected = [2, "two", 2]
+    assert_equal expected, actual
+  end
+
+  def test_measure_using_bucket_one_much_bigger_than_bucket_two
+    skip
+    subject = TwoBucket.new(5, 1, 2, "one")
+    actual = [subject.moves, subject.goal_bucket, subject.other_bucket]
+    expected = [6, "one", 1]
+    assert_equal expected, actual
+  end
+
+  def test_measure_using_bucket_one_much_smaller_than_bucket_two
+    skip
+    subject = TwoBucket.new(3, 15, 9, "one")
+    actual = [subject.moves, subject.goal_bucket, subject.other_bucket]
+    expected = [6, "two", 0]
+    assert_equal expected, actual
   end
 
   def test_with_the_same_buckets_but_a_different_goal_then_it_is_possible
     skip
     subject = TwoBucket.new(6, 15, 9, "one")
-    assert_equal 10, subject.moves
-    assert_equal "two", subject.goal_bucket
-    assert_equal 0, subject.other_bucket
+    actual = [subject.moves, subject.goal_bucket, subject.other_bucket]
+    expected = [10, "two", 0]
+    assert_equal expected, actual
   end
 end
--- exercises/practice/yacht/yacht_test.rb
+++ exercises/practice/yacht/yacht_test.rb
@@ -4,197 +4,204 @@ require_relative 'yacht'
 class YachtTest < Minitest::Test
   def test_yacht
     # skip
-    score = Yacht.new([5, 5, 5, 5, 5], 'yacht').score
+    actual = Yacht.new([5, 5, 5, 5, 5], 'yacht').score
     expected = 50
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
   def test_not_yacht
     skip
-    score = Yacht.new([1, 3, 3, 2, 5], 'yacht').score
+    actual = Yacht.new([1, 3, 3, 2, 5], 'yacht').score
     expected = 0
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
   def test_ones
     skip
-    score = Yacht.new([1, 1, 1, 3, 5], 'ones').score
+    actual = Yacht.new([1, 1, 1, 3, 5], 'ones').score
     expected = 3
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
   def test_ones_out_of_order
     skip
-    score = Yacht.new([3, 1, 1, 5, 1], 'ones').score
+    actual = Yacht.new([3, 1, 1, 5, 1], 'ones').score
     expected = 3
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
   def test_no_ones
     skip
-    score = Yacht.new([4, 3, 6, 5, 5], 'ones').score
+    actual = Yacht.new([4, 3, 6, 5, 5], 'ones').score
     expected = 0
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
   def test_twos
     skip
-    score = Yacht.new([2, 3, 4, 5, 6], 'twos').score
+    actual = Yacht.new([2, 3, 4, 5, 6], 'twos').score
     expected = 2
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
   def test_fours
     skip
-    score = Yacht.new([1, 4, 1, 4, 1], 'fours').score
+    actual = Yacht.new([1, 4, 1, 4, 1], 'fours').score
     expected = 8
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
-  def test_yacht_as_threes
+  def test_yacht_counted_as_threes
     skip
-    score = Yacht.new([3, 3, 3, 3, 3], 'threes').score
+    actual = Yacht.new([3, 3, 3, 3, 3], 'threes').score
     expected = 15
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
-  def test_yacht_of_threes_as_fives
+  def test_yacht_of_3s_counted_as_fives
     skip
-    score = Yacht.new([3, 3, 3, 3, 3], 'fives').score
+    actual = Yacht.new([3, 3, 3, 3, 3], 'fives').score
     expected = 0
-    assert_equal expected, score
+    assert_equal expected, actual
+  end
+
+  def test_fives
+    skip
+    actual = Yacht.new([1, 5, 3, 5, 3], 'fives').score
+    expected = 10
+    assert_equal expected, actual
   end
 
   def test_sixes
     skip
-    score = Yacht.new([2, 3, 4, 5, 6], 'sixes').score
+    actual = Yacht.new([2, 3, 4, 5, 6], 'sixes').score
     expected = 6
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
   def test_full_house_two_small_three_big
     skip
-    score = Yacht.new([2, 2, 4, 4, 4], 'full house').score
+    actual = Yacht.new([2, 2, 4, 4, 4], 'full house').score
     expected = 16
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
   def test_full_house_three_small_two_big
     skip
-    score = Yacht.new([5, 3, 3, 5, 3], 'full house').score
+    actual = Yacht.new([5, 3, 3, 5, 3], 'full house').score
     expected = 19
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
-  def test_two_pair_not_full_house
+  def test_two_pair_is_not_a_full_house
     skip
-    score = Yacht.new([2, 2, 4, 4, 5], 'full house').score
+    actual = Yacht.new([2, 2, 4, 4, 5], 'full house').score
     expected = 0
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
-  def test_four_of_a_kind_not_full_house
+  def test_four_of_a_kind_is_not_a_full_house
     skip
-    score = Yacht.new([1, 4, 4, 4, 4], 'full house').score
+    actual = Yacht.new([1, 4, 4, 4, 4], 'full house').score
     expected = 0
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
-  def test_yacht_not_full_house
+  def test_yacht_is_not_a_full_house
     skip
-    score = Yacht.new([2, 2, 2, 2, 2], 'full house').score
+    actual = Yacht.new([2, 2, 2, 2, 2], 'full house').score
     expected = 0
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
   def test_four_of_a_kind
     skip
-    score = Yacht.new([6, 6, 4, 6, 6], 'four of a kind').score
+    actual = Yacht.new([6, 6, 4, 6, 6], 'four of a kind').score
     expected = 24
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
-  def test_yacht_as_four_of_a_kind
+  def test_yacht_can_be_scored_as_four_of_a_kind
     skip
-    score = Yacht.new([3, 3, 3, 3, 3], 'four of a kind').score
+    actual = Yacht.new([3, 3, 3, 3, 3], 'four of a kind').score
     expected = 12
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
-  def test_full_house_not_four_of_a_kind
+  def test_full_house_is_not_four_of_a_kind
     skip
-    score = Yacht.new([3, 3, 3, 5, 5], 'four of a kind').score
+    actual = Yacht.new([3, 3, 3, 5, 5], 'four of a kind').score
     expected = 0
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
   def test_little_straight
     skip
-    score = Yacht.new([3, 5, 4, 1, 2], 'little straight').score
+    actual = Yacht.new([3, 5, 4, 1, 2], 'little straight').score
     expected = 30
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
-  def test_little_straight_not_big_straight
+  def test_little_straight_as_big_straight
     skip
-    score = Yacht.new([1, 2, 3, 4, 5], 'big straight').score
+    actual = Yacht.new([1, 2, 3, 4, 5], 'big straight').score
     expected = 0
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
-  def test_ordered_but_not_little_straight
+  def test_four_in_order_but_not_a_little_straight
     skip
-    score = Yacht.new([1, 1, 2, 3, 4], 'little straight').score
+    actual = Yacht.new([1, 1, 2, 3, 4], 'little straight').score
     expected = 0
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
-  def test_no_pairs_but_not_little_straight
+  def test_no_pairs_but_not_a_little_straight
     skip
-    score = Yacht.new([1, 2, 3, 4, 6], 'little straight').score
+    actual = Yacht.new([1, 2, 3, 4, 6], 'little straight').score
     expected = 0
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
-  def test_min_and_max_do_not_make_little_straight
+  def test_minimum_is_1_maximum_is_5_but_not_a_little_straight
     skip
-    score = Yacht.new([1, 1, 3, 4, 5], 'little straight').score
+    actual = Yacht.new([1, 1, 3, 4, 5], 'little straight').score
     expected = 0
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
   def test_big_straight
     skip
-    score = Yacht.new([4, 6, 2, 5, 3], 'big straight').score
+    actual = Yacht.new([4, 6, 2, 5, 3], 'big straight').score
     expected = 30
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
   def test_big_straight_as_little_straight
     skip
-    score = Yacht.new([6, 5, 4, 3, 2], 'little straight').score
+    actual = Yacht.new([6, 5, 4, 3, 2], 'little straight').score
     expected = 0
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
-  def test_no_pairs_but_not_big_straight
+  def test_no_pairs_but_not_a_big_straight
     skip
-    score = Yacht.new([6, 5, 4, 3, 1], 'big straight').score
+    actual = Yacht.new([6, 5, 4, 3, 1], 'big straight').score
     expected = 0
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
   def test_choice
     skip
-    score = Yacht.new([3, 3, 5, 6, 6], 'choice').score
+    actual = Yacht.new([3, 3, 5, 6, 6], 'choice').score
     expected = 23
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 
   def test_yacht_as_choice
     skip
-    score = Yacht.new([2, 2, 2, 2, 2], 'choice').score
+    actual = Yacht.new([2, 2, 2, 2, 2], 'choice').score
     expected = 10
-    assert_equal expected, score
+    assert_equal expected, actual
   end
 end
