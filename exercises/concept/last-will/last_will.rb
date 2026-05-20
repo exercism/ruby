01c363e15859c4dc299e2ecd4bc406ad03e9bcf3
Vaia Patta <vaia.patta@gmail.com>
2026-05-20 07:51:42 +0300

    Last Will given: add lines, remove extra (...)

--- exercises/concept/last-will/last_will.rb
+++ exercises/concept/last-will/last_will.rb
@@ -2,62 +2,80 @@
 
 # Secret knowledge of the Zhang family:
 class Zhang
+
   def self.bank_number_part(secret_modifier)
     zhang_part = 8_541
-    (zhang_part * secret_modifier) % 10_000
+    zhang_part * secret_modifier % 10_000
   end
 
   class Red
+
     def self.code_fragment
       512
     end
+
   end
 
   class Blue
+
     def self.code_fragment
       677
     end
+
   end
+
 end
 
 # Secret knowledge of the Khan family:
 class Khan
+
   def self.bank_number_part(secret_modifier)
     khan_part = 4_142
-    (khan_part * secret_modifier) % 10_000
+    khan_part * secret_modifier % 10_000
   end
 
   class Red
+
     def self.code_fragment
       148
     end
+
   end
 
   class Blue
+
     def self.code_fragment
       875
     end
+
   end
+
 end
 
 # Secret knowledge of the Garcia family:
 class Garcia
+
   def self.bank_number_part(secret_modifier)
     garcia_part = 4_023
-    (garcia_part * secret_modifier) % 10_000
+    garcia_part * secret_modifier % 10_000
   end
 
   class Red
+
     def self.code_fragment
       118
     end
+
   end
 
   class Blue
+
     def self.code_fragment
       923
     end
+
   end
+
 end
 
 # Enter your code below
